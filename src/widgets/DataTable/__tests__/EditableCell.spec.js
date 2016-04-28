05cbf7c82fdc032baf59b8c6a8d2e912795e13a3
Christopher Petty <christopher.m.petty@gmail.com>
2016-04-28 13:44:45 +1200

    Revert "Boat load of tests"
    This reverts commit b46c7975e8d8fe59ac464f3d7559637d1804fae3.

--- src/widgets/DataTable/__tests__/EditableCell.spec.js
+++ src/widgets/DataTable/__tests__/EditableCell.spec.js
@@ -1,41 +1,38 @@
-jest.unmock('../EditableCell');
-jest.unmock('enzyme');
-jest.unmock('sinon');
-
 import EditableCell from '../EditableCell';
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import React, {
+  StyleSheet,
   TextInput,
   View,
 } from 'react-native';
 
-describe('EditableCell', () => {
-  it('renders a View and TextInput', () => {
-    const wrapper = shallow(
-      <EditableCell />
-    );
-    expect(wrapper.find(View).length).toBe(1);
-    expect(wrapper.find(TextInput).length).toBe(1);
-  });
-  it('componentWillMount sets state to value as string', () => {
-    const wrapper = shallow(
-      <EditableCell
-        value={50}
-      />
-    );
-    expect(wrapper.state('value')).toEqual('50');
-  });
-  it('onPress works', () => {
-    const callback = sinon.spy();
-    const wrapper = shallow(
-      <EditableCell
-        value={'Foo'}
-        onEndEditing={callback}
-      />
-    );
-    wrapper.find(TextInput).simulate('tyasdhsh');
-    wrapper.find(TextInput).simulate('focusout');
-    expect(callback.called).toBe(true);
-  });
-});
+// import { shallow, render } from 'enzyme';
+// import { expect } from 'chai';
+//
+// const styles = StyleSheet.create({
+//   editableCell: {
+//     fontSize: 20,
+//     height: 45,
+//     textAlign: 'right',
+//     marginRight: 20,
+//   },
+// });
+//
+// describe('EditableCell', () => {
+//   it('renders a view', () => {
+//     const wrapper = shallow(
+//       <EditableCell />
+//     );
+//     expect(wrapper.find(View)).to.be.length(1);
+//   });
+//   it('renders TextInput with value', () => {
+//     const wrapper = shallow(
+//       <EditableCell
+//         style={styles.editableCell}
+//         value={50}
+//       />
+//   );
+//     console.log(wrapper.childAt(0));
+//     expect(wrapper.find(TextInput)).to.be.length(1, 'really?');
+//     expect(wrapper.children().prop('value')).to.contain('50');
+//   });
+// });
