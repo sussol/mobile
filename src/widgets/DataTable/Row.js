82079107d771cf53e7b12fa48e4cad7b52280d10
Christopher Petty <christopher.m.petty@gmail.com>
2016-05-24 17:31:41 +1200

    Changed Row to support onPress when no expansion given as prop

--- src/widgets/DataTable/Row.js
+++ src/widgets/DataTable/Row.js
@@ -28,11 +28,38 @@ export default class Row extends Component {
   }
 
   render() {
-    const { style, children, renderExpansion, ...touchableOpacityProps } = this.props;
+    const { style, children, renderExpansion, onPress, ...touchableOpacityProps } = this.props;
+    if (typeof renderExpansion === 'function') {
+      return (
+        <TouchableOpacity
+          {...touchableOpacityProps}
+          style={[styles.row, style]}
+          onPress={this.expandRow}
+        >
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            {children}
+          </View>
+          {this.state.expanded && renderExpansion()}
+        </TouchableOpacity>
+      );
+    }
+    if (onPress) {
+      return (
+        <TouchableOpacity
+          {...touchableOpacityProps}
+          style={[styles.row, style]}
+          onPress={onPress}
+        >
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            {children}
+          </View>
+        </TouchableOpacity>
+      );
+    }
     return (
       <TouchableOpacity
         {...touchableOpacityProps}
-        style={[defaultStyles.row, style]}
+        style={[styles.row, style]}
         onPress={typeof renderExpansion === 'function' && this.expandRow}
       >
         <View style={{ flex: 1, flexDirection: 'row' }}>
@@ -47,10 +74,11 @@ export default class Row extends Component {
 Row.propTypes = {
   style: React.View.propTypes.style,
   children: React.PropTypes.any,
+  onPress: React.PropTypes.func,
   renderExpansion: React.PropTypes.func,
 };
 
-const defaultStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'column',
