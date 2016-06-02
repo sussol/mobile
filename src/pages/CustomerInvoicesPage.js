2ac0b085929776fa6a85b56b558fb282d4be4d9d
Christopher Petty <christopher.m.petty@gmail.com>
2016-06-02 14:23:48 +1200

    More styles out

--- src/pages/CustomerInvoicesPage.js
+++ src/pages/CustomerInvoicesPage.js
@@ -228,12 +228,6 @@ const localStyles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
-  text: {
-    fontSize: 16,
-    marginLeft: 20,
-    textAlign: 'left',
-  },
   rightMostCell: {
     borderRightWidth: 0,
   },
