723e11fa4d4b977433784cdf95c7a5da7a4ed6fa
Christopher Petty <christopher.m.petty@gmail.com>
2016-06-14 12:27:46 +1200

    Doc block headerCell component

--- src/widgets/DataTable/HeaderCell.js
+++ src/widgets/DataTable/HeaderCell.js
@@ -15,6 +15,22 @@ import {
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+/**
+ * Renders a headerCell that supports being a plain View with Text or being a TouchableOpacity (with
+ * callback). In the latter case Sort arrows will be rendered and controlled with isSelected and
+ * isAscending props.
+ * @param   {object}  props         Properties passed where component was created.
+ * @prop    {boolean} isSelected    When false up+down sort arrows renderHeader, otherwise as below
+ * @prop    {boolean} isAscending   Sort arrow up if true, down if false.
+ * @prop    {StyleSheet} style      Style of the headerCell (View props)
+ * @prop    {StyleSheet} textStyle  Style of the text in the HeaderCell
+ * @prop    {number} width          flexbox flex property, gives weight to the headerCell width
+ * @prop    {func} onPress          CallBack (i.e. should change sort order in parent)
+ * @prop    {string}  text          Text to render in headerCell
+ * @return  {React.Component}       Return TouchableOpacity with sort arrows if onPress is given a
+ *                                  function. Otherwise return a View.
+ */
+
 export function HeaderCell(props) {
   const {
     style,
