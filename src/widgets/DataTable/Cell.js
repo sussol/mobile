6369e5f546c400bcc1c514efbeaf9e16e6a005af
Christopher Petty <christopher.m.petty@gmail.com>
2016-06-15 12:32:17 +1200

    Cell refactor and tests (passing)

--- src/widgets/DataTable/Cell.js
+++ src/widgets/DataTable/Cell.js
@@ -13,39 +13,38 @@ import {
 } from 'react-native';
 
 /**
- * Renders a headerCell that supports being a plain View with Text or being a TouchableOpacity (with
- * callback). In the latter case Sort arrows will be rendered and controlled with isSelected and
- * isAscending props.
+ * Renders a Cell that supports being a string as children, or any component.
  * @param   {object}  props         Properties passed where component was created.
- * @prop    {StyleSheet} style      Style of the headerCell (View props)
- * @prop    {StyleSheet} textStyle  Style of the text in the HeaderCell
- * @prop    {number} width          flexbox flex property, gives weight to the headerCell width
- * @prop    {func} onPress          CallBack (i.e. should change sort order in parent)
- * @prop    {string}  text          Text to render in headerCell
+ * @prop    {StyleSheet} style      Style of the Cell (View props)
+ * @prop    {StyleSheet} textStyle  Style of the text in the Cell
+ * @prop    {number} width          flexbox flex property, gives weight to the Cell width
+ * @prop    {string}  text          Text to render in Cell
  * @return  {React.Component}       Return TouchableOpacity with sort arrows if onPress is given a
  *                                  function. Otherwise return a View.
  */
 export function Cell(props) {
   const { style, textStyle, width, children, ...viewProps } = props;
 
-  if (typeof children !== 'string') {
+  // Render string child in a Text Component
+  if (typeof children === 'string') {
     return (
       <View {...viewProps} style={[defaultStyles.cell, style, { flex: width }]}>
-        {children}
+        <Text style={textStyle}>
+          {children}
+        </Text>
       </View>
     );
   }
-
+  // Render any type of child component(s)
   return (
     <View {...viewProps} style={[defaultStyles.cell, style, { flex: width }]}>
-      <Text style={textStyle}>
-        {children}
-      </Text>
+      {children}
     </View>
   );
 }
 
 Cell.propTypes = {
+  ...View.propTypes,
   style: View.propTypes.style,
   textStyle: Text.propTypes.style,
   width: React.PropTypes.number,
