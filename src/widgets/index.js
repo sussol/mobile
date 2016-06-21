4cefd9955c0a5dec7f9f69ea2822f93b5233b420
Christopher Petty <christopher.m.petty@gmail.com>
2016-06-21 12:27:47 +1200

    Revert "Moved ToggleBar to widget root folder"
    This reverts commit 090853bb3af5cfe933ab861bb841f784f982d13b.

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -2,4 +2,4 @@ export { Button } from './Button';
 export { ConfirmModal, DeleteModal, LoginModal } from './modals';
 export { DataTable } from './DataTable';
 export { SearchBar } from './SearchBar';
-export { ToggleBar } from './ToggleBar';
+export { ToggleBar, ToggleButton } from './ToggleBar';
