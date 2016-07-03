84bc4f35c6a0d01e978a31d1e1e1559502cf4bca
Edwin <edwin@sussol.net>
2016-07-04 11:30:45 +1200

    Styles to go with PageInfo widget

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -24,6 +24,7 @@ export const FINALISED_RED = '#f63b30';
 export const PAGE_CONTENT_PADDING_HORIZONTAL = 20;
 export const PAGE_CONTENT_PADDING_TOP = 10;
 export const PAGE_CONTENT_PADDING_BOTTOM = 20;
+export const SEARCH_BAR_WIDTH = 500;
 
 export default StyleSheet.create({
   appBackground: {
@@ -54,6 +55,11 @@ export default StyleSheet.create({
   horizontalContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  verticalContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
     justifyContent: 'space-between',
   },
   modal: {
