09020419621df94a1ccc42386408edcededd7978
Christopher Petty <christopher.m.petty@gmail.com>
2016-07-04 16:11:40 +1200

    Merge branch 'master' into stocktakes-manage-page
    # Conflicts:
    #	src/database/DataTypes/Stocktake.js
    #	src/database/DataTypes/StocktakeLine.js
    #	src/database/DataTypes/index.js
    #	src/sync/incomingSyncUtils.js
    #	src/sync/outgoingSyncUtils.js

--- src/pages/CustomersPage.js
+++ src/pages/CustomersPage.js
@@ -6,35 +6,109 @@
  */
 
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import { SearchBar } from '../widgets';
+import { GenericTablePage } from './GenericTablePage';
 
-import globalStyles from '../globalStyles';
-import { Button } from '../widgets';
+const DATA_TYPES_DISPLAYED = ['Customer'];
 
-export function CustomersPage(props) {
-  return (
-    <View style={props.style}>
-      <Text>Customers go here.</Text>
-        <Button
-          style={globalStyles.button}
-          textStyle={globalStyles.buttonText}
-          text="Specific Customer"
-          onPress={() => props.navigateTo('customer', 'Customer Name')}
-        />
-        <Button
-          style={globalStyles.button}
-          textStyle={globalStyles.buttonText}
-          text="New Customer Invoice"
-          onPress={() => props.navigateTo('customerInvoice', 'Invoice Num')}
-        />
-    </View>
-  );
+/**
+* Renders the page for displaying Customers.
+* @prop   {Realm}               database      App wide database.
+* @prop   {func}                navigateTo    CallBack for navigation stack.
+* @state  {Realm.Results}       transactions  Filtered to have only supplier_invoice.
+*/
+export class CustomersPage extends GenericTablePage {
+  constructor(props) {
+    super(props);
+    this.state.sortBy = 'name';
+    this.state.customers = props.database.objects('Name')
+                                            .filtered('type == "store" OR type == "patient"');
+    this.columns = COLUMNS;
+    this.dataTypesDisplayed = DATA_TYPES_DISPLAYED;
+    this.getUpdatedData = this.getUpdatedData.bind(this);
+    this.onRowPress = this.onRowPress.bind(this);
+  }
+
+  onRowPress(customer) {
+    this.props.navigateTo(
+      'customer',
+      `${customer.name}`,
+      { customer: customer },
+    );
+  }
+
+  onCheckablePress(customer) {
+    super.onCheckablePress(customer);
+    this.props.database.write(() => {
+      if (!customer.useMasterList) {
+        customer.useMasterList = true;
+      } else {
+        customer.useMasterList = !customer.useMasterList;
+      }
+      this.props.database.save('Name', customer);
+    });
+  }
+
+  /**
+   * Returns updated data according to searchTerm, sortBy and isAscending.
+   */
+  getUpdatedData(searchTerm, sortBy, isAscending) {
+    let data = this.state.customers.filtered(`name BEGINSWITH[c] "${searchTerm}"`);
+    data = data.sorted(sortBy, !isAscending); // 2nd arg: reverse sort
+    return data;
+  }
+
+  renderCell(key, customer) {
+    switch (key) {
+      default:
+      case 'code':
+        return customer.code;
+      case 'name':
+        return customer.name;
+      case 'transactions.length':
+        return customer.transactions.length;
+      case 'selected':
+        return {
+          type: 'checkable',
+          isChecked: customer.useMasterList,
+        };
+    }
+  }
+
+  renderSearchBar() {
+    return (
+      <SearchBar
+        onChange={(event) => this.onSearchChange(event)}
+        keyboardType="numeric"
+      />);
+  }
 }
 
 CustomersPage.propTypes = {
+  database: React.PropTypes.object,
   navigateTo: React.PropTypes.func.isRequired,
-  style: View.propTypes.style,
 };
+
+const COLUMNS = [
+  {
+    key: 'code',
+    width: 1,
+    title: 'CODE',
+  },
+  {
+    key: 'name',
+    width: 5,
+    title: 'NAME',
+    sortable: true,
+  },
+  {
+    key: 'transactions.length',
+    width: 1,
+    title: 'INVOICES',
+  },
+  {
+    key: 'selected',
+    width: 1,
+    title: 'MASTER LIST',
+  },
+];
---
+++ src/widgets/DataTable/CheckableCell.js
@@ -0,0 +1,79 @@
+/* @flow weak */
+
+/**
+ * OfflineMobile Android
+ * Sustainable Solutions (NZ) Ltd. 2016
+ */
+
+import React from 'react';
+import {
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+/**
+ * Renders a CheckableCell that renders either renderIsChecked or renderIsNotChecked when isChecked
+ * is true or false respectively. Whole cell returned is pressable. Callback should affect state of
+ * Parent in some way that keeps the state of parent in sync with state of the CheckableCell. Kept
+ * separate to maintain responsiveness of the cell.
+ * @param   {object}  props             Properties passed where component was created.
+ * @prop    {StyleSheet} style          Style of the CheckableCell (View props).
+ * @prop    {number} width              Flexbox flex property, gives weight to the CheckableCell width
+ * @prop    {object} renderIsChecked    Object is rendered as child in CheckableCell if checked.
+ * @prop    {object} renderIsNotChecked Object is rendered as child in CheckableCell if notchecked.
+ * @prop    {boolean} isChecked         Used to set the initial state of the cell when the
+ *                                      component mounts or rerenders (e.g. table sort
+ *                                      order change).
+ * @return  {React.Component}           Return TouchableOpacity with child rendered according to the
+ *                                      above 3 props.
+ */
+export class CheckableCell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false,
+    };
+    this.onPress = this.onPress.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({ isChecked: this.props.isChecked });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ isChecked: nextProps.isChecked });
+  }
+
+  onPress() {
+    this.setState({ isChecked: !this.state.isChecked });
+    this.props.onPress();
+  }
+
+  render() {
+    const { style,
+      width,
+      renderIsChecked,
+      renderIsNotChecked,
+    } = this.props;
+
+    return (
+      <TouchableOpacity style={[style, { flex: width }]} onPress={() => this.onPress()}>
+        {this.state.isChecked ? renderIsChecked : renderIsNotChecked}
+      </TouchableOpacity>
+    );
+  }
+}
+
+CheckableCell.propTypes = {
+  style: View.propTypes.style,
+  width: React.PropTypes.number,
+  onPress: React.PropTypes.func,
+  renderIsChecked: React.PropTypes.object,
+  renderIsNotChecked: React.PropTypes.object,
+  isChecked: React.PropTypes.bool,
+};
+
+CheckableCell.defaultProps = {
+  width: 1,
+  isChecked: false,
+};
