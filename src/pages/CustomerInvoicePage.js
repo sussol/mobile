cac1886aa89e677155c552459a7c25377f6f0a4d
Edwin <edwin@sussol.net>
2016-07-04 13:32:10 +1200

    Customer Invoice page has PageInfo

--- src/pages/CustomerInvoicePage.js
+++ src/pages/CustomerInvoicePage.js
@@ -6,8 +6,14 @@
  */
 
 import React from 'react';
+import {
+  View,
+} from 'react-native';
 
 import { GenericTablePage } from './GenericTablePage';
+import globalStyles from '../globalStyles';
+import { Button, PageInfo, ToggleBar } from '../widgets';
+import { formatDate } from '../utilities';
 
 const DATA_TYPES_DISPLAYED = ['Transaction', 'TransactionLine', 'Item', 'ItemLine'];
 
@@ -15,12 +21,14 @@ export class CustomerInvoicePage extends GenericTablePage {
   constructor(props) {
     super(props);
     this.state.sortBy = 'itemName';
+    this.state.isUsingMasterList = this.props.transaction.useMasterList;
     this.columns = COLUMNS;
     this.dataTypesDisplayed = DATA_TYPES_DISPLAYED;
     this.databaseListenerId = null;
     this.getUpdatedData = this.getUpdatedData.bind(this);
     this.onEndEditing = this.onEndEditing.bind(this);
     this.onDatabaseEvent = this.onDatabaseEvent.bind(this);
+    this.renderPageInfo = this.renderPageInfo.bind(this);
   }
 
   /**
@@ -61,6 +69,44 @@ export class CustomerInvoicePage extends GenericTablePage {
     });
   }
 
+  toggleMasterList() {
+    this.setState({ isUsingMasterList: !this.state.isUsingMasterList });
+  }
+
+  renderPageInfo() {
+    const infoColumns = [
+      [
+        {
+          title: 'Entry Date:',
+          info: formatDate(this.props.transaction.entryDate),
+        },
+        {
+          title: 'Confirm Date:',
+          info: formatDate(this.props.transaction.confirmDate),
+        },
+        {
+          title: 'Entered By:',
+          info: this.props.transaction.enteredBy && this.props.transaction.enteredBy.username,
+        },
+      ],
+      [
+        {
+          title: 'Customer:',
+          info: this.props.transaction.otherParty && this.props.transaction.otherParty.name,
+        },
+        {
+          title: 'Their Ref:',
+          info: this.props.transaction.theirRef,
+        },
+        {
+          title: 'Comment:',
+          info: this.props.transaction.comment,
+        },
+      ],
+    ];
+    return <PageInfo columns={infoColumns} />;
+  }
+
   renderCell(key, transactionItem) {
     switch (key) {
       default:
@@ -77,10 +123,49 @@ export class CustomerInvoicePage extends GenericTablePage {
         };
     }
   }
+
+  render() {
+    return (
+      <View style={globalStyles.pageContentContainer}>
+        <View style={globalStyles.container}>
+          <View style={globalStyles.pageTopSectionContainer}>
+            <View style={globalStyles.verticalContainer}>
+              {this.renderPageInfo()}
+              {this.renderSearchBar()}
+            </View>
+            <View style={globalStyles.verticalContainer}>
+              <Button
+                style={globalStyles.button}
+                textStyle={globalStyles.buttonText}
+                text="New Item"
+                onPress={this.onNewInvoice}
+              />
+              <ToggleBar
+                style={globalStyles.toggleBar}
+                textOffStyle={globalStyles.toggleText}
+                textOnStyle={globalStyles.toggleTextSelected}
+                toggleOffStyle={globalStyles.toggleOption}
+                toggleOnStyle={globalStyles.toggleOptionSelected}
+                toggles={[
+                  {
+                    text: 'Use Master List',
+                    onPress: () => this.toggleMasterList(),
+                    isOn: this.state.isUsingMasterList,
+                  },
+                ]}
+              />
+            </View>
+          </View>
+          {this.renderDataTable()}
+        </View>
+      </View>
+    );
+  }
 }
 
 CustomerInvoicePage.propTypes = {
   database: React.PropTypes.object,
+  transaction: React.PropTypes.object,
 };
 
 const COLUMNS = [
