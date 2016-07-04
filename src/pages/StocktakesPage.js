c51f39154f898a62eb15bcc18f3fa02fbca73d07
Christopher Petty <christopher.m.petty@gmail.com>
2016-07-04 12:05:21 +1200

    StocktakesPage refactored

--- src/pages/StocktakesPage.js
+++ src/pages/StocktakesPage.js
@@ -5,78 +5,43 @@
  * Sustainable Solutions (NZ) Ltd. 2016
  */
 
-
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
-import {
-  Cell,
-  DataTable,
-  Header,
-  HeaderCell,
-  Row,
-} from '../widgets/DataTable';
-
-import Icon from 'react-native-vector-icons/Ionicons';
+import { SearchBar, Button, BottomConfirmModal, ToggleBar } from '../widgets';
 import { generateUUID } from '../database';
-import { ListView } from 'realm/react-native';
-import { Button, BottomConfirmModal, ToggleBar } from '../widgets';
-import globalStyles, { SUSSOL_ORANGE } from '../globalStyles';
-const SORT_BY = 'createdDate'
+import globalStyles from '../globalStyles';
+import { GenericTablePage } from './GenericTablePage';
+
+const DATA_TYPES_DISPLAYED = ['StockTake'];
+
 /**
-* Renders the page for displaying Stocktakes.
-* @prop   {Realm}               database        App wide database.
-* @prop   {func}                navigateTo      callback for navigation stack.
-* @const  {string}              SORT_BY          Locked to createdDate via created date column.
-* @state  {ListView.DataSource} dataSource      DataTable input, used to update rows being rendered.
-* @state  {Realm.Results}       Stocktakes      Results object containing all objects in the
-*                                               Database of type Stocktake.
-* @state  {boolean}             isAscending     Direction SORT_BY should sort.
-*                                               (ascending/descending:true/false).
-* @state  {boolean}             showCurrent     Boolean control of toggle bar options.
-* @state  {array}               deleteSelection Stores id of the stocktakes selected for deletion.
+* Renders the page for displaying Customers.
+* @prop   {Realm}               database      App wide database.
+* @prop   {func}                navigateTo    CallBack for navigation stack.
+* @state  {Realm.Results}       transactions  Filtered to have only supplier_invoice.
 */
-export class StocktakesPage extends React.Component {
+export class StocktakesPage extends GenericTablePage {
   constructor(props) {
     super(props);
-    const dataSource = new ListView.DataSource({
-      rowHasChanged: (row1, row2) => row1 !== row2,
-    });
-    this.state = {
-      dataSource: dataSource,
-      stocktakes: props.database.objects('Stocktake'),
-      isAscending: false,
-      showCurrent: true,
-      deleteSelection: [],
-    };
+    this.state.sortBy = 'name';
+    this.state.showCurrent = true;
+    this.state.stocktakes = props.database.objects('Stocktake');
+    this.columns = COLUMNS;
+    this.dataTypesDisplayed = DATA_TYPES_DISPLAYED;
+    this.getUpdatedData = this.getUpdatedData.bind(this);
+    this.onRowPress = this.onRowPress.bind(this);
+    this.onNewStockTake = this.onNewStockTake.bind(this);
     this.onDeleteConfirm = this.onDeleteConfirm.bind(this);
     this.onDeleteCancel = this.onDeleteCancel.bind(this);
-    this.onDeleteButtonPress = this.onDeleteButtonPress.bind(this);
-    this.onColumnSort = this.onColumnSort.bind(this);
-    this.renderHeader = this.renderHeader.bind(this);
-    this.renderRow = this.renderRow.bind(this);
-    this.refreshData = this.refreshData.bind(this);
-    this.onNewStockTake = this.onNewStockTake.bind(this);
-  }
-
-  componentWillMount() {
-    this.refreshData();
-  }
-
-  onColumnSort() {
-    this.setState({ isAscending: !this.state.isAscending });
-    this.refreshData();
+    this.onToggleStatusFilter = this.onToggleStatusFilter.bind(this);
   }
 
   onNewStockTake() {
     this.props.database.write(() => {
       this.props.database.create('Stocktake', {
         id: generateUUID(),
-        name: 'best stocktake',
+        name: Math.random().toString(),
         createdDate: new Date(),
         status: 'new',
         comment: 'Testing StocktakesPage',
@@ -87,34 +52,29 @@ export class StocktakesPage extends React.Component {
     this.props.navigateTo('stocktakeManager', 'New StockTake');
   }
 
-  /**
-   * Takes an array of stocktakes and deletes them from database
-   */
+  onRowPress(stocktake) {
+    this.props.navigateTo(
+      'stocktakeManager',
+      `${stocktake.name}`,
+      { stocktake: stocktake },
+    );
+  }
+
   onDeleteConfirm() {
-    const { stocktakes, deleteSelection } = this.state;
+    const { stocktakes, selection } = this.state;
     const { database } = this.props;
     database.write(() => {
-      for (let i = 0; i < deleteSelection.length; i++) {
-        const stocktake = stocktakes.filtered('id == $0', deleteSelection[i])[0];
+      for (let i = 0; i < selection.length; i++) {
+        const stocktake = stocktakes.find(s => s.id === selection[i]);
         if (stocktake.isValid()) database.delete('Stocktake', stocktake);
       }
     });
-    this.setState({ deleteSelection: [] });
+    this.setState({ selection: [] });
     this.refreshData();
   }
 
   onDeleteCancel() {
-    this.setState({ deleteSelection: [] });
-    this.refreshData();
-  }
-
-  onDeleteButtonPress(stocktake) {
-    const { deleteSelection } = this.state;
-    if (deleteSelection.indexOf(stocktake.id) >= 0) {
-      deleteSelection.splice(deleteSelection.indexOf(stocktake.id), 1);
-    } else {
-      deleteSelection.push(stocktake.id);
-    }
+    this.setState({ selection: [] });
     this.refreshData();
   }
 
@@ -126,100 +86,40 @@ export class StocktakesPage extends React.Component {
   }
 
   /**
-   * Updates data within dataSource in state according to SORT_BY and
-   * isAscending. SortBy is const set to 'createdDate'.
+   * Returns updated data according to searchTerm, sortBy and isAscending.
    */
-  refreshData() {
-    const { stocktakes, showCurrent, dataSource, isAscending } = this.state;
+  getUpdatedData(searchTerm, sortBy, isAscending) {
+    const { stocktakes, showCurrent } = this.state;
     const toggleFilter = showCurrent ? 'status == "new"' : 'status != "new"';
     const data = stocktakes
                   .filtered(toggleFilter)
-                  .sorted(SORT_BY, !isAscending); // 2nd arg: reverse sort order if true
-    this.setState({ dataSource: dataSource.cloneWithRows(data) });
+                  .sorted(sortBy, !isAscending); // 2nd arg: reverse sort order if true
+    return data;
   }
 
-  renderHeader() {
-    return (
-      <Header style={globalStyles.dataTableHeader}>
-        <HeaderCell
-          style={globalStyles.dataTableHeaderCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[0]}
-          text={'DESCRIPTION'}
-        />
-        <HeaderCell
-          style={globalStyles.dataTableHeaderCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[1]}
-          onPress={() => this.onColumnSort()}
-          isAscending={this.state.isAscending}
-          isSelected={true}
-          text={'CREATED DATE'}
-        />
-        <HeaderCell
-          style={globalStyles.dataTableHeaderCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[2]}
-          text={'STATUS'}
-        />
-        <HeaderCell
-          style={[
-            globalStyles.dataTableHeaderCell,
-            localStyles.rightMostCell,
-            localStyles.deleteCell,
-          ]}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[3]}
-          text={'DELETE'}
-        />
-      </Header>
-    );
+  renderCell(key, stocktake) {
+    switch (key) {
+      default:
+      case 'name':
+        return stocktake.name;
+      case 'createdDate':
+        return stocktake.createdDate.toDateString();
+      case 'status':
+        return stocktake.status;
+      case 'selected':
+        return {
+          type: 'checkable',
+          icon: 'md-remove-circle',
+        };
+    }
   }
 
-  renderRow(stocktake) {
-    const deleteColor = this.state.deleteSelection.indexOf(stocktake.id) >= 0 ?
-                                          SUSSOL_ORANGE :
-                                          'grey';
+  renderSearchBar() {
     return (
-      <Row
-        style={globalStyles.dataTableRow}
-        onPress={() => this.props.navigateTo('stocktakeManager', 'Create Stocktake')}
-      >
-        <Cell
-          style={globalStyles.dataTableCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[0]}
-        >
-          {stocktake.comment}
-        </Cell>
-        <Cell
-          style={globalStyles.dataTableCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[1]}
-        >
-          {stocktake.createdDate.toDateString()}
-        </Cell>
-        <Cell
-          style={globalStyles.dataTableCell}
-          textStyle={globalStyles.dataTableText}
-          width={COLUMN_WIDTHS[2]}
-        >
-          {stocktake.status}
-        </Cell>
-        <TouchableOpacity
-          style={{ flex: COLUMN_WIDTHS[3] }}
-          onPress={() => this.onDeleteButtonPress(stocktake)}
-        >
-          <Cell
-            style={[globalStyles.dataTableCell, localStyles.rightMostCell, localStyles.deleteCell]}
-            textStyle={globalStyles.dataTableText}
-            width={COLUMN_WIDTHS[3]}
-          >
-            <Icon name="md-remove-circle" size={15} color={deleteColor} />
-          </Cell>
-        </TouchableOpacity>
-      </Row>
-    );
+      <SearchBar
+        onChange={(event) => this.onSearchChange(event)}
+        keyboardType="numeric"
+      />);
   }
 
   render() {
@@ -256,21 +156,15 @@ export class StocktakesPage extends React.Component {
               />
             </View>
           </View>
-          <DataTable
-            style={globalStyles.dataTable}
-            listViewStyle={localStyles.listView}
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRow}
-            renderHeader={this.renderHeader}
-          />
-          <BottomConfirmModal
-            isOpen={this.state.deleteSelection.length > 0}
-            questionText="Are you sure you want to delete these stocktakes?"
-            onCancel={() => this.onDeleteCancel()}
-            onConfirm={() => this.onDeleteConfirm()}
-            confirmText='Delete'
-          />
+          {this.renderDataTable()}
         </View>
+        <BottomConfirmModal
+          isOpen={this.state.selection.length > 0}
+          questionText="Are you sure you want to delete these stocktakes?"
+          onCancel={() => this.onDeleteCancel()}
+          onConfirm={() => this.onDeleteConfirm()}
+          confirmText="Delete"
+        />
       </View>
     );
   }
@@ -280,22 +174,34 @@ StocktakesPage.propTypes = {
   database: React.PropTypes.object,
   navigateTo: React.PropTypes.func.isRequired,
 };
-const COLUMN_WIDTHS = [6, 2, 2, 1];
-const localStyles = StyleSheet.create({
-  listView: {
-    flex: 1,
+
+const COLUMNS = [
+  {
+    key: 'name',
+    width: 6,
+    title: 'NAME',
   },
+  {
+    key: 'createdDate',
+    width: 2,
+    title: 'CREATED DATE',
+    sortable: true,
+  },
+  {
+    key: 'status',
+    width: 2,
+    title: 'STATUS',
+  },
+  {
+    key: 'selected',
+    width: 1,
+    title: 'DELETE',
+  },
+];
+
+const localStyles = StyleSheet.create({
   buttonViewTop: {
     justifyContent: 'flex-end',
     alignItems: 'center',
-  },
-  deleteCell: {
-    alignItems: 'center',
-  },
-  rightMostCell: {
-    borderRightWidth: 0,
-  },
-  dataTable: {
-    flex: 1,
   },
 });
