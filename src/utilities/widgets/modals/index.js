29fed8544db509ff45583f94a3a47ee4b7bc6165
Christopher Petty <christopher.m.petty@gmail.com>
2016-07-05 10:19:47 +1200

    Widgets please go home

--- src/utilities/widgets/modals/index.js
+++
@@ -1,5 +0,0 @@
-export { ConfirmModal } from './ConfirmModal';
-export { FinaliseModal } from './FinaliseModal';
-export { LoginModal } from './LoginModal';
-export { BottomModal } from './BottomModal';
-export { BottomConfirmModal } from './BottomConfirmModal';
