8fb1f170198959477dc73a3325c67b9fe0c784c2
Edwin <edwin@sussol.net>
2016-07-13 14:45:12 +1200

    Supply original core database to sync, filtering database to UI

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
-export { Database } from './Database';
+export { createRecord } from './createRecord';
 export { generateUUID } from './utilities';
 export { schema } from './schema';
-export { createRecord } from './createRecord';
+export { Database } from './Database';
+export { UIDatabase } from './UIDatabase';
 
 export const CHANGE_TYPES = {
   CREATE: 'create',
