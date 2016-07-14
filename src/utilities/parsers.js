83fcd5e04f9da798728578968531187f3d734142
Edwin <edwin@sussol.net>
2016-07-14 17:39:36 +1200

    Invalid number strings in, e.g., customer invoice page, become 0

--- src/utilities/parsers.js
+++ src/utilities/parsers.js
@@ -1,8 +1,6 @@
 export function parsePositiveNumber(numberString) {
   if (!numberString || numberString.length < 1) return null;
-  try {
-    return Math.max(parseFloat(numberString), 0); // Negative numbers become 0
-  } catch (error) {
-    throw error;
-  }
+  const number = parseFloat(numberString);
+  if (isNaN(number)) return 0; // Invalid strings become 0
+  return Math.max(number, 0); // Negative numbers become 0
 }
