17bde0f07143424e539f8a0c034c1bff76b54732
Christopher Petty <christopher.m.petty@gmail.com>
2016-07-26 16:44:48 +0930

    Fixes spamming create new invoice, structure for changing appearance when running a function

--- src/widgets/Button.js
+++ src/widgets/Button.js
@@ -6,25 +6,47 @@ import {
   View,
 } from 'react-native';
 
-export function Button(props) {
-  if (props.isDisabled) {
+export class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+    };
+  }
+
+  async onPress() {
+    // By definition, the second argument should be called after rerender (doesn't appear to)
+    await this.setState({ isLoading: true }, this.props.onPress);
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    const props = this.props;
+    if (props.isDisabled) {
+      return (
+        <View style={[props.style, { backgroundColor: props.disabledColor }]}>
+          <Text style={props.textStyle}>{props.text}</Text>
+        </View>
+      );
+    }
+    if (this.state.isLoading) {
+      return (
+        <View style={[props.style, { backgroundColor: props.disabledColor }]}>
+          <Text style={props.textStyle}>Working</Text>
+        </View>
+      );
+    }
+
     return (
-      <View style={[props.style, { backgroundColor: props.disabledColor }]}>
+      <TouchableHighlight
+        style={props.style}
+        underlayColor="#B5B5B5"
+        onPress={() => { this.onPress(); }}
+      >
         <Text style={props.textStyle}>{props.text}</Text>
-      </View>
+      </TouchableHighlight>
     );
   }
-  return (
-    <TouchableHighlight
-      style={props.style}
-      underlayColor="#B5B5B5"
-      onPress={() => {
-        props.onPress();
-      }}
-    >
-      <Text style={props.textStyle}>{props.text}</Text>
-    </TouchableHighlight>
-  );
 }
 
 Button.propTypes = {
