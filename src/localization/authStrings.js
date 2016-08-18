69395baf484ae45bbaf2f9d1649c31e6633846a6
Christopher Petty <christopher.m.petty@gmail.com>
2016-08-18 17:40:16 +1200

    The culling of ' (en)'. Can revert this commit if testing, is useful.

--- src/localization/authStrings.js
+++ src/localization/authStrings.js
@@ -10,10 +10,10 @@ const CURRENT_LANGUAGE = 'tetum'; // settings not set up for this yet
 
 const strings = {
   en: {
-    logging_in: 'Logging in... (en)',
-    login: 'Login (en)',
-    password: 'Password (en)',
-    user_name: 'User Name (en)',
+    logging_in: 'Logging in...',
+    login: 'Login',
+    password: 'Password',
+    user_name: 'User Name',
   },
   tetum: {
     logging_in: 'Logging in... (t)',
--- src/localization/pageInfoStrings.js
+++ src/localization/pageInfoStrings.js
@@ -10,15 +10,15 @@ const CURRENT_LANGUAGE = 'en'; // settings not set up for this yet
 
 const strings = {
   en: {
-    address: 'Address (en)',
-    code: 'Code (en)',
-    comment: 'Comment (en)',
-    confirm_date: 'Confirm Date (en)',
-    customer: 'Customer (en)',
-    entered_by: 'Entered By (en)',
-    entry_date: 'Entry Date (en)',
-    months_stock_required: 'Months Stock Required (en)',
-    their_ref: 'Their Ref (en)',
+    address: 'Address',
+    code: 'Code',
+    comment: 'Comment',
+    confirm_date: 'Confirm Date',
+    customer: 'Customer',
+    entered_by: 'Entered By',
+    entry_date: 'Entry Date',
+    months_stock_required: 'Months Stock Required',
+    their_ref: 'Their Ref',
   },
   tetum: {
   },
