e14b2a6751b146faf72e51cd63e70d40532f7b17
Andrei <andrei@andreis-MacBook-Pro.local>
2017-04-05 14:48:41 +1200

    Changes to add to sussol-mobil
    Will add functions to parsers and date formatter in sussol-utils node
    module

--- src/utilities/append-to-sussol-utilities.js
+++ src/utilities/append-to-sussol-utilities.js
@@ -1,6 +1,37 @@
-export function parsePositiveDouble(numberString) {
+export function parsePositiveFloat(numberString) {
   if (!numberString || numberString.length < 1) return null;
-  var number = parseFloat(numberString);
+  const number = parseFloat(numberString);
   if (isNaN(number)) return 0; // Invalid strings become 0
   return number; // Negative numbers become 0
+}
+export function formatExpiryDate(date) {
+  if (!date) return '';
+
+  let month = (date.getMonth() + 1).toString();
+  month = month.length === 1 ? `0${month}` : month;
+  return `${month}/${date.getFullYear()}`;
+}
+export function parseExpiryDate(date) {
+  if (!date) return null;
+
+  const parts = date.split('/');
+  if (parts.length !== 2) return null;
+
+  const month = parseInt(parts[0], 10);
+  if (isNaN(month) || month < 1 || month > 12) return null;
+
+  if (parts[1].length > 4 || parts[1].length === 0) return null;
+  const year = parseInt(formatToFullYear(parts[1]), 10);
+  if (isNaN(year)) return null;
+  // Date object takes months starting with zero, our month will be + 1
+  // So next month, day zero is previous month last day
+  // for 12th month, will parse to 0 day of next year which will be last day
+  // of previous year
+  const returnDate = new Date();
+  returnDate.setFullYear(year, month, 0);
+  return returnDate;
+}
+function formatToFullYear(yearString) {
+  const addOn = '200'.substring(0, 4 - yearString.length);
+  return `${addOn}${yearString}`;
 }
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -4,6 +4,7 @@ export {
   parsePositiveInteger,
   truncateString,
 } from 'sussol-utilities';
-export { parsePositiveDouble } from './append-to-sussol-utilities';
+export {
+  parsePositiveFloat, formatExpiryDate, parseExpiryDate } from './append-to-sussol-utilities';
 export { formatStatus } from './formatStatus';
 export { sortDataBy } from './sortDataBy';
