492f91073dad349cedac64a3d141827f8a08b43a
Andrei <andrei@andreis-MacBook-Pro.local>
2017-05-29 21:47:48 +1200

    Remove unused

--- src/database/utilities/index.js
+++ src/database/utilities/index.js
@@ -2,7 +2,5 @@ export { createRecord } from './createRecord';
 export { getNextNumber, reuseNumber } from './numberSequenceUtilities';
 export {
   getTotal,
-  addBatchToParent,
-  removeTransactionBatchUtil,
-  classAsJson,
+  addBatchToParent
  } from './utilities';
