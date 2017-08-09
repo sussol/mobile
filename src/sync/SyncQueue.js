c67fbe1f5bb48969d3a5574fb0543408c3e27b76
Christopher Petty <christopher.m.petty@gmail.com>
2017-08-09 14:57:41 +1200

    Remove redundant code
    Now should only be one SyncOut record for every record.id anyway, it
    just gets updated to current time and type delete as per last commit.

--- src/sync/SyncQueue.js
+++ src/sync/SyncQueue.js
@@ -52,14 +52,6 @@ export class SyncQueue {
   onDatabaseEvent(changeType, recordType, record, causedBy) {
     if (causedBy === 'sync') return; // Don't re-sync any changes caused by a sync
     if (recordTypesSynced.indexOf(recordType) >= 0) {
-      // If a delete, first remove any sync out records that already have the id,
-      // so that sync doesn't try to refer to them next time it does a push
-      if (changeType === DELETE) {
-        const recordsToDelete = this.database
-          .objects('SyncOut')
-          .filtered('recordId == $0', record.id);
-        this.database.delete('SyncOut', recordsToDelete);
-      }
       switch (changeType) {
         case CREATE:
         case UPDATE:
