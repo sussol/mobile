2a2284aff385fb2dadde4f93e58c6cabaec4d87d
nmadruga <nicole@sussol.net>
2017-09-18 10:11:52 +1200

    Translations for Kiribati on version 2

--- src/localization/syncStrings.js
+++ src/localization/syncStrings.js
@@ -8,22 +8,23 @@ import LocalizedStrings from 'react-native-localization';
 export const syncStrings = new LocalizedStrings({
   gb: {
     last_sync: 'LAST SYNC',
-    sync_enabled: 'SYNC ENABLED',
-    sync_error: 'SYNC ERROR',
-    sync_in_progress: 'SYNC IN PROGRESS',
     manual_sync: 'Manual Sync',
-  },
-  gil: { // TODO add manual_sync
-    last_sync: 'LAST SYNC',
     sync_enabled: 'SYNC ENABLED',
     sync_error: 'SYNC ERROR',
     sync_in_progress: 'SYNC IN PROGRESS',
   },
-  tl: { // TODO add manual_sync
+  gil: {
+    last_sync: 'LAST SYNC',
+    manual_sync: 'kamatoaa bwa e rin\nte bwai are ko karaoia',
+    sync_enabled: 'SYNC ENABLED',
+    sync_error: 'SYNC ERROR',
+    sync_in_progress: 'SYNC IN PROGRESS',
+  },
+  tl: {
     last_sync: 'SYNC FOIN DADAUN',
+    manual_sync: 'Synk Manual',
     sync_enabled: 'HAMORIS SYNC',
     sync_error: 'SYNC SALA',
     sync_in_progress: "SYNC LA'O HELA",
-    manual_sync: 'Synk Manual',
   },
 });
