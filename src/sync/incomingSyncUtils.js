eb8bb04b648dbe6e433903eb7a392ca17d7a5b97
Christopher Petty <christopher.m.petty@gmail.com>
2018-03-16 14:16:31 +1300

    Working!

--- src/sync/incomingSyncUtils.js
+++ src/sync/incomingSyncUtils.js
@@ -71,7 +71,7 @@ export function createOrUpdateRecord(database, settings, recordType, record) {
         department: database.getOrCreate('ItemDepartment', record.department_ID),
         description: record.description,
         name: record.item_name,
-        crossReferenceItem: database.getOrCreate('Item', record.cross_ref_ID),
+        crossReferenceItem: database.getOrCreate('Item', record.cross_ref_item_ID),
       };
       console.log('====================================');
       console.log(record);
