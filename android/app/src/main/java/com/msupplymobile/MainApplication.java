d09718564ed30ed3a7f68bb7880ded2ac2b8e2fa
Christopher Petty <christopher.m.petty@gmail.com>
2018-07-05 11:46:34 +1200

    indentation

--- android/app/src/main/java/com/msupplymobile/MainApplication.java
+++ android/app/src/main/java/com/msupplymobile/MainApplication.java
@@ -32,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFSPackage(),
+          new RNFSPackage(),
           new RealmReactPackage(),
           new VectorIconsPackage(),
           new ReactNativeLocalizationPackage(),
