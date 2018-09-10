4f90f9b09b4b96c8343ba9bfaef5735951a23209
Sworup Shakya <sworup@sussol.net>
2018-09-10 13:06:50 +0545

    Add validation and change API route
    - Mantis Issue 9040
    - Await json response
    - Change API route for request demo (needs to be replaced)
    - Validate password length and if password contains spaces

--- src/authentication/DemoSiteRequest.js
+++ src/authentication/DemoSiteRequest.js
@@ -12,12 +12,13 @@ export class DemoSiteRequest {
     if (password.length === 0) throw new Error('Enter the password');
     if (repeatPassword.length === 0) throw new Error('Enter the repeat password');
     if (password !== repeatPassword) throw new Error('Password & repeat password must match');
-
+    if (password.length < 8 || password.length > 32) throw new Error('Password must be 8-32 characters long');
+    if (!this.passwordCheck(password)) throw new Error('Password cannot contain spaces.');
     // Hash the password
     const passwordHash = hashPassword(password);
 
     // TODO: Need proper URL in 4D to work
-    const URL = 'http://localhost:8080/v4/mobile/requestDemo';
+    const URL = 'http://192.168.3.145:7848/api/v4/requestDemo';
     let responseJson;
     try {
       const response = await fetch(URL, {
@@ -28,7 +29,7 @@ export class DemoSiteRequest {
           password: passwordHash,
         }),
       });
-      responseJson = response.json();
+      responseJson = await response.json();
 
       if (responseJson.error) {
         throw new Error(responseJson.error);
@@ -42,6 +43,11 @@ export class DemoSiteRequest {
 
   validateEmail(text) {
     const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return reg.test(text);
+  }
+
+  passwordCheck(text) {
+    const reg = /^\S*$/;
     return reg.test(text);
   }
 }
