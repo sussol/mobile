7570fe08007f48a132741693ee6e8dd2fe584256
Josh <josh@sussol.net>
2018-12-06 12:34:03 +1300

    Add Dashboard Page initial

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 
 export class DashboardPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: null,
+      reports: props.database
+        .objects('Name')
+        .snapshot()
+        .map(name => {
+          return { key: name.id, data: name.name };
+        }),
+      isDisplayingChart: false,
+    };
   }
 
   render() {
     return (
       <View>
-        <Text>Dashboard goes here.</Text>
+        <FlatList data={this.state.reports} renderItem={({ item }) => <Text>{item.data}</Text>} />
       </View>
     );
   }
