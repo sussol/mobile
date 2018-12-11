09c6fe77b3aceacfb26e0e1fe51058ab5cc702ab
wlthomson <will.tho@outlook.com>
2018-12-11 15:22:39 +1300

    Update BarChart, LineChart styles

--- src/widgets/ReportChart.js
+++ src/widgets/ReportChart.js
@@ -21,17 +21,25 @@ export class ReportChart extends React.Component {
     switch (this.state.type) {
       case 'BarChart':
         return (
-          <VictoryChart width={this.state.width} height={this.state.height} domainPadding={50}>
-            <VictoryBar style={victoryStyles.BarChart} data={this.state.data} />
+          <VictoryChart
+            width={this.state.width}
+            height={this.state.height}
+            padding={victoryStyles.BarChart.padding}
+            domainPadding={victoryStyles.BarChart.domainPadding}
+          >
+            <VictoryBar style={{ data: victoryStyles.BarChart.data }} data={this.state.data} />
             <VictoryAxis
+              fixLabelOverlap={victoryStyles.BarChart.fixLabelOverlap}
               style={{
                 axis: { stroke: SUSSOL_ORANGE },
                 ticks: { stroke: SUSSOL_ORANGE },
                 tickLabels: { stroke: SUSSOL_ORANGE },
+                fixLabelOverlap: victoryStyles.BarChart.fixLabelOverlap,
               }}
             />
             <VictoryAxis
               dependentAxis
+              fixLabelOverlap={victoryStyles.BarChart.fixLabelOverlap}
               style={{
                 axis: { stroke: SUSSOL_ORANGE },
                 ticks: { stroke: SUSSOL_ORANGE },
@@ -55,17 +63,25 @@ export class ReportChart extends React.Component {
         );
       case 'LineChart':
         return (
-          <VictoryChart width={this.state.width} height={this.state.height} domainPadding={50}>
-            <VictoryLine style={victoryStyles.LineChart} data={this.state.data} />
+          <VictoryChart
+            width={this.state.width}
+            height={this.state.height}
+            padding={victoryStyles.LineChart.padding}
+            domainPadding={victoryStyles.LineChart.domainPadding}
+          >
+            <VictoryLine style={{ data: victoryStyles.LineChart.data }} data={this.state.data} />
             <VictoryAxis
+              fixLabelOverlap={victoryStyles.LineChart.fixLabelOverlap}
               style={{
                 axis: { stroke: SUSSOL_ORANGE },
                 ticks: { stroke: SUSSOL_ORANGE },
                 tickLabels: { stroke: SUSSOL_ORANGE },
+                fixLabelOverlap: victoryStyles.LineChart.fixLabelOverlap,
               }}
             />
             <VictoryAxis
               dependentAxis
+              fixLabelOverlap={victoryStyles.LineChart.fixLabelOverlap}
               style={{
                 axis: { stroke: SUSSOL_ORANGE },
                 ticks: { stroke: SUSSOL_ORANGE },
@@ -90,7 +106,17 @@ export class ReportChart extends React.Component {
 }
 
 const victoryStyles = {
-  BarChart: { data: { fill: SUSSOL_ORANGE } },
-  LineChart: { data: { stroke: SUSSOL_ORANGE } },
+  BarChart: {
+    padding: { top: 75, bottom: 75, left: 90, right: 60 },
+    domainPadding: 50,
+    fixLabelOverlap: true,
+    data: { fill: SUSSOL_ORANGE },
+  },
+  LineChart: {
+    padding: { top: 75, bottom: 75, left: 90, right: 60 },
+    domainPadding: 50,
+    fixLabelOverlap: true,
+    data: { stroke: SUSSOL_ORANGE },
+  },
   PieChart: { labelRadius: 325, innerRadius: 200, padAngle: 2.5, padding: 75, colorScale: 'warm' },
 };
