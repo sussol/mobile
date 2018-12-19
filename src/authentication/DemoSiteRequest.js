08a46de45f6c05242572f932beb90f257fb8fdd7
Sworup Shakya <sworup@sussol.net>
2018-12-19 16:34:21 +0545

    Extract fields validation calls to a separate method

--- src/authentication/DemoSiteRequest.js
+++ src/authentication/DemoSiteRequest.js
@@ -7,6 +7,24 @@ import { validationStrings } from '../localization';
 const DEMO_SITE_URL = 'https://demo.msupply.org/api/v4/mobile/requestDemo';
 export class DemoSiteRequest {
   async createActivationURL(username, email, password, repeatPassword) {
+    this.validateFields(username, email, password, repeatPassword);
+
+    let responseJson;
+    try {
+      const response = await fetch(DEMO_SITE_URL, {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+      });
+      responseJson = await response.json();
+      if (responseJson.error) throw new Error(responseJson.error);
+      return responseJson;
+    } catch (error) {
+      // Pass error up
+      throw error;
+    }
+  }
+
+  validateFields(username, email, password, repeatPassword) {
     // Client side validation
     if (email.length === 0) throw new Error(validationStrings.email.required);
     if (!this.validateEmail(email)) throw new Error(validationStrings.email.valid);
@@ -17,23 +35,6 @@ export class DemoSiteRequest {
     if (this.textLengthInvalid(password)) throw new Error(validationStrings.password.lengthInvalid);
     if (this.textContainsSpaces(password)) {
       throw new Error(validationStrings.password.containsSpaces);
-    }
-
-    let responseJson;
-    try {
-      const response = await fetch(DEMO_SITE_URL, {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password }),
-      });
-      responseJson = await response.json();
-
-      if (responseJson.error) {
-        throw new Error(responseJson.error);
-      }
-      return responseJson;
-    } catch (error) {
-      // Pass error up
-      throw error;
     }
   }
 
