64bb585efe4cc76abcda32d676cbc9ac9e44c781
joshxg <griffinjoshua5@gmail.com>
2019-03-20 12:05:08 +1300

    Add createInternalRecord helper method

---
+++ src/sync/createInternalRecord.js
@@ -0,0 +1,32 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+/**
+ * Methods to create internal records from a requested
+ * sync/external record. Used primarily by
+ * createOrUpdateRecord in incomingSyncUtils.
+ */
+
+import { parseBoolean } from './incomingSyncUtils';
+
+export const createPeriodInternalRecord = (record, database) => ({
+  id: record.ID,
+  startDate: record.startDate,
+  endDate: record.endDate,
+  name: record.name,
+  periodSchedule: database.getOrCreate('PeriodSchedule', record.periodScheduleID),
+});
+
+export const createOptionsInternalRecord = record => ({
+  id: record.ID,
+  name: record.name,
+  type: record.type,
+  isActive: parseBoolean(record.isActive),
+});
+
+export const createPeriodScheduleInternalRecord = record => ({
+  id: record.ID,
+  name: record.name,
+});
