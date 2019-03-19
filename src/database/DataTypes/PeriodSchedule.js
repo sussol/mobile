61172d8041aa41bef0b01aa747d611b29781cdb0
joshxg <griffinjoshua5@gmail.com>
2019-03-20 11:25:26 +1300

    Add PeriodSchedule schema

---
+++ src/database/DataTypes/PeriodSchedule.js
@@ -0,0 +1,20 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import Realm from 'realm';
+
+export class PeriodSchedule extends Realm.Object {}
+
+export default PeriodSchedule;
+
+PeriodSchedule.schema = {
+  name: 'PeriodSchedule',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    name: { type: 'string', default: 'Placeholder Name' },
+    periods: { type: 'list', objectType: 'Period', optional: 'true' },
+  },
+};
