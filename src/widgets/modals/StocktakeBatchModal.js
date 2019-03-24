c9f860def1b43eaa82249932e3f82fbcdd261044
joshxg <griffinjoshua5@gmail.com>
2019-03-24 21:30:39 +1300

    Add basic StockTakeBatchModal

---
+++ src/widgets/modals/StocktakeBatchModal.js
@@ -0,0 +1,274 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/forbid-prop-types */
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-native-modalbox';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { GenericPage } from '../../pages/GenericPage';
+
+import { tableStrings, buttonStrings } from '../../localization';
+import { parsePositiveInteger } from '../../utilities';
+import { Button, PageButton, ExpiryTextInput, PageInfo } from '..';
+import globalStyles, {
+  WARM_GREY,
+  SUSSOL_ORANGE,
+  dataTableStyles,
+  expansionPageStyles,
+} from '../../globalStyles';
+
+export default class StocktakeBatchModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onEndEditing = (key, stocktakeBatch, newValue) => {
+    const { database } = this.props;
+
+    database.write(() => {
+      switch (key) {
+        case 'countedTotalQuantity': {
+          const newCountedQuantity = parsePositiveInteger(newValue);
+          if (newCountedQuantity === null) return;
+
+          stocktakeBatch.countedTotalQuantity = parsePositiveInteger(newValue);
+          break;
+        }
+        case 'batch': {
+          if (!newValue || newValue === '' || newValue === `(${tableStrings.no_batch_name})`) {
+            stocktakeBatch.batch = '';
+          } else stocktakeBatch.batch = newValue;
+          break;
+        }
+        case 'expiryDate':
+          stocktakeBatch.expiryDate = newValue;
+          break;
+        default:
+          break;
+      }
+      database.save('StocktakeBatch', stocktakeBatch);
+    });
+  };
+
+  refreshData = () => {
+    const { stocktakeItem } = this.props;
+    this.setState({ data: stocktakeItem.batches });
+  };
+
+  renderCell = (key, stocktakeBatch) => {
+    const { stocktakeItem } = this.props;
+    const { stocktake } = stocktakeItem;
+    const isEditable = !stocktake.isFinalised;
+    switch (key) {
+      default:
+        return {
+          cellContents: stocktakeBatch[key],
+        };
+      case 'batch':
+        return {
+          type: isEditable ? 'editable' : 'text',
+          cellContents:
+            stocktakeBatch[key] && stocktakeBatch[key] !== ''
+              ? stocktakeBatch[key]
+              : `(${tableStrings.no_batch_name})`,
+          keyboardType: 'default',
+        };
+      case 'countedTotalQuantity': {
+        const emptyCellContents = isEditable ? '' : tableStrings.not_counted;
+        return {
+          type: isEditable ? 'editable' : 'text',
+          cellContents: stocktakeBatch.hasBeenCounted
+            ? stocktakeBatch.countedTotalQuantity
+            : emptyCellContents,
+          placeholder: tableStrings.not_counted,
+        };
+      }
+      case 'expiryDate': {
+        return (
+          <ExpiryTextInput
+            key={stocktakeBatch.id}
+            isEditable={isEditable}
+            onEndEditing={newValue => {
+              this.onEndEditing(key, stocktakeBatch, newValue);
+              this.refreshData();
+            }}
+            text={stocktakeBatch[key]}
+            style={dataTableStyles.text}
+          />
+        );
+      }
+      case 'difference': {
+        const { difference } = stocktakeBatch;
+        const prefix = difference > 0 ? '+' : '';
+        return { cellContents: `${prefix}${difference}` };
+      }
+      case 'reason': {
+        return (
+          <TouchableOpacity key={stocktakeBatch.id} onPress={this.onChangeReason}>
+            <Text>{stocktakeBatch.option ? stocktakeBatch.option.title : ''}</Text>
+            <Icon name="pencil" size={14} style={localStyles.editIcon} color={SUSSOL_ORANGE} />
+          </TouchableOpacity>
+        );
+      }
+    }
+  };
+
+  renderAddBatchButton = () => {
+    const { database, stocktakeItem } = this.props;
+
+    const addNewBatch = () => {
+      database.write(() => {
+        stocktakeItem.createNewBatch(database);
+      });
+      this.refreshData();
+    };
+
+    return (
+      <PageButton
+        text={buttonStrings.add_batch}
+        onPress={addNewBatch}
+        isDisabled={stocktakeItem.stocktake.isFinalised}
+        style={localStyles.addBatchButton}
+      />
+    );
+  };
+
+  renderPageInfo = () => {
+    const { stocktakeItem } = this.props;
+    const { itemName } = stocktakeItem;
+
+    const infoColumns = [
+      [
+        {
+          title: 'By Batch:',
+          info: itemName,
+        },
+      ],
+    ];
+    return <PageInfo columns={infoColumns} />;
+  };
+
+  renderFooter = () => {
+    const { onConfirm } = this.props;
+    return (
+      <View style={localStyles.footer}>
+        <Button
+          text="OK"
+          disabledColor={WARM_GREY}
+          style={[globalStyles.button, localStyles.OKButton]}
+          textStyle={[globalStyles.buttonText, localStyles.OKButtonText]}
+          onPress={onConfirm}
+        />
+      </View>
+    );
+  };
+
+  render() {
+    const { database, genericTablePageStyles, isOpen } = this.props;
+    const { data } = this.state;
+
+    return (
+      <Modal
+        isOpen={isOpen}
+        style={[localStyles.modal]}
+        backdropPressToClose={false}
+        backdropOpacity={0.88}
+        swipeToClose={false}
+        position="top"
+      >
+        <GenericPage
+          data={data}
+          renderCell={this.renderCell}
+          refreshData={this.refreshData}
+          hideSearchBar={true}
+          dontRenderSearchBar={true}
+          onEndEditing={this.onEndEditing}
+          renderTopLeftComponent={this.renderPageInfo}
+          renderTopRightComponent={this.renderAddBatchButton}
+          columns={[
+            {
+              key: 'batch',
+              width: 2,
+              title: 'BATCH NAME',
+              alignText: 'center',
+            },
+            {
+              key: 'expiryDate',
+              width: 1,
+              title: 'EXPIRY',
+              alignText: 'center',
+            },
+            {
+              key: 'snapshotTotalQuantity',
+              width: 1,
+              title: unwrapText(tableStrings.snapshot_quantity),
+              alignText: 'right',
+            },
+            {
+              key: 'countedTotalQuantity',
+              width: 1,
+              title: unwrapText(tableStrings.actual_quantity),
+              alignText: 'right',
+            },
+            {
+              key: 'difference',
+              width: 1,
+              title: tableStrings.difference,
+              alignText: 'right',
+            },
+            {
+              key: 'reason',
+              width: 1,
+              title: 'Reason',
+              alignText: 'right',
+            },
+          ]}
+          dataTypesLinked={['StocktakeBatch', 'Stocktake']}
+          database={database}
+          pageStyles={expansionPageStyles}
+          {...genericTablePageStyles}
+        />
+        {this.renderFooter()}
+      </Modal>
+    );
+  }
+}
+
+StocktakeBatchModal.propTypes = {
+  database: PropTypes.object,
+  genericTablePageStyles: PropTypes.object,
+  stocktakeItem: PropTypes.object.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+};
+
+const unwrapText = text => text.replace(/\n/g, ' ');
+const localStyles = StyleSheet.create({
+  addBatchButton: {
+    height: 30,
+    width: 90,
+  },
+  modal: {
+    height: '100%',
+    width: '100%',
+  },
+  footer: {
+    flex: 1,
+    maxHeight: 50,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  OKButton: {
+    backgroundColor: SUSSOL_ORANGE,
+  },
+  OKButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
