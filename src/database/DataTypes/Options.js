5eff88b97ac298594602cfeeb649dc403f2c8406
joshxg <griffinjoshua5@gmail.com>
2019-04-03 12:32:15 +1300

    Add update to options doc string

--- src/database/DataTypes/Options.js
+++ src/database/DataTypes/Options.js
@@ -8,10 +8,13 @@ import Realm from 'realm';
  * An Options object is a simple explanation of why a change or
  * value was used in a situation. Used for stock takes, to
  * provide a reason for having a stock take batch quantity differ
- * from the stock take batch snapshot quantity.
+ * from the stock take batch snapshot quantity. Types used in
+ * mSupply are stocktakeLineAdjustment, cashOutTransaction,
+ * requisitionLineVariance. Only stocktakeLineAdjustment
+ * types are accepted on incoming sync.
  * @property  {string}  id
  * @property  {string}  title     A generic name field
- * @property  {string}  type      The type of option. See below for types
+ * @property  {string}  type      The type of option
  * @property  {boolean} isActive  If the option is currently available to use
  */
 export class Options extends Realm.Object {}
