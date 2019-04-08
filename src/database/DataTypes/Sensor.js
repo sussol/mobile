6fe301784b61f1d34d7d532fc88534644d5d756f
andreievg <andrei.evguenov@gmail.com>
2019-04-08 16:37:49 +1200

    Updates to schema

---
+++ src/database/DataTypes/Sensor.js
@@ -0,0 +1,28 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import Realm from 'realm';
+
+export class Sensor extends Realm.Object {
+  // TODO
+  // Does this need to be a class ??
+}
+
+Sensor.schema = {
+  name: 'Sensor',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    location: { type: 'Location', optional: true },
+    name: { type: 'string', optional: true },
+    macAddress: { type: 'string', optional: true },
+    batteryLevel: { type: 'double', optional: true },
+    temperature: { type: 'double', optional: true },
+    lastConnectionTimestamp: { type: 'date', optional: true },
+    sensorLogs: { type: 'list', objectType: 'SensorLog' },
+  },
+};
+
+export default Sensor;
