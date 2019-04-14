091d7cb2e8cbd4f82a0c1d80825d3c8bd63189de
andreievg <andrei.evguenov@gmail.com>
2019-04-14 20:50:22 +1200

    all MenuPage changes

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -15,7 +15,47 @@ import { SETTINGS_KEYS } from '../settings';
 
 import globalStyles, { APP_FONT_FAMILY, SHADOW_BORDER, GREY, WARMER_GREY } from '../globalStyles';
 
-const { SYNC_SITE_NAME } = SETTINGS_KEYS;
+/* eslint-disable global-require */
+
+const { SYNC_SITE_NAME, THIS_STORE_CUSTOM_DATA } = SETTINGS_KEYS;
+
+const ORIGINAL_LAYOUT = [['customer', 'supplier', 'stock']];
+const VACCINE_MODULE_LAYOUT = [['customer', 'supplier'], ['stock', 'modules']];
+
+const SECTIONS = {
+  customer: {
+    icon: require('../images/menu_people.png'),
+    buttons: [
+      { title: navStrings.customer_invoices, page: 'customerInvoices' },
+      { title: navStrings.customer_requisitions, page: 'customerRequisitions' },
+    ],
+  },
+  supplier: {
+    icon: require('../images/menu_truck.png'),
+    buttons: [
+      { title: navStrings.supplier_invoices, page: 'supplierInvoices' },
+      { title: navStrings.supplier_requisitions, page: 'supplierRequisitions' },
+    ],
+  },
+  stock: {
+    icon: require('../images/menu_pc_clipboard.png'),
+    buttons: [
+      { title: navStrings.current_stock, page: 'stock' },
+      { title: navStrings.stocktakes, page: 'stocktakes' },
+    ],
+  },
+  modules: {
+    icon: require('../images/menu_modules.png'),
+    buttons: [], // buttons will come from mathich MODULES as per below
+  },
+};
+
+// Below keys are matched against current store customData (stored in settings)
+// i.e. customData['usesVaccineModule']data === 'true'
+const MODULES = {
+  // TODO, change from 'stock' to 'vaccineModule'
+  usesVaccineModule: { title: navStrings.vaccine_module, page: 'stock' },
+};
 
 export class MenuPage extends React.Component {
   constructor(props) {
@@ -24,7 +64,24 @@ export class MenuPage extends React.Component {
   }
 
   componentWillMount() {
-    const { database } = this.props;
+    const { database, settings } = this.props;
+
+    this.sections = SECTIONS;
+    // check is there are any modules enabled in this store customData
+    const usableModules = this.getUsedModules(settings.get(THIS_STORE_CUSTOM_DATA));
+    const usesModules = usableModules.length > 0;
+    this.sections.modules.buttons = usableModules;
+
+    if (!usesModules) {
+      this.layout = ORIGINAL_LAYOUT;
+      this.imageStyle = localStyles.originalImage;
+      this.sectionStyle = localStyles.section;
+    } else {
+      this.layout = VACCINE_MODULE_LAYOUT;
+      this.imageStyle = localStyles.VM_Image;
+      // Image will go on the left side as layout changes when modules are enabled
+      this.sectionStyle = { ...localStyles.section, flexDirection: 'row' };
+    }
 
     this.databaseListenerId = database.addListener(
       // Ensure that language changes in login modal are re-rendered onto the MenuPage.
@@ -38,96 +95,66 @@ export class MenuPage extends React.Component {
     database.removeListener(this.databaseListenerId);
   }
 
+  /**
+   * Method will try to match stringified customData object for current store
+   * against keys in MODULES, will return values of keys in MODULES
+   * that match: customData[keyInModules]data === 'true'
+   * @param {string}  customDataAsString  Store custom data as string
+   * @return {array}  result              The maxOrdersPerPeriod of the orderType
+   */
+  getUsedModules = customDataAsString => {
+    if (!customDataAsString || customDataAsString.length === 0) return [];
+
+    const customData = JSON.parse(customDataAsString);
+    const result = [];
+    const checkAndAddToResult = ([moduleName, moduleValues]) => {
+      if (!customData[moduleName]) return;
+      if (!customData[moduleName].data) return;
+      if (customData[moduleName].data === 'true') result.push(moduleValues);
+    };
+
+    Object.entries(MODULES).forEach(checkAndAddToResult);
+
+    return result.length > 0 && result;
+  };
+
   exportData = () => {
     const { settings, database } = this.props;
     const syncSiteName = settings.get(SYNC_SITE_NAME);
     database.exportData(syncSiteName);
   };
 
+  renderButton = ({ title, page }) => (
+    <Button
+      style={globalStyles.menuButton}
+      textStyle={globalStyles.menuButtonText}
+      text={title}
+      // eslint-disable-next-line react/destructuring-assignment
+      onPress={() => this.props.navigateTo(page, title)}
+    />
+  );
+
+  renderSection = sectionName => {
+    const { imageStyle, sectionStyle } = this;
+    const section = this.sections[sectionName];
+    return (
+      <View style={sectionStyle}>
+        <Image style={imageStyle} resizeMode="contain" source={section.icon} />
+        <View>{section.buttons.map(this.renderButton)}</View>
+      </View>
+    );
+  };
+
   render() {
     const { isInAdminMode, logOut, navigateTo } = this.props;
+
     return (
       <View style={[globalStyles.pageContentContainer, localStyles.pageContentContainer]}>
-        <View style={[globalStyles.horizontalContainer, localStyles.horizontalContainer]}>
-          <View style={localStyles.container}>
-            <Image
-              style={localStyles.image}
-              resizeMode="contain"
-              // eslint-disable-next-line global-require
-              source={require('../images/menu_people.png')}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.customer_invoices}
-              onPress={() => navigateTo('customerInvoices', navStrings.customer_invoices)}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.customer_requisitions}
-              onPress={() => navigateTo('customerRequisitions', navStrings.customer_requisitions)}
-            />
+        {this.layout.map(row => (
+          <View style={[globalStyles.horizontalContainer, localStyles.horizontalContainer]}>
+            {row.map(this.renderSection)}
           </View>
-
-          <View style={[localStyles.container, localStyles.centralContainer]}>
-            <Image
-              style={localStyles.image}
-              resizeMode="contain"
-              // eslint-disable-next-line global-require
-              source={require('../images/menu_truck.png')}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.supplier_invoices}
-              onPress={() => navigateTo('supplierInvoices', navStrings.supplier_invoices)}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.supplier_requisitions}
-              onPress={() => navigateTo('supplierRequisitions', navStrings.supplier_requisitions)}
-            />
-            {isInAdminMode && (
-              <Button
-                style={globalStyles.menuButton}
-                textStyle={globalStyles.menuButtonText}
-                text="Export Data"
-                onPress={this.exportData}
-              />
-            )}
-          </View>
-
-          <View style={localStyles.container}>
-            <Image
-              style={localStyles.image}
-              resizeMode="contain"
-              // eslint-disable-next-line global-require
-              source={require('../images/menu_pc_clipboard.png')}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.current_stock}
-              onPress={() => navigateTo('stock', navStrings.current_stock)}
-            />
-            <Button
-              style={globalStyles.menuButton}
-              textStyle={globalStyles.menuButtonText}
-              text={navStrings.stocktakes}
-              onPress={() => navigateTo('stocktakes', navStrings.stocktakes)}
-            />
-            {isInAdminMode && (
-              <Button
-                style={globalStyles.menuButton}
-                textStyle={globalStyles.menuButtonText}
-                text="Realm Explorer"
-                onPress={() => navigateTo('realmExplorer', 'Database Contents')}
-              />
-            )}
-          </View>
-        </View>
+        ))}
         <View style={globalStyles.bottomContainer}>
           <Icon.Button
             name="power-off"
@@ -139,6 +166,22 @@ export class MenuPage extends React.Component {
           >
             <Text style={localStyles.logOutText}>{navStrings.log_out}</Text>
           </Icon.Button>
+          {isInAdminMode && (
+            <Button
+              style={globalStyles.menuButton}
+              textStyle={globalStyles.menuButtonText}
+              text="Realm Explorer"
+              onPress={() => navigateTo('realmExplorer', 'Database Contents')}
+            />
+          )}
+          {isInAdminMode && (
+            <Button
+              style={globalStyles.menuButton}
+              textStyle={globalStyles.menuButtonText}
+              text="Export Data"
+              onPress={this.exportData}
+            />
+          )}
         </View>
       </View>
     );
@@ -164,24 +207,24 @@ const localStyles = StyleSheet.create({
     flex: 9,
     justifyContent: 'space-between',
   },
-  container: {
+  section: {
     alignSelf: 'stretch',
     flex: 1,
-    paddingTop: 30,
-    paddingHorizontal: 30,
+    margin: 10,
     borderColor: SHADOW_BORDER,
     borderWidth: 1,
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  centralContainer: {
-    marginHorizontal: 40,
-  },
-  image: {
+  originalImage: {
     height: 150,
     width: 150,
-    marginBottom: 30,
+    marginBottom: 20,
+  },
+  VM_Image: {
+    height: 120,
+    width: 120,
   },
   logOutText: {
     fontFamily: APP_FONT_FAMILY,
