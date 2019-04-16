7011276d8ee80c4b0646e91f62867e77bc40e250
Sworup Shakya <sworup@sussol.net>
2019-04-16 11:35:54 +0545

    Remove codes no longer required
    - Obsolete after https://github.com/react-navigation/react-navigation/pull/3393

--- src/navigation/reducer.js
+++ src/navigation/reducer.js
@@ -10,30 +10,8 @@ const initialState = Navigator.router.getStateForAction(
 );
 
 const navigationReducer = (state = initialState, action) => {
-  // Ensure we don't push the same route twice (quick double tap of button etc.)
-  // Code from https://github.com/react-community/react-navigation/issues/135
-  if (action.type === 'Navigation/NAVIGATE') {
-    const currentRoute = state.routes[state.index];
-    if (
-      currentRoute.routeName === action.routeName &&
-      paramsEqual(currentRoute.params, action.params)
-    ) {
-      return state;
-    }
-  }
-
   const nextState = Navigator.router.getStateForAction(action, state);
   return nextState || state;
-};
-
-const paramsEqual = (params1, params2) => {
-  if (params1 === params2) return true;
-
-  if (!params1 || !params2 || Object.keys(params1).length !== Object.keys(params2).length) {
-    return false;
-  }
-
-  return Object.entries(params1).every(([key, value]) => value === params2[key]);
 };
 
 export default navigationReducer;
