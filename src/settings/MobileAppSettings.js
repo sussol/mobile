b49ec8179de43069013232582fcc7ceeb9479590
andreievg <andrei.evguenov@gmail.com>
2019-04-27 03:55:26 +1200

    add globals

--- src/settings/MobileAppSettings.js
+++ src/settings/MobileAppSettings.js
@@ -3,13 +3,18 @@ import { Settings } from 'react-native-database';
 import DeviceInfo from 'react-native-device-info';
 
 import { SETTINGS_KEYS } from './index';
+import { MILLISECONDS_PER_DAY } from '../database/utilities';
 import { setCurrentLanguage, DEFAULT_LANGUAGE } from '../localization';
+
+const { THIS_STORE_CUSTOM_DATA } = SETTINGS_KEYS;
+const DEFAULT_AMC_MONTHS_LOOKBAK = 3; // three months
 
 export class MobileAppSettings extends Settings {
   constructor(database) {
     super(database);
     this.load();
     this.set(SETTINGS_KEYS.HARDWARE_UUID, DeviceInfo.getUniqueID());
+    this.refreshGlobals();
   }
 
   set(key, value) {
@@ -21,6 +26,30 @@ export class MobileAppSettings extends Settings {
       default:
         break;
     }
+  }
+
+  refreshGlobals() {
+    let customData = {};
+
+    try {
+      customData = JSON.parse(this.get(THIS_STORE_CUSTOM_DATA));
+    } catch (e) {
+      //
+    }
+
+    const {
+      monthlyConsumptionLookBackPeriod: { data: AMCLookBackMonthsString } = {},
+      monthlyConsumptionEnforceLookBackPeriod: { data: AMCenforceLookBackString } = {},
+    } = customData;
+
+    let AMCLookBackMonth = parseInt(AMCLookBackMonthsString, 10);
+    // eslint-disable-next-line no-restricted-globals
+    if (isNaN(AMCLookBackMonth) || AMCLookBackMonth <= 0) {
+      AMCLookBackMonth = DEFAULT_AMC_MONTHS_LOOKBAK;
+    }
+
+    global.AMCmillisecondsLookBack = AMCLookBackMonth * 30 * MILLISECONDS_PER_DAY;
+    global.AMCenforceLookBack = AMCenforceLookBackString === 'true';
   }
 
   // Call functions for initialising the app on start. Checks database for any
