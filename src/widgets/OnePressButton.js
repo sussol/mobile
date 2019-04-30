372b2b4a4acfe98a946b641fd45a08889d8ed0dd
Chris Petty <christopher.m.petty@gmail.com>
2019-04-30 15:30:29 +1200

    regular imports of withOnePress

--- src/widgets/OnePressButton.js
+++ src/widgets/OnePressButton.js
@@ -1,4 +1,4 @@
-import withOnePress from './withOnePress';
+import { withOnePress } from './withOnePress';
 import { Button } from 'react-native-ui-components';
 
 export const OnePressButton = withOnePress(Button);
