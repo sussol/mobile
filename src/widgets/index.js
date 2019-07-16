2abf6e67e596ab3625911882202825da591fe2cb
joshxg <griffinjoshua5@gmail.com>
2019-07-16 15:12:34 +1200

    Add export of new components

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -19,15 +19,18 @@ export {
 } from './modals';
 
 export { AutocompleteSelector } from './AutocompleteSelector';
+export { ExpiryTextInput } from './ExpiryTextInput';
 export { FinaliseButton } from './FinaliseButton';
+export { GenericChoiceList } from './GenericChoiceList';
+export { IconCell } from './IconCell';
 export { NavigationBar } from './NavigationBar';
+export { OnePressButton } from './OnePressButton';
 export { PageButton } from './PageButton';
 export { PageInfo } from './PageInfo';
+export { SequentialSteps } from './SequentialSteps';
 export { Spinner } from './Spinner';
 export { SyncState } from './SyncState';
 export { TextEditor } from './TextEditor';
 export { TextInput } from './TextInput';
 export { ToggleBar } from './ToggleBar';
 export { ToggleSelector } from './ToggleSelector';
-export { ExpiryTextInput } from './ExpiryTextInput';
-export { OnePressButton } from './OnePressButton';
