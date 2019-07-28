8ab810e66991c8c30285e6d9dd7f3c8b6109bae5
joshxg <griffinjoshua5@gmail.com>
2019-07-29 08:41:56 +1200

    Add bluetooth methods to Sensor model

--- src/database/DataTypes/Sensor.js
+++ src/database/DataTypes/Sensor.js
@@ -5,6 +5,8 @@
 
 import Realm from 'realm';
 
+import { NativeModules } from 'react-native';
+
 export class Sensor extends Realm.Object {
   get toString() {
     return `MAC: ${this.macAddress} TEMP: ${this.temperature}`;
@@ -34,6 +36,76 @@ export class Sensor extends Realm.Object {
     const { latestAggregatedLog } = this;
     if (latestAggregatedLog === null) return false;
     return latestAggregatedLog.isInBreach;
+  }
+
+  async sendCommand({ manufacturerId, command }) {
+    const { BleManager } = NativeModules;
+    try {
+      return await BleManager.sendCommand(manufacturerId, this.macAddress, command);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async sendBlink(manufacturerId) {
+    try {
+      const { success } = await this.sendCommand({ manufacturerId, command: '*blink' });
+      return success;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async sendReset({ manufacturerId }) {
+    try {
+      const firstResult = await this.sendCommand({ manufacturerId, command: '*lint240' });
+      const { success: firstSuccess } = firstResult;
+      if (!firstSuccess) return false;
+
+      const secondResult = await this.sendCommand({ manufacturerId, command: '*sadv1000' });
+      const { success: secondSuccess } = secondResult;
+      if (!secondSuccess) return false;
+
+      return true;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async downloadLogs({ manufacturerId }) {
+    try {
+      return await this.sendCommand({ manufacturerId, command: '*logall' });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async scanAndUpdate({ manufacturerId, database }) {
+    const { BleManager } = NativeModules;
+    try {
+      const result = await BleManager.getDevices(manufacturerId, this.macAddress);
+      const { success, data } = result;
+      console.log(result);
+      if (!success) return false;
+      if (!data || data.length === 0) return false;
+      const advertisement = data[0];
+      const { macAddress, batteryLevel, currentTemperature, loggingInterval } = advertisement;
+      console.log('scan and update');
+      console.log(macAddress, batteryLevel, currentTemperature, loggingInterval);
+      if (macAddress !== this.macAddress) return false;
+      database.write(() => {
+        database.update('Sensor', {
+          id: this.id,
+          batteryLevel,
+          temperature: currentTemperature,
+          logInterval: loggingInterval,
+          lastConnectionTimestamp: new Date(),
+        });
+      });
+      return true;
+    } catch (error) {
+      return error;
+    }
   }
 }
 
