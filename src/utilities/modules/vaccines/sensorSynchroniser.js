5c8fcb3bbed2e7e3f0c7a65c144dc820c476c80d
joshxg <griffinjoshua5@gmail.com>
2019-07-30 09:28:13 +1200

    Add update sensorSynchroniser

--- src/utilities/modules/vaccines/sensorSynchroniser.js
+++ src/utilities/modules/vaccines/sensorSynchroniser.js
@@ -1,68 +1,46 @@
 import { preAggregateLogs, doFullAggregation } from './aggregation';
-import { createSensorLogs } from './utilities';
-import { applyBreaches, addHeadAndTrailingLogToBreach } from './temperatureSensorHelpers';
+import { createSensorLogs, createBreaches } from './utilities';
 
-/* eslint-disable import/prefer-default-export */
-const FIND_SENSOR = 'Searching for sensor';
-const NO_SENSOR = 'Cannot find sensor in proximity';
-const SYNCING_LOGS = 'Syncing logs:';
+export const syncSensor = async ({ database, sensor, updateMessage }) => {
+  updateMessage({ message: 'Searching for sensor', progress: 0 });
+  const downloadedLogs = await sensor.downloadLogs();
 
-const updateMessageSyncState = ({ message, lastSync, total = 100, progress = 100 }) => {
-  // eslint-disable-next-line react/destructuring-assignment, prefer-destructuring
-  if (!lastSync) lastSync = this.state.temperatureSyncState.lastSync;
-  this.setState({
-    temperatureSyncState: {
-      ...this.state.temperatureSyncState, // eslint-disable-line react/no-access-state-in-setstate, react/destructuring-assignment, max-len
-      message,
-      lastSync,
-      isSyncing: progress < 100,
-      total,
-      progress,
-    },
-  });
-};
-
-const syncSensor = async ({ database, sensor, updateMessage }) => {
-  const params = { sensor, database };
-  let result = null;
-  // Update the initial message and progress
-  updateMessage({ message: FIND_SENSOR, progress: 0 });
-  // Scan and update the sensor - number of logs to download etc.
-  result = await sensor.scanAndUpdate({ database, manufacturerId: 307 });
-  if (!result) {
-    updateMessage({ message: NO_SENSOR });
-    return;
-  }
-
-  // Update with the number of logs to download
-  updateMessage({
-    message: `${SYNCING_LOGS} ${sensor.numberOfLogs}`,
-    progress: 10,
-  });
-  // Download from the sensor all logs waiting
-  result = await sensor.downloadLogs({ manufacturerId: 307 });
-  console.log(result);
-
-  if (!result.success) {
+  if (!(downloadedLogs && downloadedLogs.success)) {
     updateMessage({ message: 'Failed to sync temperature data' });
     return;
   }
+
+  database.write(() => {
+    database.update('Sensor', {
+      id: sensor.id,
+      lastConnectionTimestamp: new Date(),
+      ...downloadedLogs.data[0],
+    });
+  });
 
   updateMessage({
     message: `Creating Logs: ${sensor.numberOfLogs}`,
     progress: 25,
   });
 
-  // Aggregation
-  // const lastSync = database.objects('SensorLog').max('timestamp');
+  const createResult = await createSensorLogs({
+    database,
+    sensor,
+    parsedLogs: downloadedLogs.data[0].logs,
+  });
 
-  result = await createSensorLogs({ database, sensor, parsedLogs: result.data[0].logs });
+  if (!(createResult && createResult.success)) {
+    updateMessage({
+      message: 'Failed creating sensor logs',
+      progress: 45,
+    });
+  }
 
   updateMessage({ message: 'Pre aggregating Logs', progress: 40 });
-  result = await preAggregateLogs(params);
-  console.log(result);
 
-  if (!result.success) {
+  const preAggregateResult = await preAggregateLogs({ sensor, database });
+
+  if (!(preAggregateResult && preAggregateResult.success)) {
     updateMessage({
       message: 'Temperature sync successfull, but failure during aggregation',
       progress: 45,
@@ -71,18 +49,8 @@ const syncSensor = async ({ database, sensor, updateMessage }) => {
   }
 
   updateMessage({ message: 'Searching for breaches', progress: 50 });
-  result = await applyBreaches(params);
-  console.log(result);
-  if (!result.success) {
-    updateMessage({
-      message: 'Temperature sync successfull, but failed to search for breaches',
-    });
-    return;
-  }
-
-  result = await addHeadAndTrailingLogToBreach(params);
-  console.log(result);
-  if (!result.success) {
+  const breachesResult = await createBreaches({ sensor, database });
+  if (!breachesResult.success) {
     updateMessage({
       message: 'Temperature sync successfull, but failed to search for breaches',
     });
@@ -90,36 +58,25 @@ const syncSensor = async ({ database, sensor, updateMessage }) => {
   }
 
   updateMessage({ message: 'Finalising aggregation', progress: 55 });
-  result = await doFullAggregation(params);
-  console.log(result);
-  // Reconfiguring sensor
+  const fullAggregationResult = await doFullAggregation({ database, sensor });
 
-  if (!result.success) {
+  if (!fullAggregationResult.success) {
     updateMessage({
       message: 'Temperature sync successfull, but failed finalise aggregation',
     });
     return;
   }
 
+  // Reconfiguring sensor
   updateMessage({ message: 'Resetting Sensor', progress: 80 });
-  result = await sensor.sendReset(307);
-  console.log(result);
+  const resetResult = await sensor.sendReset();
 
-  if (!result.success) {
+  if (!resetResult.success) {
     updateMessage({ message: 'Sync successfull, but failed to reset sensor' });
     return;
   }
 
-  // updateMessageSyncState({ message: 'Reconfiguring sensor', progress: 80 });
-  // console.log(result);
-  // result = await sensorSyncMethods.resetSensorAdvertismentFrequency(params);
-  // if (!result.success) {
-  //   updateMessageSyncState({
-  //     message: 'Temperature sync successfull, but failed to reconfigure sensor',
-  //   });
-  //   return;
-  // }
-  updateMessageSyncState({ message: 'Full temperature sync successfull' });
+  updateMessage({ message: 'Full temperature sync successfull' });
 };
 
 export const synchroniseSensors = async ({ database, updateMessage }) => {
