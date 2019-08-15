75b65411822bc944251b9fac2c78bb085d3c9306
joshxg <griffinjoshua5@gmail.com>
2019-08-15 14:51:41 +1200

    Add HeaderCell component

---
+++ src/widgets/DataTable/HeaderCell.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, Text, StyleSheet, View, TouchableOpacityPropTypes } from 'react-native';
+
+/**
+ *
+ */
+const HeaderCell = React.memo(
+  ({
+    sortDirection,
+    SortAscComponent,
+    SortDescComponent,
+    SortNeutralComponent,
+    title,
+    columnKey,
+    onPressAction,
+    dispatch,
+    ...otherProps
+  }) => {
+    const onPress = () => {
+      dispatch(onPressAction(columnKey));
+    };
+
+    const Icon = () => {
+      switch (sortDirection) {
+        case 'ASC': {
+          return SortAscComponent;
+        }
+        case 'DESC': {
+          return SortDescComponent;
+        }
+        default: {
+          return SortNeutralComponent;
+        }
+      }
+    };
+
+    const Container = onPressAction ? View : TouchableOpacity;
+
+    return (
+      <Container style={defaultStyles.headerCell} onPress={onPress} {...otherProps}>
+        <Text>{title}</Text>
+        {onPressAction && <Icon />}
+      </Container>
+    );
+  }
+);
+
+HeaderCell.propTypes = {
+  ...TouchableOpacityPropTypes,
+  title: PropTypes.string.isRequired,
+  columnKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onPressAction: PropTypes.func,
+  dispatch: PropTypes.func,
+  sortDirection: PropTypes.oneOf(['ASC', 'DESC']),
+  SortAscComponent: PropTypes.element,
+  SortDescComponent: PropTypes.element,
+  SortNeutralComponent: PropTypes.element,
+};
+
+HeaderCell.defaultProps = {
+  dispatch: null,
+  onPressAction: null,
+  sortDirection: null,
+  SortAscComponent: null,
+  SortDescComponent: null,
+  SortNeutralComponent: null,
+};
+
+const defaultStyles = StyleSheet.create({
+  headerCell: {
+    flex: 1,
+    backgroundColor: 'turquoise',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
+
+export default HeaderCell;
