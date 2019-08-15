7eb8a51668fe2a4fcfc2fb51b337aa898ffb6839
joshxg <griffinjoshua5@gmail.com>
2019-08-16 11:12:37 +1200

    Add updates to RealmRow

--- src/widgets/DataTable/RealmRow.js
+++ src/widgets/DataTable/RealmRow.js
@@ -5,18 +5,46 @@ import PropTypes from 'prop-types';
 
 import { Row } from './index';
 
-const RealmRow = React.memo(({ realmObject, ...props }) => {
-  const [rowData, setRowData] = useState(realmObject);
+class RealmRow extends React.Component {
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    realmObject.addListener(() => {
-      setRowData(realmObject);
+    this.state = {};
+  }
+
+  componentWillMount = () => {
+    const { realmObject } = this.props;
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+    realmObject.isValid();
+    console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+
+    realmObject.batches.forEach(batch => {
+      batch.addListener(() => {
+        console.log('=================================');
+        console.log('LISTNERE');
+        console.log(realmObject.totalQuantity);
+        console.log('=================================');
+        // this.setState({ realmObject });
+      });
     });
-    return realmObject.removeAllListeners();
-  }, []);
+  };
 
-  return <Row rowData={rowData} {...props} />;
-});
+  componentWillUnmount = () => {};
+
+  render() {
+    return <Row rowData={this.props.realmObject} l={{}} {...this.props} />;
+  }
+}
+
+// const RealmRow = React.memo(({ realmObject, ...props }) => {
+//   const [rowData, setRowData] = useState(realmObject);
+
+//   useEffect(() => {
+//     return realmObject.removeAllListeners();
+//   }, []);
+
+//   return <Row rowData={realmObject} {...props} />;
+// });
 
 RealmRow.propTypes = {
   realmObject: PropTypes.object.isRequired,
