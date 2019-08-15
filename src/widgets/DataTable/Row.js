a89056da9f88e87d3350ab39da0f1ab90d2403b0
joshxg <griffinjoshua5@gmail.com>
2019-08-16 11:13:07 +1200

    Add updates to Row

--- src/widgets/DataTable/Row.js
+++ src/widgets/DataTable/Row.js
@@ -5,10 +5,12 @@ import PropTypes from 'prop-types';
 
 import { View, StyleSheet } from 'react-native';
 
-const Row = React.memo(({ rowData, rowState, rowKey, renderCells }) => {
+const Row = ({ rowData, rowState, rowKey, renderCells }) => {
+  console.log('=================================');
   console.log(`Row: ${rowKey}`);
+  console.log('=================================');
   return <View style={defaultStyles.row}>{renderCells(rowData, rowState, rowKey)}</View>;
-});
+};
 
 Row.propTypes = {
   rowData: PropTypes.any.isRequired,
@@ -28,4 +30,23 @@ const defaultStyles = StyleSheet.create({
   },
 });
 
-export default Row;
+Row.whyDidYouRender = true;
+
+export default React.memo(Row, (prevProps, nextProps) => {
+  const { rowData, rowState, rowKey, renderCells } = prevProps;
+  const {
+    rowData: nextRowData,
+    rowState: nextRowState,
+    rowKey: nextRowKey,
+    renderCells: nextRenderCells,
+  } = nextProps;
+
+  console.log('=================================');
+  console.log('rowdata', rowData === nextRowData);
+  console.log('rowState', rowState === nextRowState);
+  console.log('rowKey', rowKey === nextRowKey);
+  console.log('renderCells', renderCells === nextRenderCells);
+  console.log('=================================');
+
+  return false;
+});
