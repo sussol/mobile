6ee052e5de94dcb86cc77f44128a61148b2e9755
joshxg <griffinjoshua5@gmail.com>
2019-08-18 23:40:57 +1200

    Add extract icon components from customer invoice page towidgets/icons.js

---
+++ src/widgets/icons.js
@@ -0,0 +1,26 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+import React from 'react';
+
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+
+import { dataTableColors } from '../globalStyles';
+
+export const SortAscIcon = <FAIcon name="sort-asc" size={15} color="black" />;
+export const SortNeutralIcon = <FAIcon name="sort" size={15} colour="black" />;
+export const SortDescIcon = <FAIcon name="sort-desc" size={15} colour="black" />;
+export const CheckedComponent = (
+  <IonIcon name="md-radio-button-on" size={15} color={dataTableColors.checkableCellChecked} />
+);
+export const UncheckedComponent = (
+  <IonIcon name="md-radio-button-off" size={15} color={dataTableColors.checkableCellUnchecked} />
+);
+export const DisabledCheckedComponent = (
+  <IonIcon name="md-radio-button-on" size={15} color={dataTableColors.checkableCellDisabled} />
+);
+export const DisabledUncheckedComponent = (
+  <IonIcon name="md-radio-button-off" size={15} color={dataTableColors.checkableCellDisabled} />
+);
