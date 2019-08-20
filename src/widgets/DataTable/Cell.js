2e449f74566a9f2ed426f54f85b935c992860013
joshxg <griffinjoshua5@gmail.com>
2019-08-20 21:56:22 +1200

    Add new props to Cell component

--- src/widgets/DataTable/Cell.js
+++ src/widgets/DataTable/Cell.js
@@ -1,36 +1,48 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
+import { getAdjustedStyle } from './utilities';
 
 /**
  * Renders a cell that displays a string/number value within
  * a react-native `Text` component.
  *
- * @param {string|number} value The value to render in cell
+ * @param {string|number} value     The value to render in cell
+ * @param {Object}        viewStyle Style object for the containing View
+ * @param {Object}        textStyle Style object for the inner Text
  */
-const Cell = React.memo(({ value }) => {
+const Cell = React.memo(({ value, textStyle, viewStyle, width, isLastCell }) => {
   console.log(`- Cell: ${value}`);
+
+  const internalViewStyle = getAdjustedStyle(viewStyle, width, isLastCell);
+
   return (
-    <View style={defaultStyles.cell}>
-      <Text>{value}</Text>
+    <View style={internalViewStyle}>
+      <Text style={textStyle}>{value}</Text>
     </View>
   );
 });
 
+const defaultStyles = StyleSheet.create({
+  viewStyle: {},
+  textStyle: {},
+});
+
 Cell.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  textStyle: PropTypes.object,
+  viewStyle: PropTypes.object,
+  width: PropTypes.number,
+  isLastCell: PropTypes.bool,
 };
 
 Cell.defaultProps = {
   value: '',
+  textStyle: defaultStyles.textStyle,
+  viewStyle: defaultStyles.viewStyle,
+  width: 0,
+  isLastCell: 0,
 };
-
-const defaultStyles = StyleSheet.create({
-  cell: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    justifyContent: 'center',
-  },
-});
 
 export default Cell;
