ce0c3111fd4d30ed71d6414dde93333be92ff46d
joshxg <griffinjoshua5@gmail.com>
2019-08-20 20:34:52 +1200

    Add update to icons margin

--- src/widgets/icons.js
+++ src/widgets/icons.js
@@ -9,9 +9,9 @@ import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 import { dataTableColors } from '../globalStyles';
 
-export const SortAscIcon = <FAIcon name="sort-asc" size={15} />;
-export const SortNeutralIcon = <FAIcon name="sort" size={15} />;
-export const SortDescIcon = <FAIcon name="sort-desc" size={15} />;
+export const SortAscIcon = <FAIcon name="sort-asc" size={15} style={{ marginRight: 10 }} />;
+export const SortNeutralIcon = <FAIcon name="sort" size={15} style={{ marginRight: 10 }} />;
+export const SortDescIcon = <FAIcon name="sort-desc" size={15} style={{ marginRight: 10 }} />;
 export const CheckedComponent = (
   <IonIcon name="md-radio-button-on" size={15} color={dataTableColors.checkableCellChecked} />
 );
