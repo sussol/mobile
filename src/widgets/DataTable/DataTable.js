92063de2c8ad483434cd130b52f4cbc3bed4423d
joshxg <griffinjoshua5@gmail.com>
2019-08-20 21:56:36 +1200

    Add new props to DataTable component

--- src/widgets/DataTable/DataTable.js
+++ src/widgets/DataTable/DataTable.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/forbid-prop-types */
 /**
  * mSupply Mobile
  * Sustainable Solutions (NZ) Ltd. 2019
@@ -7,12 +8,26 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { StyleSheet, VirtualizedList, VirtualizedListPropTypes } from 'react-native';
 
-const DataTable = React.memo(({ renderRow, renderHeader, ...otherProps }) => (
+/**
+ * Base DataTable component. Thin wrapper around VirtualizedList, providing
+ * a header component. All VirtualizedList props can be passed through,
+ * however renderItem is renamed renderRow.
+ *
+ * @param {Func}   renderRow    renaming of virtualizedList renderItem prop.
+ * @param {Func}   renderHeader Function which should return a header component
+ */
+const DataTable = React.memo(({ renderRow, renderHeader, style, ...otherProps }) => (
   <>
     {renderHeader()}
-    <VirtualizedList style={defaultStyles.virtualizedList} renderItem={renderRow} {...otherProps} />
+    <VirtualizedList style={style} renderItem={renderRow} {...otherProps} />
   </>
 ));
+
+const defaultStyles = StyleSheet.create({
+  virtualizedList: {
+    flex: 1,
+  },
+});
 
 DataTable.propTypes = {
   ...VirtualizedListPropTypes,
@@ -20,18 +35,14 @@ DataTable.propTypes = {
   renderHeader: PropTypes.func,
   getItem: PropTypes.func,
   getItemCount: PropTypes.func,
+  style: PropTypes.object,
 };
 
 DataTable.defaultProps = {
   getItem: (items, index) => items[index],
   getItemCount: items => items.length,
   renderHeader: null,
+  style: defaultStyles.virtualizedList,
 };
-
-const defaultStyles = StyleSheet.create({
-  virtualizedList: {
-    flex: 1,
-  },
-});
 
 export default DataTable;
