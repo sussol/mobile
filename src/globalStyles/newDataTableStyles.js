5e4e066b76b65a72a2468f0de889cb9faf82cf68
joshxg <griffinjoshua5@gmail.com>
2019-08-20 20:34:23 +1200

    Add extraction of editted styles to newX files

---
+++ src/globalStyles/newDataTableStyles.js
@@ -0,0 +1,128 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import { Dimensions } from 'react-native';
+
+import { BACKGROUND_COLOR, BLUE_WHITE, DARK_GREY } from './colors';
+import { APP_FONT_FAMILY } from './fonts';
+
+const newDataTableStyles = {
+  cellContainer: {
+    left: {
+      borderRightWidth: 2,
+      borderColor: '#ecf3fc',
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'left',
+    },
+    right: {
+      borderRightWidth: 2,
+      borderColor: '#ecf3fc',
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'right',
+    },
+    center: {
+      borderRightWidth: 2,
+      borderColor: '#ecf3fc',
+      flex: 1,
+      justifyContent: 'center',
+      textAlign: 'center',
+    },
+  },
+  cellText: {
+    left: {
+      marginLeft: 20,
+      textAlign: 'left',
+      fontFamily: APP_FONT_FAMILY,
+      fontSize: Dimensions.get('window').width / 100,
+      color: DARK_GREY,
+    },
+    right: {
+      marginRight: 20,
+      textAlign: 'right',
+      fontFamily: APP_FONT_FAMILY,
+      fontSize: Dimensions.get('window').width / 100,
+      color: DARK_GREY,
+    },
+    center: {
+      textAlign: 'center',
+      fontFamily: APP_FONT_FAMILY,
+      fontSize: Dimensions.get('window').width / 100,
+      color: DARK_GREY,
+    },
+  },
+  editableCellText: {
+    textAlign: 'right',
+    fontFamily: APP_FONT_FAMILY,
+    fontSize: Dimensions.get('window').width / 100,
+    color: DARK_GREY,
+  },
+  editableCellTextView: {
+    borderBottomColor: '#cbcbcb',
+    borderBottomWidth: 1,
+    flex: 1,
+    flexDirection: 'row',
+    width: '88%',
+    maxHeight: '65%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginRight: 20,
+  },
+  touchableCellContainer: {
+    borderRightWidth: 2,
+    borderColor: '#ecf3fc',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    backgroundColor: BACKGROUND_COLOR,
+    flex: 1,
+    flexDirection: 'row',
+    height: 45,
+  },
+  alternateRow: {
+    backgroundColor: 'white',
+    flex: 1,
+    flexDirection: 'row',
+    height: 45,
+  },
+  headerCells: {
+    left: {
+      height: 40,
+      borderRightWidth: 2,
+      borderBottomWidth: 2,
+      backgroundColor: 'white',
+      borderColor: BLUE_WHITE,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    right: {
+      height: 40,
+      borderRightWidth: 2,
+      borderBottomWidth: 2,
+      backgroundColor: 'white',
+      borderColor: BLUE_WHITE,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    center: {
+      height: 40,
+      borderRightWidth: 2,
+      borderBottomWidth: 2,
+      backgroundColor: 'white',
+      borderColor: BLUE_WHITE,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  },
+};
+
+export default newDataTableStyles;
---
+++ src/globalStyles/newPageStyles.js
@@ -0,0 +1,56 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import { APP_FONT_FAMILY } from './fonts';
+import { TRANSPARENT } from './colors';
+
+export const PAGE_CONTENT_PADDING_HORIZONTAL = 20;
+export const PAGE_CONTENT_PADDING_TOP = 10;
+export const PAGE_CONTENT_PADDING_BOTTOM = 20;
+
+const newPageStyles = {
+  newPageContentContainer: {
+    flex: 1,
+    PAGE_CONTENT_PADDING_TOP: 10,
+    PAGE_CONTENT_PADDING_HORIZONTAL: 20,
+    PAGE_CONTENT_PADDING_BOTTOM: 20,
+    backgroundColor: '#f8fbfe',
+  },
+  newContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#ecf3fc',
+  },
+  newPageTopSectionContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  newPageTopLeftSectionContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    width: 500,
+    paddingHorizontal: 10,
+  },
+  newPageTopRightSectionContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  searchBar: {
+    height: 40,
+    flex: 1,
+    fontSize: 16,
+    fontFamily: APP_FONT_FAMILY,
+    backgroundColor: TRANSPARENT,
+  },
+};
+
+export default newPageStyles;
