ff67f9166929e9a17f255d9dc8156ce054afa7a5
joshxg <griffinjoshua5@gmail.com>
2019-08-26 14:06:34 +1200

    Add onPress prop to Row component

--- src/widgets/DataTable/Row.js
+++ src/widgets/DataTable/Row.js
@@ -4,6 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 /**
  * Renders a row of children as outputted by renderCells render prop
@@ -11,14 +12,22 @@ import { View } from 'react-native';
  * @param {object} rowData Data to pass to renderCells callback
  * @param {string|number} rowKey Unique key associated to row
  * @param {object} rowState State to pass to renderCells callBack
+ * @param {func} onPress function to call on pressing the row. See below.
+ * @param {object} viewStyle Style object for the wrapping View component
  * @param {func} renderCells renderProp callBack for rendering cells based on rowData and rowState
  *                          `(rowKey, columnKey) => {...}`
- * @param {object} viewStyle Style object for the wrapping View component
+ * Tap gesture events will be captured in this component for any taps on cells within
+ * this container.
  */
-const Row = React.memo(({ rowData, rowState, rowKey, renderCells, style }) => {
+const Row = React.memo(({ rowData, rowState, rowKey, renderCells, style, onPress }) => {
   console.log('=================================');
   console.log(`Row: ${rowKey}`);
-  return <View style={style}>{renderCells(rowData, rowState, rowKey)}</View>;
+  const Container = onPress ? TouchableOpacity : View;
+  return (
+    <Container onPress={onPress} style={style}>
+      {renderCells(rowData, rowState, rowKey)}
+    </Container>
+  );
 });
 
 Row.propTypes = {
@@ -27,11 +36,13 @@ Row.propTypes = {
   rowKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   renderCells: PropTypes.func.isRequired,
   style: PropTypes.object,
+  onPress: PropTypes.func,
 };
 
 Row.defaultProps = {
   rowState: null,
   style: {},
+  onPress: null,
 };
 
 export default Row;
