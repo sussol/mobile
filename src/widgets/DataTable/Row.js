a327c609106a22dc650f14ef549d47c96f01fe8f
joshxg <griffinjoshua5@gmail.com>
2019-08-26 17:19:53 +1200

    Add updated docstring in Row.js

--- src/widgets/DataTable/Row.js
+++ src/widgets/DataTable/Row.js
@@ -8,16 +8,16 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 /**
  * Renders a row of children as outputted by renderCells render prop
+ * Tap gesture events will be captured in this component for any taps
+ * on cells within this container.
  *
  * @param {object} rowData Data to pass to renderCells callback
  * @param {string|number} rowKey Unique key associated to row
  * @param {object} rowState State to pass to renderCells callBack
- * @param {func} onPress function to call on pressing the row. See below.
+ * @param {func} onPress function to call on pressing the row.
  * @param {object} viewStyle Style object for the wrapping View component
  * @param {func} renderCells renderProp callBack for rendering cells based on rowData and rowState
  *                          `(rowKey, columnKey) => {...}`
- * Tap gesture events will be captured in this component for any taps on cells within
- * this container.
  */
 const Row = React.memo(({ rowData, rowState, rowKey, renderCells, style, onPress }) => {
   console.log('=================================');
