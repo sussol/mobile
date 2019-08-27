17a3f3dcd1bd3184f9595e73752c3c7f36259322
joshxg <griffinjoshua5@gmail.com>
2019-08-27 15:08:18 +1200

    Add comments to BaseDatabase

--- src/database/BaseDatabase.js
+++ src/database/BaseDatabase.js
@@ -1,3 +1,16 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+/**
+ * Interface to a Realm, to be wrapped by
+ * SyncDatabase or UIDatabase.
+ *
+ * Only one instance will be active at any point
+ * in the applications life-cycle.
+ */
+
 import { Database } from 'react-native-database';
 
 import { schema } from './schema';
