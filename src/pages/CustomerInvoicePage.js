e74bb718aae5c30635cee83b0d856597c05bde2a
joshxg <griffinjoshua5@gmail.com>
2019-08-28 08:54:36 +1200

    Add removal of checkForFinaliseError from CustomerInvoicePage

--- src/pages/CustomerInvoicePage.js
+++ src/pages/CustomerInvoicePage.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable import/prefer-default-export */
 /**
  * mSupply Mobile
  * Sustainable Solutions (NZ) Ltd. 2019
@@ -327,29 +329,9 @@ export const CustomerInvoicePage = ({
   );
 };
 
-/* eslint-disable react/forbid-prop-types */
 CustomerInvoicePage.propTypes = {
   database: PropTypes.object.isRequired,
   runWithLoadingIndicator: PropTypes.func.isRequired,
   transaction: PropTypes.object.isRequired,
   routeName: PropTypes.string.isRequired,
 };
-
-/**
- * Check whether a given customer invoice is safe to be finalised. If safe to finalise,
- * return null, else return an appropriate error message.
- *
- * @param   {object}  customerInvoice  The customer invoice to check.
- * @return  {string}                   Error message if unsafe to finalise, else null.
- */
-export function checkForFinaliseError(customerInvoice) {
-  if (customerInvoice.items.length === 0) {
-    return modalStrings.add_at_least_one_item_before_finalising;
-  }
-
-  if (customerInvoice.totalQuantity === 0) {
-    return modalStrings.record_stock_to_issue_before_finalising;
-  }
-
-  return null;
-}
