ff80f4f353009a1ce3e21bc5d9cf4378b6bcee72
joshxg <griffinjoshua5@gmail.com>
2019-08-27 16:38:34 +1200

    Add corrected disabling logic for CustomerInvoicePage and EditableCell

--- src/pages/CustomerInvoicePage.js
+++ src/pages/CustomerInvoicePage.js
@@ -126,13 +126,13 @@ export const CustomerInvoicePage = ({
   const renderCells = useCallback((rowData, rowState = {}, rowKey) => {
     const {
       cellContainer,
-      editableCellText,
       editableCellTextView,
       cellText,
       touchableCellContainer,
     } = newDataTableStyles;
     return columns.map(({ key: colKey, type, width, alignText }, index) => {
       const isLastCell = index === columns.length - 1;
+      const isDisabled = isFinalised || (rowState && rowState.disabled);
       switch (type) {
         case 'editable':
           return (
@@ -143,14 +143,14 @@ export const CustomerInvoicePage = ({
               columnKey={colKey}
               editAction={editTotalQuantity}
               isFocused={colKey === (rowState && rowState.focusedColumn)}
-              disabled={rowState && rowState.disabled}
+              isDisabled={isDisabled}
               focusAction={focusCell}
               focusNextAction={focusNext}
               dispatch={dispatch}
               width={width}
               viewStyle={cellContainer[alignText || 'left']}
               textInputStyle={cellText[alignText || 'left']}
-              textStyle={editableCellText}
+              textStyle={cellText[alignText || 'left']}
               textViewStyle={editableCellTextView}
               isLastCell={isLastCell}
               debug
