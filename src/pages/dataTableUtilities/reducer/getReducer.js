700ee8c82c4a50e1abc997a9c2b81512101c5d3e
joshxg <griffinjoshua5@gmail.com>
2019-09-01 13:53:10 +1200

    Add creation of programSupplierRequisition reducer

--- src/pages/dataTableUtilities/reducer/getReducer.js
+++ src/pages/dataTableUtilities/reducer/getReducer.js
@@ -140,6 +140,25 @@ const supplierRequisitions = {
   refreshData,
 };
 
+const programSupplierRequisition = {
+  ...BASE_TABLE_PAGE_REDUCER,
+  filterData,
+  selectRow,
+  deselectRow,
+  deselectAll,
+  openBasicModal,
+  closeBasicModal,
+  editTheirRef,
+  editComment,
+  refreshData,
+  addMasterListItems,
+  addItem,
+  createAutomaticOrder,
+  useSuggestedQuantities,
+  hideOverStocked,
+  showOverStocked,
+};
+
 const PAGE_REDUCERS = {
   customerInvoice,
   customerInvoices,
