db6fca6abd6575dc57c6323e0b5e7110afc4cd2b
josh <griffinjoshua5@gmail.com>
2019-09-11 21:43:47 +1200

    Add initial tableActions.js file

---
+++ src/pages/dataTableUtilities/tableActions.js
@@ -0,0 +1,173 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import { UIDatabase } from '../../database/index';
+import Settings, { SETTINGS_KEYS } from '../../settings';
+import { createRecord } from '../../database/utilities/index';
+import { closeModal } from './pageActions';
+import { ACTIONS } from './constants';
+
+/**
+ * Sorts the underlying data array by the key provided. Determines
+ * direction by the previous direction.
+ *
+ * @param {String} sortBy Key to sortBy - see utilities/sortDataBy.js
+ */
+export const sortData = sortBy => ({
+  type: ACTIONS.SORT_DATA,
+  payload: { sortBy },
+});
+
+/**
+ * Filters the underlying data array by the term provided.
+ * Fields which should be used for filtering are set as
+ * filterDataKeys in state. Sort order is preserved.
+ *
+ * @param {String} searchTerm String to filter by.
+ */
+export const filterData = searchTerm => ({
+  type: ACTIONS.FILTER_DATA,
+  payload: { searchTerm },
+});
+
+/**
+ * Refreshes the underlying data array by slicing backingData.
+ * BackingData is a live realm collection which side effects i.e.
+ * finalising can make out of sync with the data array used for display.
+ */
+export const refreshData = () => ({ type: ACTIONS.REFRESH_DATA });
+
+/**
+ * Hides all items which have current stock on hand greater than the
+ * threshold MOS stock for that item.
+ */
+export const hideOverStocked = () => ({ type: ACTIONS.HIDE_OVER_STOCKED });
+
+/**
+ * Shows all items, regardless of current stock on hand, toggles
+ * showAll to true and removes the current search filtering. Sort is
+ * kept stable.
+ */
+export const showOverStocked = () => ({ type: ACTIONS.REFRESH_DATA });
+
+/**
+ * Hides all items which have a current stock on hand less than 0.
+ */
+export const hideStockOut = () => ({ type: ACTIONS.HIDE_STOCK_OUT });
+
+/**
+ * Shows all items, regardless of current stock on hand, toggles
+ * showAll to true and removes the current search filtering. Sort is
+ * kept stable.
+ */
+export const showStockOut = () => ({ type: ACTIONS.REFRESH_DATA });
+
+/**
+ * Adds all items from master lists, according to the type of pageObject.
+ * i.e. a CustomerInvoice adds items from all of the custoemrs masterlists.
+ * where as a Supplier Requisition adds items from all of this stores
+ * masterlists.
+ *
+ * @param {String} objectType Type of object to add items for.
+ */
+export const addMasterListItems = objectType => (dispatch, getState) => {
+  const { pageObject } = getState();
+
+  const thisStore = UIDatabase.objects('Name').filtered(
+    'id == $0',
+    Settings.get(SETTINGS_KEYS.THIS_STORE_NAME_ID)
+  )[0];
+
+  UIDatabase.write(() => {
+    pageObject.addItemsFromMasterList(UIDatabase, thisStore);
+    UIDatabase.save(objectType, pageObject);
+  });
+  dispatch({ type: 'addMasterListItems', objectType });
+};
+
+/**
+ * Creates an 'item' record - i.e. StocktakeItem from an Item
+ * object.
+ * If the item is already a part of the underlying pageObject,
+ * do not add it.
+ *
+ * use cases: Adding a row to an Item based page - i.e. Stocktake,
+ * Requisition - NOT Batch based i.e. SupplierInvoice.
+ *
+ * @param {Object} item           The item to be added.
+ * @param {String} addedItemType  The item type to be added.
+ */
+export const addItem = (item, addedItemType) => (dispatch, getState) => {
+  const { pageObject } = getState();
+
+  if (!pageObject.hasItem(item)) {
+    UIDatabase.write(() => {
+      const addedItem = createRecord(UIDatabase, addedItemType, pageObject, item);
+      dispatch({ type: ACTIONS.ADD_RECORD, payload: { item: addedItem } });
+    });
+  } else {
+    dispatch(closeModal());
+  }
+};
+
+/**
+ * Wrappers around addItem action creator to pass a pageObject type.
+ *
+ * @param {Object} item The item to be added.
+ */
+export const addTransactionItem = item => addItem(item, 'TransactionItem');
+export const addStocktakeItem = item => addItem(item, 'StocktakeItem');
+export const addRequisitionItem = item => addItem(item, 'RequisitionItem');
+
+/**
+ * Creates a transaction batch which will be associated with the current stores
+ * pageObject.
+ * use case: Pages which are batch-based i.e. SupplierInvoice page.
+ * @param {Object} item The item to create a transaction batch for.
+ */
+export const addTransactionBatch = item => (dispatch, getState) => {
+  const { pageObject } = getState();
+
+  UIDatabase.write(() => {
+    const transItem = createRecord(UIDatabase, 'TransactionItem', pageObject, item);
+    const itemBatch = createRecord(UIDatabase, 'ItemBatch', item, '');
+    const addedBatch = createRecord(UIDatabase, 'TransactionBatch', transItem, itemBatch);
+    dispatch({ type: ACTIONS.ADD_RECORD, payload: { item: addedBatch } });
+  });
+};
+
+/**
+ * Creates an automatic order for a Supplier Requisition.
+ */
+export const createAutomaticOrder = () => (dispatch, getState) => {
+  const { pageObject } = getState();
+
+  const thisStore = UIDatabase.objects('Name').filtered(
+    'id == $0',
+    Settings.get(SETTINGS_KEYS.THIS_STORE_NAME_ID)
+  )[0];
+
+  UIDatabase.write(() => {
+    pageObject.createAutomaticOrder(UIDatabase, thisStore);
+    UIDatabase.save('Requisition', pageObject);
+  });
+
+  dispatch({ type: ACTIONS.REFRESH_DATA });
+};
+
+/**
+ * Sets all requested quantities to the suggested quantity for
+ * a requisition.
+ */
+export const useSuggestedQuantities = () => (dispatch, getState) => {
+  const { pageObject } = getState();
+
+  UIDatabase.write(() => {
+    pageObject.setRequestedToSuggested(UIDatabase);
+    UIDatabase.save('Requisition', pageObject);
+  });
+
+  dispatch({ type: ACTIONS.REFRESH_DATA });
+};
