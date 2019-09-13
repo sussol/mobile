265a14367330760066bdad1bf92096f1b546d257
josh <griffinjoshua5@gmail.com>
2019-09-14 11:23:12 +1200

    Add update to columns, tidying up adding editable field

--- src/pages/dataTableUtilities/getColumns.js
+++ src/pages/dataTableUtilities/getColumns.js
@@ -21,7 +21,7 @@ const PAGE_COLUMN_WIDTHS = {
 
 const PAGE_COLUMNS = {
   customerInvoice: ['itemCode', 'itemName', 'availableQuantity', 'totalQuantity', 'remove'],
-  supplierInvoice: ['itemCode', 'itemName', 'totalQuantity', 'editableExpiryDate', 'remove'],
+  supplierInvoice: ['itemCode', 'itemName', 'totalQuantity', 'expiryDate', 'remove'],
   customerInvoices: ['serialNumber', 'customer', 'status', 'entryDate', 'comment', 'remove'],
   supplierRequisitions: [
     'serialNumber',
@@ -59,7 +59,7 @@ const PAGE_COLUMNS = {
     'snapshotTotalQuantity',
     'countedTotalQuantity',
     'difference',
-    'modalControl',
+    'batches',
   ],
   stocktakeEditorReasons: [
     'itemCode',
@@ -68,175 +68,264 @@ const PAGE_COLUMNS = {
     'countedTotalQuantity',
     'difference',
     'reason',
-    'modalControl',
+    'batches',
   ],
 };
 
 const COLUMNS = () => ({
+  // CODE COLUMNS
   serialNumber: {
+    type: 'string',
     key: 'serialNumber',
     title: tableStrings.invoice_number,
     sortable: true,
-  },
-  supplier: {
-    key: 'otherPartyName',
-    title: tableStrings.supplier,
-    sortable: true,
-  },
-  customer: {
-    key: 'otherPartyName',
-    title: tableStrings.customer,
-    sortable: true,
-  },
-  comment: {
-    key: 'comment',
-    title: tableStrings.comment,
-    lines: 2,
+    editable: false,
   },
   itemCode: {
+    type: 'string',
     key: 'itemCode',
     title: tableStrings.item_code,
     sortable: true,
+    editable: false,
   },
+  code: {
+    type: 'string',
+    key: 'code',
+    title: tableStrings.code,
+    alignText: 'left',
+    sortable: true,
+    editable: false,
+  },
+
+  // STRING COLUMNS
+
   itemName: {
+    type: 'string',
     key: 'itemName',
     title: tableStrings.item_name,
     sortable: true,
+    editable: false,
   },
+  name: {
+    type: 'string',
+    key: 'name',
+    title: tableStrings.name,
+    alignText: 'left',
+    sortable: true,
+    editable: false,
+  },
+  supplier: {
+    type: 'string',
+    key: 'otherPartyName',
+    title: tableStrings.supplier,
+    sortable: true,
+    editable: false,
+  },
+  customer: {
+    type: 'string',
+    key: 'otherPartyName',
+    title: tableStrings.customer,
+    sortable: true,
+    editable: false,
+  },
+  comment: {
+    type: 'string',
+    key: 'comment',
+    title: tableStrings.comment,
+    lines: 2,
+    editable: false,
+  },
+  unit: {
+    type: 'string',
+    key: 'unit',
+    title: tableStrings.unit,
+    alignText: 'center',
+    sortable: false,
+    editable: false,
+  },
+  status: {
+    type: 'string',
+    key: 'status',
+    title: tableStrings.status,
+    sortable: true,
+    editable: false,
+  },
+
+  // EDITABLE STRING COLUMNS
+
+  batchName: {
+    type: 'editableString',
+    key: 'batch',
+    title: tableStrings.batch_name,
+    alignText: 'center',
+  },
+
+  // NUMERIC COLUMNS
+
   availableQuantity: {
+    type: 'numeric',
     key: 'availableQuantity',
     title: tableStrings.available_stock,
-    sortable: true,
     alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  numberOfItems: {
+    type: 'numeric',
+    key: 'numberOfItems',
+    title: tableStrings.items,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  ourStockOnHand: {
+    type: 'numeric',
+    key: 'ourStockOnHand',
+    title: tableStrings.current_stock,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  suggestedQuantity: {
+    type: 'numeric',
+    key: 'suggestedQuantity',
+    title: tableStrings.suggested_quantity,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  difference: {
+    type: 'numeric',
+    key: 'difference',
+    title: tableStrings.difference,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  snapshotTotalQuantity: {
+    type: 'numeric',
+    key: 'snapshotTotalQuantity',
+    title: tableStrings.snapshot_quantity,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  theirStockOnHand: {
+    type: 'numeric',
+    key: 'stockOnHand',
+    title: tableStrings.their_stock,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+  price: {
+    type: 'numeric',
+    key: 'price',
+    title: tableStrings.price,
+    alignText: 'center',
+    editable: false,
+    sortable: true,
+  },
+  monthlyUsage: {
+    type: 'numeric',
+    key: 'monthlyUsage',
+    title: tableStrings.monthly_usage,
+    alignText: 'right',
+    sortable: true,
+    editable: false,
+  },
+
+  // EDITABLE NUMERIC COLUMNS
+
+  requiredQuantity: {
+    type: 'editableNumeric',
+    key: 'requiredQuantity',
+    title: tableStrings.required_quantity,
+    alignText: 'right',
+    sortable: true,
+    editable: true,
+  },
+  countedTotalQuantity: {
+    type: 'editableNumeric',
+    key: 'countedTotalQuantity',
+    title: tableStrings.actual_quantity,
+    alignText: 'right',
+    sortable: true,
+    editable: true,
   },
   totalQuantity: {
+    type: 'editableNumeric',
     key: 'totalQuantity',
-    type: 'editable',
     title: tableStrings.quantity,
-    sortable: true,
     alignText: 'right',
+    sortable: true,
+    editable: true,
   },
-  remove: {
-    key: 'remove',
-    type: 'checkable',
-    title: tableStrings.remove,
-    alignText: 'center',
-  },
-  editableExpiryDate: {
-    key: 'expiryDate',
-    type: 'editableDate',
-    title: tableStrings.batch_expiry,
-    alignText: 'center',
+
+  // DATE COLUMNS
+
+  createdDate: {
+    type: 'date',
+    key: 'createdDate',
+    title: tableStrings.created_date,
+    alignText: 'left',
+    sortable: true,
+    editable: false,
   },
   entryDate: {
     type: 'date',
     key: 'entryDate',
     title: tableStrings.entered_date,
     sortable: true,
+    editable: false,
   },
-  numberOfItems: {
-    key: 'numberOfItems',
-    title: tableStrings.items,
-    sortable: true,
-    alignText: 'right',
-  },
-  status: {
-    type: 'status',
-    key: 'status',
-    title: tableStrings.status,
-    sortable: true,
-  },
-  ourStockOnHand: {
-    key: 'ourStockOnHand',
-    title: tableStrings.current_stock,
-    sortable: true,
-    alignText: 'right',
-  },
-  monthlyUsage: {
-    key: 'monthlyUsage',
-    title: tableStrings.monthly_usage,
-    sortable: true,
-    alignText: 'right',
-  },
-  suggestedQuantity: {
-    key: 'suggestedQuantity',
-    title: tableStrings.suggested_quantity,
-    sortable: true,
-    alignText: 'right',
-  },
-  requiredQuantity: {
-    type: 'editable',
-    key: 'requiredQuantity',
-    title: tableStrings.required_quantity,
-    sortable: true,
-    alignText: 'right',
-  },
-  price: {
-    key: 'price',
-    title: tableStrings.price,
+
+  // EDITABLE DATE COLUMNS
+  expiryDate: {
+    type: 'editableDate',
+    key: 'expiryDate',
+    title: tableStrings.batch_expiry,
     alignText: 'center',
+    sortable: false,
+    editable: true,
   },
-  unit: {
-    key: 'unit',
-    title: tableStrings.unit,
-    alignText: 'center',
-  },
-  code: {
-    key: 'code',
-    title: tableStrings.code,
-    alignText: 'left',
-    sortable: true,
-  },
-  name: {
-    key: 'name',
-    title: tableStrings.name,
-    alignText: 'left',
-    sortable: true,
-  },
-  createdDate: {
-    key: 'createdDate',
-    type: 'date',
-    title: tableStrings.created_date,
-    alignText: 'left',
-    sortable: true,
-  },
-  selected: {
-    key: 'selected',
+
+  // CHECKABLE COLUMNS
+  remove: {
     type: 'checkable',
+    key: 'remove',
+    title: tableStrings.remove,
+    alignText: 'center',
+    sortable: false,
+    editable: false,
+  },
+
+  selected: {
+    type: 'checkable',
+    key: 'selected',
     title: tableStrings.selected,
     alignText: 'center',
+    sortable: false,
+    editable: false,
   },
-  difference: {
-    key: 'difference',
-    title: tableStrings.difference,
-    sortable: true,
-    alignText: 'right',
-  },
-  countedTotalQuantity: {
-    type: 'editable',
-    key: 'countedTotalQuantity',
-    title: tableStrings.actual_quantity,
-    sortable: true,
-    alignText: 'right',
-  },
-  snapshotTotalQuantity: {
-    key: 'snapshotTotalQuantity',
-    title: tableStrings.snapshot_quantity,
-    sortable: true,
-    alignText: 'right',
-  },
-  modalControl: {
-    key: 'modalControl',
-    type: 'modalControl',
+
+  // MISC COLUMNS
+
+  batches: {
+    type: 'icon',
+    key: 'batch',
     title: tableStrings.batches,
     sortable: false,
     alignText: 'center',
+    editable: false,
   },
   reason: {
-    type: 'reason',
+    type: 'dropDown',
     key: 'mostUsedReasonTitle',
     title: tableStrings.reason,
     alignText: 'right',
+    sortable: false,
+    editable: false,
   },
 });
 
