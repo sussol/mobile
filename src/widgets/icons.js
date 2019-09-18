a995d732e0d3833aecd359a01683c9344d480f83
josh <griffinjoshua5@gmail.com>
2019-09-19 09:28:14 +1200

    Add correct icon name

--- src/widgets/icons.js
+++ src/widgets/icons.js
@@ -16,8 +16,9 @@ import { SUSSOL_ORANGE, dataTableColors } from '../globalStyles';
 export const SortAscIcon = <FAIcon name="sort-asc" size={15} style={{ marginRight: 10 }} />;
 export const SortNeutralIcon = <FAIcon name="sort" size={15} style={{ marginRight: 10 }} />;
 export const SortDescIcon = <FAIcon name="sort-desc" size={15} style={{ marginRight: 10 }} />;
+
 export const CheckedComponent = (
-  <IonIcon name="md-radio-button-on" size={15} color={dataTableColors.checkableCellChecked} />
+  <IonIcon name="md-remove-circle" size={15} color={dataTableColors.checkableCellChecked} />
 );
 export const UncheckedComponent = (
   <IonIcon name="md-radio-button-off" size={15} color={dataTableColors.checkableCellUnchecked} />
