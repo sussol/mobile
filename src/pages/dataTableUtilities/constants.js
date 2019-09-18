59b8e20bfe9f4ceadc006ddc107ad81aa25c57c2
Will Thomson <will.tho@outlook.com>
2019-09-18 12:36:31 +1200

    Add datatable type and key constants to dataTableUtilities

---
+++ src/pages/dataTableUtilities/constants.js
@@ -0,0 +1,48 @@
+// @ts-check
+
+export const COLUMN_TYPES = {
+  CHECKABLE: 'checkable',
+  DATE_EDITABLE: 'editableDate',
+  DATE: 'date',
+  DROP_DOWN: 'dropdown',
+  ICON: 'icon',
+  NUMERIC_EDITABLE: 'editableNumeric',
+  NUMERIC: 'numeric',
+  STRING_EDITABLE: 'editableString',
+  STRING: 'string',
+};
+
+export const COLUMN_KEYS = {
+  AVAILABLE_QUANTITY: 'availableQuantity',
+  BATCH: 'batch',
+  CODE: 'code',
+  COMMENT: 'comment',
+  COUNTED_TOTAL_QUANTITY: 'countedTotalQuantity',
+  CREATED_DATE: 'createdDate',
+  DIFFERENCE: 'difference',
+  ENTRY_DATE: 'entryDate',
+  EXPIRY_DATE: 'expiryDate',
+  ITEM_CODE: 'itemCode',
+  ITEM_NAME: 'itemName',
+  MONTHLY_USAGE: 'monthlyUsage',
+  NAME: 'name',
+  NUMBER_OF_ITEMS: 'numberOfItems',
+  OTHER_PARTY_NAME: 'otherPartyName',
+  OUR_STOCK_ON_HAND: 'ourStockOnHand',
+  PRICE: 'price',
+  REASON_TITLE: 'reasonTitle',
+  REMOVE: 'remove',
+  REQUIRED_QUANTITY: 'requiredQuantity',
+  SELECTED: 'selected',
+  SERIAL_NUMBER: 'serialNumber',
+  SNAPSHOT_TOTAL_QUANTITY: 'snapshotTotalQuantity',
+  STATUS: 'status',
+  STOCK_ON_HAND: 'stockOnHand',
+  SUGGESTED_QUANTITY: 'suggestedQuantity',
+  SUPPLIED_QUANTITY: 'suppliedQuantity',
+  TOTAL_QUANTITY: 'totalQuantity',
+  UNIT: 'unit',
+  VALUE: 'value',
+};
+
+export default { COLUMN_TYPES, COLUMN_KEYS };
