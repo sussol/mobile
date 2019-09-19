dc440119c44f1ee021b0bfab3b2c3e85e55b5514
josh <griffinjoshua5@gmail.com>
2019-09-19 14:13:17 +1200

    Add use correct column types in DataTableRow

--- src/widgets/DataTable/DataTableRow.js
+++ src/widgets/DataTable/DataTableRow.js
@@ -87,8 +87,8 @@ const DataTableRow = React.memo(
           const cellAlignment = alignText || 'left';
 
           switch (type) {
-            case COLUMN_TYPES.STRING_EDITABLE:
-            case COLUMN_TYPES.NUMERIC_EDITABLE:
+            case COLUMN_TYPES.EDITABLE_STRING:
+            case COLUMN_TYPES.EDITABLE_NUMERIC:
               return (
                 <TextInputCell
                   key={columnKey}
@@ -112,7 +112,7 @@ const DataTableRow = React.memo(
                 />
               );
 
-            case COLUMN_TYPES.DATE_EDITABLE:
+            case COLUMN_TYPES.EXPIRY_DATE_EDITABLE:
               return (
                 <NewExpiryDateInput
                   key={columnKey}
