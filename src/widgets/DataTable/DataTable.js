012e1d4d4eb4d45b8739c80565366ff6f741d8b8
josh <griffinjoshua5@gmail.com>
2019-09-20 09:30:53 +1200

    Add keyboard logic to focusNextCell

--- src/widgets/DataTable/DataTable.js
+++ src/widgets/DataTable/DataTable.js
@@ -6,7 +6,7 @@
 
 import PropTypes from 'prop-types';
 import React, { useMemo, useRef, useCallback } from 'react';
-import { StyleSheet, VirtualizedList, VirtualizedListPropTypes } from 'react-native';
+import { StyleSheet, VirtualizedList, VirtualizedListPropTypes, Keyboard } from 'react-native';
 import RefContext from './RefContext';
 
 /**
@@ -69,11 +69,13 @@ const DataTable = React.memo(({ renderRow, renderHeader, style, data, columns, .
     return newRef;
   };
 
-  // Focuses the next editable cell in the list. Back to the top on last row.
+  // Focuses the next editable cell in the list. On the last row, dismiss the keyboard.
   const focusNextCell = refIndex => {
+    if (refIndex + 1 === numberOfEditableCells) return Keyboard.dismiss();
+
     const cellRef = getCellRef((refIndex + 1) % numberOfEditableCells);
 
-    cellRef.current.focus();
+    return cellRef.current.focus();
   };
 
   // Adjusts the passed row to the top of the list.
