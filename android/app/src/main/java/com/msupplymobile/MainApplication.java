0a8fda49709a9f433463a0d4635635f2b82db51c
josh <griffinjoshua5@gmail.com>
2019-10-17 16:06:46 +1300

    Add removal of extraneous dependencies

--- android/app/src/main/java/com/msupplymobile/MainApplication.java
+++ android/app/src/main/java/com/msupplymobile/MainApplication.java
@@ -2,24 +2,15 @@ package com.msupplymobile;
 
 import android.app.Application;
 
-import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
-import com.facebook.react.ReactApplication;
-import com.bugsnag.BugsnagReactNative;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
-
-import java.util.Arrays;
 
 public class MainApplication extends Application implements ReactApplication {
 
