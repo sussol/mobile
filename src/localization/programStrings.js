512f48390bd2d5ad4c9f2ec0d93f4e620a1c70e0
josh <griffinjoshua5@gmail.com>
2019-10-19 23:19:51 +1300

    Add french translation (maybe)

--- src/localization/programStrings.js
+++ src/localization/programStrings.js
@@ -62,6 +62,7 @@ export const programStrings = new LocalizedStrings({
     general_stocktake: 'Inventaire général',
     general_order: 'Commande Générale',
     program_stocktake: 'Inventaire par\n programme',
+    emergency_orders: 'Urgence commandes',
   },
   gil: {
     select_a_program: 'Select a program',
