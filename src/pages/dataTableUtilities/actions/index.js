582423432e254b5810a46ee827c6e167ce3be325
josh <griffinjoshua5@gmail.com>
2019-11-04 17:16:51 +1300

    Add actions exporting

--- src/pages/dataTableUtilities/actions/index.js
+++ src/pages/dataTableUtilities/actions/index.js
@@ -3,6 +3,16 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
+import { CellActionsLookup } from './cellActions';
+import { RowActionsLookup } from './rowActions';
+import { TableActionsLookup } from './tableActions';
+import { PageActionsLookup } from './pageActions';
+
 export { ACTIONS } from './constants';
 
-export { getPageActions } from './getPageActions';
+export const PageActions = {
+  ...CellActionsLookup,
+  ...RowActionsLookup,
+  ...TableActionsLookup,
+  ...PageActionsLookup,
+};
