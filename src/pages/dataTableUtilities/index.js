b8b9d5d4df8dff44a60faa689a59f4a0517a80c4
josh <griffinjoshua5@gmail.com>
2019-11-04 17:19:43 +1300

    Add update dataTableUtils index

--- src/pages/dataTableUtilities/index.js
+++ src/pages/dataTableUtilities/index.js
@@ -2,18 +2,15 @@
  * mSupply Mobile
  * Sustainable Solutions (NZ) Ltd. 2019
  */
-
-export { getPageActions } from './actions';
-
 export { DataTablePageReducer } from './reducer';
-
 export { COLUMN_TYPES, COLUMN_NAMES, COLUMN_KEYS } from './constants';
-
 export { recordKeyExtractor, getItemLayout } from './utilities';
+export { PageActions } from './actions';
 
 /* eslint-disable import/first */
 import getColumns from './getColumns';
 import getPageInfoColumns from './getPageInfoColumns';
 import getPageInitialiser from './getPageInitialiser';
+import { getPageDispatchers } from './getPageDispatchers';
 
-export { getColumns, getPageInfoColumns, getPageInitialiser };
+export { getColumns, getPageInfoColumns, getPageInitialiser, getPageDispatchers };
