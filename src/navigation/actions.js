5d7b08aac2c2d315681d98b16e7468fa8e01c76c
josh <griffinjoshua5@gmail.com>
2019-11-05 23:35:10 +1300

    Add prescriber navigation action

--- src/navigation/actions.js
+++ src/navigation/actions.js
@@ -30,6 +30,12 @@ import { ROUTES } from './constants';
  *
  */
 
+export const gotoPrescribers = () =>
+  NavigationActions.navigate({
+    routeName: ROUTES.PRESCRIBERS,
+    params: { title: 'Prescribers' },
+  });
+
 /**
  * Action creator which first creates a customer invoice, and then navigates to it
  * for editing.
