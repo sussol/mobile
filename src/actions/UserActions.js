7a3c32002d3ad0980b49136c43f94cec905af1c8
josh <griffinjoshua5@gmail.com>
2019-11-10 16:19:24 +1300

    Add UserReducer

---
+++ src/actions/UserActions.js
@@ -0,0 +1,16 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+export const USER_ACTION_TYPES = {
+  AUTHENTICATE: 'USER/AUTHENTICATE',
+  FAILED_AUTH: 'USER/FAILED_AUTH',
+  LOG_IN: 'USER/LOG_IN',
+  LOG_OUT: 'USER/LOG_OUT',
+};
+
+export const UserActions = {
+  login: user => ({ type: USER_ACTION_TYPES.LOG_IN, payload: { user } }),
+  logout: () => ({ type: USER_ACTION_TYPES.LOG_OUT }),
+};
---
+++ src/reducers/UserReducer.js
@@ -0,0 +1,26 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import { USER_ACTION_TYPES } from '../actions/UserActions';
+
+const initialState = () => ({ currentUser: null });
+
+export const UserReducer = (state = initialState(), action) => {
+  const { type } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.LOG_IN: {
+      const { payload } = action;
+      const { user } = payload;
+
+      return { ...state, currentUser: user };
+    }
+    case USER_ACTION_TYPES.LOG_OUT: {
+      return { ...state, currentUser: null };
+    }
+    default:
+      return state;
+  }
+};
