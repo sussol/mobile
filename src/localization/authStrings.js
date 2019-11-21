ee2b9671b87d460f89a49abe9342c12b389fbbcb
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-11-22 12:01:50 +1300

    remove pushing_changes_text

--- src/localization/authStrings.js
+++ src/localization/authStrings.js
@@ -15,7 +15,6 @@ export const authStrings = new LocalizedStrings({
     email: 'E-mail Address',
     warning_sync_edit:
       'WARNING:\nThis will effect syncing for this store.\nMake sure you know what you doing!',
-    pushing_changes_text: 'Pushing changes to the server',
   },
   fr: {
     logging_in: 'Connexion en cours',
