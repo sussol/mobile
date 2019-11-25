275dc5b09b27d5441814aa423cddaa7472344dbf
josh <griffinjoshua5@gmail.com>
2019-11-25 15:04:10 +1300

    Add use of navigation focus custom hook in usePopOver hook

--- src/hooks/usePopover.js
+++ src/hooks/usePopover.js
@@ -1,15 +1,18 @@
 import React, { useState, useCallback, useRef } from 'react';
+import { useNavigationFocus } from './useNavigationFocus';
 
 /**
  * Hook which manages popover position over a DOM element.
  * Pass a ref created by React.createRef. Receive callbacks,
  */
-export const usePopover = () => {
+export const usePopover = navigation => {
   const [visibilityState, setVisibilityState] = useState(false);
   const ref = useRef(React.createRef());
 
   const showPopover = useCallback(() => setVisibilityState(true), []);
   const closePopover = useCallback(() => setVisibilityState(false), []);
+
+  useNavigationFocus(navigation, null, closePopover);
 
   return [ref, visibilityState, showPopover, closePopover];
 };
