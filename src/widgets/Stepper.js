b515195624478981dab92507c58a6e16cc372d29
josh <griffinjoshua5@gmail.com>
2019-11-29 12:40:34 +1300

    Add docstring

--- src/widgets/Stepper.js
+++ src/widgets/Stepper.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { TextInput, View, TouchableOpacity } from 'react-native';
 
-const Stepper = ({ value, onUpdate, lowerLimit, upperLimit }) => {
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import { SUSSOL_ORANGE, APP_FONT_FAMILY } from '../globalStyles/index';
+
+/**
+ * Stepper component to update a numerical value. The `value` passed will be
+ * cast to a Number, so should return a valid number when cast to `Number`.
+ *
+ * @prop {String|Number} value        The current numerical value
+ * @prop {Func}          onChangeText Callback for updating the value
+ * @prop {Number}        lowerLimit   The lower limit of the numerical range
+ * @prop {Number}        upperLimit   The upper limit of the numerical range
+ * @prop {Number}        iconSize     The size of the icons
+ * @prop {Number}        iconColour   The colour of the icons
+ *
+ */
+const Stepper = ({ value, onChangeText, lowerLimit, upperLimit, iconSize, iconColour }) => {
   const currentValue = React.useRef(Number(value));
   const currentAdjustmentAmount = React.useRef(1);
   const valueAdjustmentInterval = React.useRef();
   const adjustmentIncreaseInterval = React.useRef();
 
+  const onUpdate = newValue => {
+    currentValue.current = newValue;
+    onChangeText(newValue);
+  };
+
   const decrementValue = () => {
     if (lowerLimit >= currentValue.current) return;
     currentValue.current -= currentAdjustmentAmount.current;
-    onUpdate(currentValue.current);
+    onChangeText(currentValue.current);
   };
 
   const incrementValue = () => {
     if (currentValue.current > upperLimit) return;
     currentValue.current += currentAdjustmentAmount.current;
-    onUpdate(currentValue.current);
+    onChangeText(currentValue.current);
   };
 
   const inreaseIncrement = () => {
@@ -48,14 +67,23 @@ const Stepper = ({ value, onUpdate, lowerLimit, upperLimit }) => {
   };
 
   return (
-    <View>
-      <TouchableOpacity onPressIn={() => onStartingLongPress(true)} onPressOut={onEndLongPress}>
-        <View style={{ backgroundColor: 'red', width: 100, height: 100 }} />
-      </TouchableOpacity>
-
-      <TextInput onChangeText={onUpdate} value={String(currentValue.current)} />
+    <View style={{ flexDirection: 'row' }}>
       <TouchableOpacity onPressIn={() => onStartingLongPress(false)} onPressOut={onEndLongPress}>
-        <View style={{ backgroundColor: 'blue', width: 100, height: 100 }} />
+        <IonIcon name="ios-remove" size={iconSize} color={iconColour} />
+      </TouchableOpacity>
+      <TextInput
+        onChangeText={onUpdate}
+        value={String(value)}
+        style={{
+          marginHorizontal: 50,
+          width: 100,
+          textAlign: 'center',
+          fontSize: 16,
+          fontFamily: APP_FONT_FAMILY,
+        }}
+      />
+      <TouchableOpacity onPressIn={() => onStartingLongPress(true)} onPressOut={onEndLongPress}>
+        <IonIcon name="ios-add" size={iconSize} color={iconColour} />
       </TouchableOpacity>
     </View>
   );
@@ -64,13 +92,17 @@ const Stepper = ({ value, onUpdate, lowerLimit, upperLimit }) => {
 Stepper.defaultProps = {
   lowerLimit: 0,
   upperLimit: 99999,
+  iconSize: 45,
+  iconColour: SUSSOL_ORANGE,
 };
 
 Stepper.propTypes = {
   lowerLimit: PropTypes.number,
   upperLimit: PropTypes.number,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  onUpdate: PropTypes.func.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  iconSize: PropTypes.number,
+  iconColour: PropTypes.string,
 };
 
 export const TestPage = () => {
@@ -78,7 +110,7 @@ export const TestPage = () => {
   const onUpdate = x => setValue(String(x));
   return (
     <View>
-      <Stepper value={value} onUpdate={onUpdate} />
+      <Stepper value={value} onChangeText={onUpdate} />
     </View>
   );
 };
