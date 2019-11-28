df6e5e51fe019ca819bf8d6c897ea1c79430136d
josh <griffinjoshua5@gmail.com>
2019-11-29 12:49:17 +1300

    Add further styles, remove develop code and add more doc string clarifications

--- src/widgets/Stepper.js
+++ src/widgets/Stepper.js
@@ -1,23 +1,37 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextInput, View, TouchableOpacity } from 'react-native';
+import { TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import { SUSSOL_ORANGE, APP_FONT_FAMILY } from '../globalStyles/index';
 
 /**
  * Stepper component to update a numerical value. The `value` passed will be
- * cast to a Number, so should return a valid number when cast to `Number`.
+ * cast, so should return a valid number when cast to `Number`.
  *
- * @prop {String|Number} value        The current numerical value
- * @prop {Func}          onChangeText Callback for updating the value
- * @prop {Number}        lowerLimit   The lower limit of the numerical range
- * @prop {Number}        upperLimit   The upper limit of the numerical range
- * @prop {Number}        iconSize     The size of the icons
- * @prop {Number}        iconColour   The colour of the icons
+ * Has increment and decrement buttons which increase the passed value by one.
+ * The buttons can also be held, increase by one every 50ms. The increment amount
+ * then grows exponentially every 500ms, reseting to one after releasing.
+ *
+ * @prop {String|Number} value          The current numerical value.
+ * @prop {Func}          onChangeText   Callback for updating the value.
+ * @prop {Number}        lowerLimit     The lower limit of the numerical range.
+ * @prop {Number}        upperLimit     The upper limit of the numerical range.
+ * @prop {Number}        iconSize       The size of the icons.
+ * @prop {Number}        iconColour     The colour of the icons.
+ * @prop {Object}        textInputStyle Style object for the underlying TextInput.
  *
  */
-const Stepper = ({ value, onChangeText, lowerLimit, upperLimit, iconSize, iconColour }) => {
+const Stepper = ({
+  value,
+  onChangeText,
+  lowerLimit,
+  upperLimit,
+  iconSize,
+  iconColour,
+  textInputStyle,
+}) => {
   const currentValue = React.useRef(Number(value));
   const currentAdjustmentAmount = React.useRef(1);
   const valueAdjustmentInterval = React.useRef();
@@ -66,34 +80,39 @@ const Stepper = ({ value, onChangeText, lowerLimit, upperLimit, iconSize, iconCo
     currentAdjustmentAmount.current = 1;
   };
 
+  const onStartDecrementPress = () => onStartingLongPress(false);
+  const onStartIncrementPress = () => onStartingLongPress(true);
+
   return (
-    <View style={{ flexDirection: 'row' }}>
-      <TouchableOpacity onPressIn={() => onStartingLongPress(false)} onPressOut={onEndLongPress}>
+    <View style={localStyles.row}>
+      <TouchableOpacity onPressIn={onStartDecrementPress} onPressOut={onEndLongPress}>
         <IonIcon name="ios-remove" size={iconSize} color={iconColour} />
       </TouchableOpacity>
-      <TextInput
-        onChangeText={onUpdate}
-        value={String(value)}
-        style={{
-          marginHorizontal: 50,
-          width: 100,
-          textAlign: 'center',
-          fontSize: 16,
-          fontFamily: APP_FONT_FAMILY,
-        }}
-      />
-      <TouchableOpacity onPressIn={() => onStartingLongPress(true)} onPressOut={onEndLongPress}>
+      <TextInput onChangeText={onUpdate} value={String(value)} style={{ textInputStyle }} />
+      <TouchableOpacity onPressIn={onStartIncrementPress} onPressOut={onEndLongPress}>
         <IonIcon name="ios-add" size={iconSize} color={iconColour} />
       </TouchableOpacity>
     </View>
   );
 };
 
+const localStyles = StyleSheet.create({
+  row: { flexDirection: 'row' },
+  textInput: {
+    marginHorizontal: 50,
+    width: 100,
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: APP_FONT_FAMILY,
+  },
+});
+
 Stepper.defaultProps = {
   lowerLimit: 0,
   upperLimit: 99999,
   iconSize: 45,
   iconColour: SUSSOL_ORANGE,
+  textInputStyle: localStyles.textInput,
 };
 
 Stepper.propTypes = {
@@ -103,14 +122,5 @@ Stepper.propTypes = {
   onChangeText: PropTypes.func.isRequired,
   iconSize: PropTypes.number,
   iconColour: PropTypes.string,
-};
-
-export const TestPage = () => {
-  const [value, setValue] = React.useState('0');
-  const onUpdate = x => setValue(String(x));
-  return (
-    <View>
-      <Stepper value={value} onChangeText={onUpdate} />
-    </View>
-  );
+  textInputStyle: PropTypes.object,
 };
