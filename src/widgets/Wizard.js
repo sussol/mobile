9ecc1bc8d95edbf5b3bf2aaf28ab35cbbb272cea
josh <griffinjoshua5@gmail.com>
2019-12-01 17:13:06 +1300

    Add Wizard component

---
+++ src/widgets/Wizard.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { StepsTracker } from './StepsTracker';
+import { TabNavigator } from './TabNavigator';
+import DataTablePageView from './DataTablePageView';
+
+/**
+ * Layout component for a Tracker and TabNavigator, displaying steps
+ * to completion for completion. See TabNavigator and StepsTracker
+ * for individual component implementation.
+ */
+const Wizard = ({ tabs, titles, onPress, currentTabIndex }) => (
+  <DataTablePageView>
+    <StepsTracker
+      numberOfSteps={tabs.length}
+      currentStep={currentTabIndex}
+      onPress={onPress}
+      title={titles[currentTabIndex]}
+    />
+    <TabNavigator tabs={tabs} currentTabIndex={currentTabIndex} />
+  </DataTablePageView>
+);
+
+Wizard.propTypes = {
+  tabs: PropTypes.array.isRequired,
+  titles: PropTypes.array.isRequired,
+  onPress: PropTypes.func.isRequired,
+  currentTabIndex: PropTypes.number.isRequired,
+};
