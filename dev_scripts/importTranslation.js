7cebd0b0b68e6079033e8165010bed3be4bb872a
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-12-10 10:03:51 +1300

    refactor for loops, change for map

--- dev_scripts/importTranslation.js
+++ dev_scripts/importTranslation.js
@@ -1,3 +1,6 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-useless-escape */
+/* eslint-disable array-callback-return */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 /* eslint-disable no-return-assign */
@@ -12,11 +15,24 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
+const fs = require('fs');
+
 const localizationFiles = [];
+let localizationFileName = [];
+const keys = [];
+const values = [];
+let key = null;
+let value = null;
+// To identify {nextLine} in the csv files
+const nextLine = /{nextLine}/gi;
+// To identify {emptySpace} in the csv files
+const emptySpace = /{emptySpace}/gi;
 let index = 0;
-require('fs')
-  .readdirSync('././src/localization/')
-  .forEach(file => {
+
+getLocalizationFiles = () => {
+  // Reads all files in localization files and save them in localizationFiles array
+  fs.readdirSync('././src/localization/').forEach(file => {
+    // Filter to only use get JSON files
     if (file.split('.').pop() === 'json') {
       const fileName = require(`../src/localization/${file}`);
       localizationFiles.push(fileName);
@@ -24,65 +40,73 @@ require('fs')
       index += 1;
     }
   });
+};
 
-// get the third command line argument
-const language = process.argv[2];
+// Gets the language entered by the User (fr, la, gil, tl)
+getSelectedLanguage = () => process.argv[2];
 
-// read the file chosen by the user
-const fileContent = fs.readFileSync(`./src/localization/translations/${language}.csv`, 'utf8');
+UpdateJsonFiles = selectedLanguage => {
+  // read the file chosen by the user
+  const fileContent = fs.readFileSync(
+    `./src/localization/translations/${selectedLanguage}.csv`,
+    'utf8'
+  );
 
-// it creates one string per line in arrayOfData
-const arrayOfData = fileContent.toString().split('\n');
+  // Creates one string per line available in the csv file
+  const arrayOfData = fileContent.toString().split('\n');
 
-let localizationFileName = [];
-const keys = [];
-const values = [];
-let key;
-let value;
-const nextLine = /{nextLine}/gi;
-const emptySpace = /{emptySpace}/gi;
-
-for (localizationFile of localizationFiles) {
-  for (indexOfArrayOfData in arrayOfData) {
-    const data = JSON.stringify(arrayOfData[indexOfArrayOfData])
-      .replace(/"/g, '')
-      .split(',');
-
-    // [ key, value in english, value in other language ] of line i of whole csv file
-    data[indexOfArrayOfData] = arrayOfData[indexOfArrayOfData].split(',').slice(1);
-    // authStrings, buttonStrings, etc.
-    localizationFileName = arrayOfData[indexOfArrayOfData].split(',').slice(0)[0];
-
-    if (localizationFile.name === localizationFileName) {
-      key = data[indexOfArrayOfData][0];
-      value = data[indexOfArrayOfData][2]
-        .replace(nextLine, '\n')
-        .replace(emptySpace, '"')
+  // Go through all localizationFiles
+  localizationFiles.map(localizationFile => {
+    arrayOfData.map((arrayOfData, indexOfArrayOfData) => {
+      const data = JSON.stringify(arrayOfData)
+        .replace(/"/g, '')
+        .split(',');
+      // [ key, value in english, value in other language ] of line i of whole csv file
+      data[indexOfArrayOfData] = arrayOfData.split(',').slice(1);
+      // authStrings, buttonStrings, etc.
+      localizationFileName = arrayOfData
+        .split(',')
+        .slice(0)[0]
         .trim();
-      // if there is a key and value related with it, push changes in keys and values
-      if (value !== '') {
-        keys.push(key);
-        values.push(value);
+      if (localizationFile.name === localizationFileName) {
+        key = data[indexOfArrayOfData][0];
+        value = data[indexOfArrayOfData][2]
+          .replace(nextLine, '\n')
+          .replace(emptySpace, '\"')
+          .trim();
+        // Checks if the value in empty or there is some text to save
+        if (value !== '') {
+          keys.push(key);
+          values.push(value);
+        }
       }
-    }
-  }
+    });
+    const resultObject = {};
+    keys.forEach((key, value) => (resultObject[key] = values[value]));
+    // Updates strings related with the selectedLanguage in localizationFile
+    localizationFile[selectedLanguage] = resultObject;
+    // Cleans both arrays to be ready to use in next localizationFile
+    keys.length = 0;
+    values.length = 0;
 
-  const resultObject = {};
-  keys.forEach((key, value) => (resultObject[key] = values[value]));
-  // update the correct object in the localization file
-  localizationFile[language] = resultObject;
+    // Keeps the localizationFile name and removes the one added to localizationFile
+    const fileName = localizationFile.name;
+    delete localizationFile.name;
 
-  keys.length = 0;
-  values.length = 0;
-
-  const fileName = localizationFile.name;
-  delete localizationFile.name;
-
-  // save changes in each localization file (authStrings, buttonStrings, etc)
-  localizationFile = JSON.stringify(localizationFile, null, 2);
-  fs.writeFile(`./src/localization/${fileName}.json`, localizationFile, err => {
-    if (err) throw err;
-    // eslint-disable-next-line no-console
-    console.log(`The file ${fileName}.json has been saved`);
+    // Updates localizationFile
+    localizationFile = JSON.stringify(localizationFile, null, 2);
+    fs.writeFile(`./src/localization/${fileName}.json`, localizationFile, err => {
+      if (err) throw err;
+      // eslint-disable-next-line no-console
+      console.log(`The file ${fileName}.json has been saved`);
+    });
   });
-}
+};
+
+executeImportTranslation = () => {
+  getLocalizationFiles();
+  const selectedLanguage = getSelectedLanguage();
+  UpdateJsonFiles(selectedLanguage);
+};
+
+executeImportTranslation();
