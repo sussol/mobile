e6b682d3c61d016e371b26a0f0ca28d3718f3528
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-12-18 11:23:57 +1300

    Add function to check object type

---
+++ src/utilities/checkIsObject.js
@@ -0,0 +1,10 @@
+/**
+ * Check if the given parameter is an Object type {} and
+ * return a boolean.
+ *
+ * @param   {object}  object  The object to check.
+ *
+ */
+
+export const checkIsObject = object =>
+  object && typeof dataObject === 'object' && object.constructor === Object;
