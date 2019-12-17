81053ae57d099a79a7b181fc548f4369bfe8e61c
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-12-17 16:22:57 +1300

    Unlink react-native-svg

--- android/app/src/main/java/com/msupplymobile/MainApplication.java
+++ android/app/src/main/java/com/msupplymobile/MainApplication.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
