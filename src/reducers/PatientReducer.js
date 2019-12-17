37f58c6a959593b945136d5b2cdb1047637d6fb6
josh <griffinjoshua5@gmail.com>
2019-12-18 12:23:38 +1300

    Add removal of unused logic from PatientReducer

--- src/reducers/PatientReducer.js
+++ src/reducers/PatientReducer.js
@@ -9,80 +9,16 @@ const patientInitialState = () => ({
   currentPatient: null,
   isEditing: false,
   isCreating: false,
-  isValid: {
-    code: true,
-    dateOfBirth: true,
-    country: true,
-    addressOne: true,
-    addressTwo: true,
-  },
-  hasValue: {
-    code: false,
-    firstName: false,
-    lastName: false,
-  },
-  firstName: '',
-  lastName: '',
-  code: '',
-  dateOfBirth: '',
-  emailAddress: '',
-  phoneNumber: '',
-  addressOne: '',
-  addressTwo: '',
-  country: '',
 });
 
 export const PatientReducer = (state = patientInitialState(), action) => {
   const { type } = action;
 
   switch (type) {
-    case PATIENT_ACTIONS.FIELD_VALIDITY: {
-      const { payload } = action;
-      const { field, validity } = payload;
-      const { isValid } = state;
-
-      const newValidityState = { ...isValid, [field]: validity };
-
-      return { ...state, isValid: newValidityState };
-    }
-
-    case PATIENT_ACTIONS.FIELD_UPDATE: {
-      const { payload } = action;
-      const { field, value } = payload;
-      const { hasValue } = state;
-
-      const isRequired = field in hasValue;
-
-      if (isRequired) {
-        return { ...state, hasValue: { ...hasValue, [field]: !!value }, [field]: value };
-      }
-
-      return { ...state, [field]: value };
-    }
-
     case PATIENT_ACTIONS.PATIENT_EDIT: {
-      const { hasValue } = state;
       const { payload } = action;
       const { patient } = payload;
-      const { firstName, lastName, emailAddress, code, phoneNumber } = patient;
-
-      const requiredKeys = Object.keys(hasValue);
-      const newHasValue = requiredKeys.reduce(
-        (acc, hasValueKey) => ({ ...acc, [hasValueKey]: !!patient[hasValueKey] }),
-        {}
-      );
-
-      return {
-        ...state,
-        isEditing: true,
-        currentPatient: patient,
-        firstName,
-        lastName,
-        emailAddress,
-        code,
-        phoneNumber,
-        hasValue: newHasValue,
-      };
+      return { ...state, isEditing: true, currentPatient: patient };
     }
 
     case PATIENT_ACTIONS.PATIENT_CREATION: {
