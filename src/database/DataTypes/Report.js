41be9be6560c3040c3733229a78494b4a9e7c37a
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-12-18 09:10:34 +1300

    Add getter and setter for data

--- src/database/DataTypes/Report.js
+++ src/database/DataTypes/Report.js
@@ -2,6 +2,47 @@
  * mSupply Mobile
  * Sustainable Solutions (NZ) Ltd. 2019
  */
+
 import Realm from 'realm';
 
-export class Report extends Realm.Object {}
+/**
+ * A Report.
+ *
+ * @property  {string}            id
+ * @property  {string}            type
+ * @property  {string}            title
+ * @property  {string}            _data
+ */
+
+export class Report extends Realm.Object {
+  /**
+   * Get data.
+   *
+   * @return  {Object}
+   */
+  get data() {
+    return JSON.parse(this._data);
+  }
+
+  /**
+   * Set data.
+   *
+   * @param  {dataObject}  dataObject
+   */
+  set data(dataObject) {
+    this._data = JSON.stringify(dataObject);
+  }
+}
+
+Report.schema = {
+  name: 'Report',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    type: 'string',
+    title: 'string',
+    _data: 'string',
+  },
+};
+
+export default Report;
