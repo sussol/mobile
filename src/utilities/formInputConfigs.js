16158b8d666aa6954d728e2141987c55df024e3d
josh <griffinjoshua5@gmail.com>
2019-12-18 13:51:09 +1300

    Add formInputConfigs docstring

--- src/utilities/formInputConfigs.js
+++ src/utilities/formInputConfigs.js
@@ -3,6 +3,25 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
+/**
+ * File contains constants and config objects which declaritively define
+ * form inputs to be used with `FormControl`.
+ *
+ * For a particular form, add a field to FIELD_CONFIGS, which will define what
+ * form input configs to use.
+ *
+ * FormInputConfig have the following structure:
+ * {
+ *     type: 'text' : A ValidationTextInput component
+ *     initialValue : The value to seed the form input component with.
+ *     key: A key for the underlying record.
+ *     isRequired : Indicator whether this input is required to be filled for completion.
+ *     label: A label for the form input.
+ *     invalidMessage: A message when the input is invalid.
+ *     validator: A function to call to determine if the current input is valid.
+ * }
+ */
+
 const FORM_INPUT_KEYS = {
   FIRST_NAME: 'firstName',
   LAST_NAME: 'lastName',
