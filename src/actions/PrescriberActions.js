9543cb9d4ff652a0d4d83e18005d49bdddf3a44c
josh <griffinjoshua5@gmail.com>
2019-12-18 21:17:35 +1300

    Add prescriber actions

---
+++ src/actions/PrescriberActions.js
@@ -0,0 +1,58 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+import { UIDatabase, generateUUID } from '../database';
+import { PageActions } from '../pages/dataTableUtilities/actions';
+import { ROUTES } from '../navigation/constants';
+
+export const PRESCRIBER_ACTIONS = {
+  EDIT: 'Prescriber/edit',
+  CREATE: 'Prescriber/create',
+  COMPLETE: 'Prescriber/complete',
+};
+
+const closeModal = () => ({ type: PRESCRIBER_ACTIONS.COMPLETE });
+
+const createPrescriber = () => ({ type: PRESCRIBER_ACTIONS.CREATE });
+
+const editPrescriber = prescriber => ({
+  type: PRESCRIBER_ACTIONS.EDIT,
+  payload: { prescriber },
+});
+
+const updatePrescriber = completedForm => (dispatch, getState) => {
+  const { prescriber } = getState();
+  const { currentPrescriber } = prescriber;
+
+  UIDatabase.write(() => {
+    UIDatabase.update('Prescriber', {
+      ...currentPrescriber,
+      ...completedForm,
+    });
+  });
+
+  dispatch(closeModal());
+  dispatch(PageActions.refreshData(ROUTES.DISPENSARY));
+};
+
+const saveNewPrescriber = completedForm => dispatch => {
+  UIDatabase.write(() => {
+    UIDatabase.update('Prescriber', {
+      id: generateUUID(),
+      ...completedForm,
+    });
+  });
+
+  dispatch(closeModal());
+  dispatch(PageActions.refreshData(ROUTES.DISPENSARY));
+};
+
+export const PrescriberActions = {
+  createPrescriber,
+  updatePrescriber,
+  editPrescriber,
+  closeModal,
+  saveNewPrescriber,
+};
