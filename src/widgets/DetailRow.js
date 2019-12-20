73751d0976f90d46464959fa1c8848f493aa2264
josh <griffinjoshua5@gmail.com>
2019-12-20 14:33:13 +1300

    Add detail row style adjustments

--- src/widgets/DetailRow.js
+++ src/widgets/DetailRow.js
@@ -22,7 +22,7 @@ export const DetailRow = ({ details }) => {
   const Details = React.useCallback(
     () =>
       details.map(({ label, text }) => (
-        <View>
+        <View style={{ flex: 1 }}>
           <SimpleLabel label={label} size="small" text={text} />
         </View>
       )),
@@ -40,6 +40,8 @@ const localStyles = {
   rowContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'flex-start',
+    flex: 1,
   },
 };
 
