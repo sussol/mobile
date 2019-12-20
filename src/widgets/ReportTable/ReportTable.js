cc80e7bfd3ec242777ef9c9e95a628805e710ae3
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2019-12-20 14:33:55 +1300

    Create component ReportTable

--- src/widgets/ReportTable/ReportTable.js
+++ src/widgets/ReportTable/ReportTable.js
@@ -1,91 +1,37 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/forbid-prop-types */
 /**
  * mSupply Mobile
  * Sustainable Solutions (NZ) Ltd. 2018
  */
 
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, FlatList, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { VictoryChart, VictoryBar, VictoryAxis } from 'victory-native';
-import { APP_FONT_FAMILY, GREY, LIGHT_GREY, DARK_GREY, SUSSOL_ORANGE } from '../../globalStyles';
+import { LIGHT_GREY } from '../../globalStyles';
+import { ReportRow } from './ReportRow';
 
-export const ReportTable = ({ title, type, data }) => {
-  const [dimensions, setDimensions] = useState({ height: null, width: null });
-  const { height, width } = dimensions;
+export const ReportTable = ({ rows, header }) => {
+  const renderItem = ({ item, index }) => <ReportRow rowData={item} rowIndex={index} />;
 
-  // Victory Native sizes are set using absolute values. Parents dimensions are used to
-  // calculate relative values for width and height for each chart.
-  const onLayout = event => {
-    const newDimensionsObj = {
-      height: event.nativeEvent.layout.width,
-      width: event.nativeEvent.layout.height,
-    };
-    setDimensions(newDimensionsObj);
-  };
+  const renderHeader = () => <ReportRow rowData={header} isHeader rowIndex={0} />;
 
-  const { padTop, padBottom, padLeft, padRight, padDomain, style } = victoryStyles.barChart;
-
-  const padding = {
-    top: height * padTop,
-    bottom: height * padBottom,
-    left: width * padLeft,
-    right: width * padRight,
-  };
-
-  const chartDimensions = width * (1 - (padLeft + padRight));
-  const domainPadding = chartDimensions * padDomain;
-
-  const { rows, header } = data;
-
+  // TODO: KeyExtractor should be altered to not use the index.
   return (
-    <View style={localStyles.ChartContainer} onLayout={onLayout}>
-      <ReportTable rows={rows} headers={header} />;
+    <View style={localStyles.container}>
+      {renderHeader()}
+      <FlatList data={rows} renderItem={renderItem} keyExtractor={(_, index) => `${index}`} />
     </View>
   );
 };
 
 ReportTable.propTypes = {
-  title: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  data: PropTypes.array.isRequired,
+  header: PropTypes.array.isRequired,
+  rows: PropTypes.array.isRequired,
 };
 
 const localStyles = StyleSheet.create({
-  ChartContainer: {
-    width: '75%',
-    minHeight: '100%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {
+    backgroundColor: LIGHT_GREY,
   },
 });
-
-const victoryStyles = {
-  axisX: {
-    fixLabelOverlap: true,
-    style: {
-      axis: { stroke: LIGHT_GREY },
-      ticks: { stroke: DARK_GREY },
-      tickLabels: { fontFamily: APP_FONT_FAMILY, fill: GREY, textAnchor: 'end', angle: -45 },
-    },
-  },
-  axisY: {
-    fixLabelOverlap: false,
-    style: {
-      axis: { stroke: LIGHT_GREY },
-      ticks: { stroke: DARK_GREY },
-      tickLabels: { fontFamily: APP_FONT_FAMILY, fill: GREY },
-    },
-  },
-  barChart: {
-    padTop: 0.1,
-    padBottom: 0.15,
-    padLeft: 0.1,
-    padRight: 0.05,
-    padDomain: 0.05,
-    style: { data: { fill: SUSSOL_ORANGE } },
-  },
-};
