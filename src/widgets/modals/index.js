58bc9d8401bf5cb3abe0c05e9893a0af368eda4d
josh <griffinjoshua5@gmail.com>
2019-12-24 12:53:46 +1300

    Add removal of bottomModals from old index file

--- src/widgets/modals/index.js
+++ src/widgets/modals/index.js
@@ -3,13 +3,10 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
-export { BottomConfirmModal } from './BottomConfirmModal';
-export { BottomModalContainer } from './BottomModalContainer';
 export { ByProgramModal } from './ByProgramModal';
 export { ConfirmModal } from './ConfirmModal';
+export { DataTablePageModal } from './DataTablePageModal';
 export { DemoUserModal } from './DemoUserModal';
 export { FinaliseModal } from './FinaliseModal';
 export { LoginModal } from './LoginModal';
-export { DataTablePageModal } from './DataTablePageModal';
-export { BottomTextEditor } from './BottomTextEditor';
 export { NewConfirmModal } from './NewConfirmModal';
