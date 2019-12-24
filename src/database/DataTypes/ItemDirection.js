684ff10fbddea2458e545e1eb7d84db407e58496
josh <griffinjoshua5@gmail.com>
2019-12-24 21:15:43 +1300

    Add ItemDirection class

---
+++ src/database/DataTypes/ItemDirection.js
@@ -0,0 +1,19 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2016
+ */
+
+import Realm from 'realm';
+
+export class ItemDirection extends Realm.Object {}
+
+ItemDirection.schema = {
+  name: 'ItemDirection',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    priority: 'string',
+    direction: 'string',
+    item: { type: 'Item', optional: true },
+  },
+};
