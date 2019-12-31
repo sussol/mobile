f5f0f3ea326fbfe69622f4590005e81c5dffdc0c
josh <griffinjoshua5@gmail.com>
2019-12-31 19:21:19 +1300

    Add updated Wizard component using the wizard state

--- src/widgets/Wizard.js
+++ src/widgets/Wizard.js
@@ -2,31 +2,46 @@
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import { Stepper } from './Stepper';
 import { TabNavigator } from './TabNavigator';
 import DataTablePageView from './DataTablePageView';
+
+import { WizardActions } from '../actions/WizardActions';
+import { selectCurrentTab } from '../selectors/wizard';
 
 /**
  * Layout component for a Tracker and TabNavigator, displaying steps
  * to completion for completion. See TabNavigator and StepsTracker
  * for individual component implementation.
  */
-export const Wizard = ({ tabs, titles, onPress, currentTabIndex }) => (
+const WizardComponent = ({ tabs, titles, currentTab, nextTab }) => (
   <DataTablePageView>
     <Stepper
       numberOfSteps={tabs.length}
-      currentStep={currentTabIndex}
-      onPress={onPress}
+      currentStep={currentTab}
+      onPress={nextTab}
       titles={titles}
     />
-    <TabNavigator tabs={tabs} currentTabIndex={currentTabIndex} />
+    <TabNavigator tabs={tabs} currentTabIndex={currentTab} />
   </DataTablePageView>
 );
 
-Wizard.propTypes = {
+WizardComponent.propTypes = {
   tabs: PropTypes.array.isRequired,
   titles: PropTypes.array.isRequired,
-  onPress: PropTypes.func.isRequired,
-  currentTabIndex: PropTypes.number.isRequired,
+  nextTab: PropTypes.func.isRequired,
+  currentTab: PropTypes.number.isRequired,
 };
+
+const mapStateToProps = state => {
+  const currentTab = selectCurrentTab(state);
+  return { currentTab };
+};
+
+const mapDispatchToProps = dispatch => ({
+  nextTab: tab => dispatch(WizardActions.switchTab(tab)),
+});
+
+export const Wizard = connect(mapStateToProps, mapDispatchToProps)(WizardComponent);
