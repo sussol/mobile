a16ef242c0999dcadcc071fe525195d909bdcbba
josh <griffinjoshua5@gmail.com>
2019-12-31 18:37:31 +1300

    Add update to PRescriptionPage using new reducer

--- src/pages/PrescriptionPage.js
+++ src/pages/PrescriptionPage.js
@@ -13,36 +13,25 @@ import { PrescriberSelect } from '../widgets/Tabs/PrescriberSelect';
 import { ItemSelect } from '../widgets/Tabs/ItemSelect';
 import { PrescriptionConfirmation } from '../widgets/Tabs/PrescriptionConfirmation';
 
-import {
-  selectItem,
-  selectPrescriber,
-  switchTab,
-  editQuantity,
-} from '../reducers/PrescriptionReducer';
-
-const mapDispatchToProps = dispatch => {
-  const choosePrescriber = prescriberID => dispatch(selectPrescriber(prescriberID));
-  const chooseItem = itemID => dispatch(selectItem(itemID));
-  const nextTab = currentTab => dispatch(switchTab(currentTab + 1));
-  const updateQuantity = (id, quantity) => dispatch(editQuantity(id, quantity));
-  return { nextTab, choosePrescriber, chooseItem, updateQuantity };
-};
-
-const mapStateToProps = state => {
-  const { prescription, patient, prescriber } = state;
-  return { ...prescription, ...patient, ...prescriber };
-};
+import { useRecordListener } from '../hooks';
+import { WizardActions } from '../actions/WizardActions';
 
 const tabs = [PrescriberSelect, ItemSelect, PrescriptionConfirmation];
 const titles = ['Select the Prescriber', 'Select items', 'Finalise'];
 
-export const Prescription = ({ currentTab, nextTab }) => (
-  <Wizard tabs={tabs} titles={titles} currentTabIndex={currentTab} onPress={nextTab} />
-);
+export const Prescription = ({ transaction, completePrescription }) => {
+  useRecordListener(completePrescription, transaction, 'Transaction');
+  return <Wizard tabs={tabs} titles={titles} />;
+};
 
-export const PrescriptionPage = connect(mapStateToProps, mapDispatchToProps)(Prescription);
+const mapDispatchToProps = dispatch => {
+  const completePrescription = () => dispatch(WizardActions.complete());
+  return { completePrescription };
+};
+
+export const PrescriptionPage = connect(null, mapDispatchToProps)(Prescription);
 
 Prescription.propTypes = {
-  currentTab: PropTypes.number.isRequired,
-  nextTab: PropTypes.func.isRequired,
+  transaction: PropTypes.object.isRequired,
+  completePrescription: PropTypes.func.isRequired,
 };
