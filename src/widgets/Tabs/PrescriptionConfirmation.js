5fbc51621c021142793c64ed20155bc4f7ad6f9a
josh <griffinjoshua5@gmail.com>
2020-01-07 15:18:08 +1300

    Add updated PrescriptionConfirmation

--- src/widgets/Tabs/PrescriptionConfirmation.js
+++ src/widgets/Tabs/PrescriptionConfirmation.js
@@ -12,12 +12,18 @@ import { PrescriptionSummary } from '../PrescriptionSummary';
 import { PrescriptionInfo } from '../PrescriptionInfo';
 import { FlexView } from '../FlexView';
 import { PageButton } from '../PageButton';
+import { FlexRow } from '../FlexRow';
 
+import { UIDatabase } from '../../database/index';
+import { pay } from '../../utilities/modules/dispensary/pay';
 import { FinaliseActions } from '../../actions/FinaliseActions';
+import { PaymentSummary } from '../PaymentSummary';
 
 const mapStateToProps = state => {
-  const { prescription, patient, prescriber } = state;
-  return { ...prescription, ...patient, ...prescriber };
+  const { prescription, patient, prescriber, user, payment } = state;
+  const { cashAmount } = payment;
+
+  return { ...prescription, ...patient, ...prescriber, ...user, cashAmount };
 };
 
 const mapDispatchToProps = dispatch => {
@@ -25,17 +31,33 @@ const mapDispatchToProps = dispatch => {
   return { openFinaliseModal };
 };
 
-const PrescriptionConfirmationComponent = ({ transaction, openFinaliseModal }) => (
-  <FlexView flex={1}>
-    <PrescriptionInfo />
-    <PrescriptionSummary transaction={transaction} />
-    <PageButton style={{ alignSelf: 'flex-end' }} text="Complete" onPress={openFinaliseModal} />
-  </FlexView>
-);
+const PrescriptionConfirmationComponent = ({
+  transaction,
+  currentUser,
+  currentPatient,
+  paymentAmount,
+}) => {
+  const confirmPrescription = React.useCallback(
+    () => UIDatabase.write(() => pay(currentUser, currentPatient, transaction, paymentAmount)),
+    []
+  );
+  return (
+    <FlexView flex={1}>
+      <PrescriptionInfo />
+      <FlexRow flex={1}>
+        <PrescriptionSummary transaction={transaction} />
+        <PaymentSummary />
+      </FlexRow>
+      <PageButton style={{ alignSelf: 'flex-end' }} text="Complete" onPress={confirmPrescription} />
+    </FlexView>
+  );
+};
 
 PrescriptionConfirmationComponent.propTypes = {
   transaction: PropTypes.object.isRequired,
-  openFinaliseModal: PropTypes.func.isRequired,
+  currentUser: PropTypes.object.isRequired,
+  currentPatient: PropTypes.object.isRequired,
+  paymentAmount: PropTypes.object.isRequired,
 };
 
 export const PrescriptionConfirmation = connect(
