a84a3278811a73f8accc75313622ef7c0c63df77
josh <griffinjoshua5@gmail.com>
2020-01-07 15:17:20 +1300

    Add currency and total logic to NumberLabelRow

--- src/widgets/NumberLabelRow.js
+++ src/widgets/NumberLabelRow.js
@@ -4,10 +4,11 @@
  */
 
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { SimpleLabel } from './SimpleLabel';
+import { FlexRow } from './FlexRow';
+import { FlexView } from './FlexView';
 
 /**
  * Layout and display component rendering some text and a number
@@ -16,31 +17,41 @@ import { SimpleLabel } from './SimpleLabel';
  * Simple wrapper around `SimpleLabel` component. See for further
  * detail.
  *
- * @prop {String} size   The font size - see SimpleLabel for details.
- * @prop {String} text   Left hand side text value.
- * @prop {Number} number Right hand side number value.
+ * @prop {String} size       The font size - see SimpleLabel for details.
+ * @prop {String} text       Left hand side text value.
+ * @prop {Number} number     Right hand side number value.
+ * @prop {Number} total      Total value - displays text / total rather than just total.
+ * @prop {Bool}   isCurrency Indicator whether to appends `$` to each number value.
  */
-export const NumberLabelRow = ({ size, text, number }) => (
-  <View style={localStyles.mainContainer}>
-    <View style={localStyles.flexNine}>
-      <SimpleLabel text={text} size={size} />
-    </View>
-    <View style={localStyles.flexOne}>
-      <SimpleLabel text={number} size={size} />
-    </View>
-  </View>
-);
+export const NumberLabelRow = ({ size, text, number, total, isCurrency }) => {
+  const withCurrency = textInput => (isCurrency ? `$${textInput}` : textInput);
+  const withTotalText = total
+    ? `${withCurrency(number)} / ${withCurrency(total)}`
+    : withCurrency(number);
 
-const localStyles = StyleSheet.create({
-  mainContainer: { flex: 1, flexDirection: 'row', justifyContent: 'space-between' },
-  flexNine: { flex: 9 },
-  flexOne: { flex: 1 },
-});
+  return (
+    <FlexRow flex={1}>
+      <FlexView flex={1}>
+        <SimpleLabel text={text} size={size} />
+      </FlexView>
+      <FlexView flex={1}>
+        <SimpleLabel text={withTotalText} size={size} textAlign="right" />
+      </FlexView>
+    </FlexRow>
+  );
+};
+
+NumberLabelRow.defaultProps = {
+  total: 0,
+  isCurrency: false,
+};
 
 NumberLabelRow.propTypes = {
   size: PropTypes.string,
   text: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired,
+  total: PropTypes.number,
+  isCurrency: PropTypes.bool,
 };
 
 NumberLabelRow.defaultProps = { size: 'large' };
