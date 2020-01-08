a963ddb07020002fb041d6176d28ebf90a50de28
josh <griffinjoshua5@gmail.com>
2020-01-09 09:15:55 +1300

    Add payment total selector adjustment

--- src/selectors/payment.js
+++ src/selectors/payment.js
@@ -13,11 +13,11 @@ export const selectPrescriptionSubTotal = ({ payment }) => {
 };
 
 export const selectPrescriptionTotal = ({ payment }) => {
-  const { insurancePolicy } = payment;
-
+  const { policy } = payment;
+  console.log(!!policy);
   const subtotal = selectPrescriptionSubTotal({ payment });
 
-  return currency(subtotal * (insurancePolicy ? insurancePolicy.discountRate / 100 : 1));
+  return currency(subtotal * (policy ? (policy.discountRate || 100) / 100 : 1));
 };
 
 export const selectCreditBeingUsed = ({ payment }) => {
