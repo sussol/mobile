dba8a2ccbf194c088f9e6d7e828cdaab60f4ef1e
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2020-01-13 13:45:38 +1300

    Add map data to get values

--- src/widgets/PieChart.js
+++ src/widgets/PieChart.js
@@ -11,7 +11,6 @@ import { VictoryLabel, VictoryPie } from 'victory-native';
 import { APP_FONT_FAMILY, GREY } from '../globalStyles';
 
 export const PieChart = ({ width, height, data }) => {
-  const { values } = data;
   const {
     padVertical,
     padHorizontal,
@@ -30,7 +29,7 @@ export const PieChart = ({ width, height, data }) => {
   };
 
   const widthPadded = width * (1 - padHorizontal);
-  return (
+  return data.map(({ values }) => (
     <VictoryPie
       width={width}
       height={height}
@@ -40,9 +39,9 @@ export const PieChart = ({ width, height, data }) => {
       labelRadius={widthPadded * labelRadius}
       colorScale={colorScale}
       labelComponent={<VictoryLabel style={style} />}
-      data={values[0]}
+      data={values}
     />
-  );
+  ));
 };
 
 PieChart.propTypes = {
