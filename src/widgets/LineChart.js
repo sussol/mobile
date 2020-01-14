31adcb1fe063c3358f288917eac0822fc5c93e3a
Diego <32987464+diegoevangelisti@users.noreply.github.com>
2020-01-14 13:55:37 +1300

    Add label to each line in LineChart

--- src/widgets/LineChart.js
+++ src/widgets/LineChart.js
@@ -6,7 +6,13 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryAxis, VictoryChart, VictoryLine, VictoryScatter } from 'victory-native';
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLine,
+  VictoryScatter,
+} from 'victory-native';
 
 import { APP_FONT_FAMILY, GREY, LIGHT_GREY, DARK_GREY, SUSSOL_ORANGE } from '../globalStyles';
 
@@ -33,6 +39,7 @@ export const LineChart = ({ width, height, data }) => {
     left: width * padLeft,
     right: width * padRight,
   };
+
   return (
     <VictoryChart width={width} height={height} padding={padding}>
       {data.map(({ values }) => (
@@ -40,6 +47,14 @@ export const LineChart = ({ width, height, data }) => {
       ))}
       {data.map(({ values }) => (
         <VictoryLine style={lineStyle} data={values} />
+      ))}
+      {data.map(({ values, label }) => (
+        <VictoryLabel
+          datum={{ x: values.length - 1, y: values[values.length - 1].y }}
+          text={label}
+          style={victoryStyles.labelStyle}
+          textAnchor="middle"
+        />
       ))}
       {renderYAxis()}
       {renderXAxis()}
@@ -72,5 +87,11 @@ const victoryStyles = {
     dotSize: 3.5,
     dotStyle: { data: { fill: SUSSOL_ORANGE } },
     lineStyle: { data: { stroke: SUSSOL_ORANGE } },
+  },
+  style: { fontFamily: APP_FONT_FAMILY, fill: GREY },
+  labelStyle: {
+    fontFamily: APP_FONT_FAMILY,
+    fill: DARK_GREY,
+    fontSize: 16,
   },
 };
