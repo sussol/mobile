f515f61b49e2b9cd82a9f423eca720e932dc2aeb
josh <griffinjoshua5@gmail.com>
2020-01-18 12:14:05 +1300

    Add payments and insurance to modules reducer

--- src/reducers/ModulesReducer.js
+++ src/reducers/ModulesReducer.js
@@ -9,34 +9,35 @@ import { SYNC_TRANSACTION_COMPLETE } from '../sync/constants';
 
 /**
  * Simple reducer managing the stores current modules state.
- *
- * State shape:
- * {
- *     usingDashboard: [bool],
- *     usingDispensary: [bool],
- *     usingVaccines: [bool],
- *     usingModules: [bool],
- * }
  */
 
 const checkModule = key => UIDatabase.getSetting(key).toLowerCase() === 'true';
 
-const modulesInitialState = () => {
+const initialState = () => {
   const usingDashboard = checkModule(SETTINGS_KEYS.DASHBOARD_MODULE);
   const usingDispensary = checkModule(SETTINGS_KEYS.DISPENSARY_MODULE);
   const usingVaccines = checkModule(SETTINGS_KEYS.VACCINE_MODULE);
-  const usingModules = usingDashboard || usingDispensary || usingVaccines;
-  const modules = { usingDashboard, usingDispensary, usingVaccines, usingModules };
-  return { ...modules };
+  const usingPayments = checkModule(SETTINGS_KEYS.PAYMENT_MODULE);
+  const usingModules = usingDashboard || usingDispensary || usingVaccines || usingPayments;
+  const usingInsurance = UIDatabase.objects('InsuranceProvider').length > 0;
+
+  return {
+    usingPayments,
+    usingDashboard,
+    usingDispensary,
+    usingVaccines,
+    usingModules,
+    usingInsurance,
+  };
 };
 
-export const ModulesReducer = (state = modulesInitialState(), action) => {
+export const ModulesReducer = (state = initialState(), action) => {
   const { type } = action;
 
   switch (type) {
     // After sync, refresh modules state
     case SYNC_TRANSACTION_COMPLETE: {
-      return modulesInitialState();
+      return initialState();
     }
 
     default:
