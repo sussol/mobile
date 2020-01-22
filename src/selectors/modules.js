ef8a375664ded8fdf2d39b5490fd9ee7c6a332f5
josh <griffinjoshua5@gmail.com>
2020-01-23 11:12:15 +1300

    Add modules selector

---
+++ src/selectors/modules.js
@@ -0,0 +1,9 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2020
+ */
+
+export const selectUsingSupplierCredits = ({ modules }) => {
+  const { usingSupplierCredits } = modules;
+  return usingSupplierCredits;
+};
