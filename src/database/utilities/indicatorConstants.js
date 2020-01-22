7d95a6ad3f796a208b2e9a55a373f5bd99cfd70d
Will Thomson <will.tho@outlook.com>
2020-01-22 13:12:48 +1300

    Add type enforcement to indicator value setter

---
+++ src/database/utilities/indicatorConstants.js
@@ -0,0 +1,13 @@
+export const INDICATOR_CODES = {
+  HIV: 'HIV',
+  REGIMEN: 'REGIMEN',
+};
+
+export const INDICATOR_COLUMN_CODES = {
+  REGIMEN_VALUE: 'regimen_value',
+  REGIMEN_COMMENT: 'regimen_comment',
+};
+
+export const INDICATOR_VALUE_TYPES = {
+  STRING: 'string',
+};
