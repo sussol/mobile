1546f30ebeba3d0260bcc044f6d1785d700e9aaa
Will Thomson <will.tho@outlook.com>
2020-01-25 21:56:08 +1300

    Add cash transaction description bottom modal

--- src/widgets/modals/CashTransactionModal.js
+++ src/widgets/modals/CashTransactionModal.js
@@ -3,54 +3,79 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { BottomTextEditor } from '../bottomModals';
 import { PencilIcon, ChevronDownIcon } from '../icons';
 
-export const CashTransactionModal = () => (
-  <View style={localStyles.modalContainerStyle}>
-    <TouchableOpacity style={localStyles.containerStyle}>
-      <View style={localStyles.textContainerStyle}>
-        <Text style={localStyles.textStyle}>Select a name</Text>
-      </View>
-      <View style={localStyles.iconContainerStyle}>
-        <ChevronDownIcon />
-      </View>
-    </TouchableOpacity>
-    <TouchableOpacity style={localStyles.containerStyle}>
-      <View style={localStyles.textContainerStyle}>
-        <Text style={localStyles.textStyle}>Choose transaction type</Text>
-      </View>
-      <View style={localStyles.iconContainerStyle}>
-        <ChevronDownIcon />
-      </View>
-    </TouchableOpacity>
-    <TouchableOpacity style={localStyles.containerStyle}>
-      <View style={localStyles.textContainerStyle}>
-        <Text style={localStyles.textStyle}>Enter transaction amount</Text>
-      </View>
-      <View style={localStyles.iconContainerStyle}>
-        <PencilIcon />
-      </View>
-    </TouchableOpacity>
-    <TouchableOpacity style={localStyles.containerStyle}>
-      <View style={localStyles.textContainerStyle}>
-        <Text style={localStyles.textStyle}>Select reason</Text>
-      </View>
-      <View style={localStyles.iconContainerStyle}>
-        <ChevronDownIcon />
-      </View>
-    </TouchableOpacity>
-    <TouchableOpacity style={localStyles.containerStyle}>
-      <View style={localStyles.textContainerStyle}>
-        <Text style={localStyles.textStyle}>Enter description</Text>
-      </View>
-      <View style={localStyles.iconContainerStyle}>
-        <PencilIcon />
-      </View>
-    </TouchableOpacity>
-  </View>
-);
+export const CashTransactionModal = () => {
+  const [description, setDescription] = useState('Enter a description');
+  
+  const [textBuffer, setTextBuffer] = useState('');
+  const [isTextEditorOpen, setIsTextEditorOpen] = useState(false);
+
+  const onPressDescription = () => {
+    setTextBuffer(description);
+    setIsTextEditorOpen(true);
+  }
+
+
+  return (
+    <View style={localStyles.modalContainerStyle}>
+        <TouchableOpacity style={localStyles.containerStyle}>
+        <View style={localStyles.textContainerStyle}>
+            <Text style={localStyles.textStyle}>Select a name</Text>
+        </View>
+        <View style={localStyles.iconContainerStyle}>
+            <ChevronDownIcon />
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={localStyles.containerStyle}>
+        <View style={localStyles.textContainerStyle}>
+            <Text style={localStyles.textStyle}>Choose transaction type</Text>
+        </View>
+        <View style={localStyles.iconContainerStyle}>
+            <ChevronDownIcon />
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={localStyles.containerStyle}>
+        <View style={localStyles.textContainerStyle}>
+            <Text style={localStyles.textStyle}>Enter transaction amount</Text>
+        </View>
+        <View style={localStyles.iconContainerStyle}>
+            <PencilIcon />
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={localStyles.containerStyle}>
+        <View style={localStyles.textContainerStyle}>
+            <Text style={localStyles.textStyle}>Select reason</Text>
+        </View>
+        <View style={localStyles.iconContainerStyle}>
+            <ChevronDownIcon />
+        </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={localStyles.containerStyle} onPress={onPressDescription}>
+        <View style={localStyles.textContainerStyle}>
+            <Text style={localStyles.textStyle}>{description}</Text>
+        </View>
+        <View style={localStyles.iconContainerStyle}>
+            <PencilIcon />
+        </View>
+        </TouchableOpacity>
+        <BottomTextEditor
+            isOpen={isTextEditorOpen}
+            buttonText={"Confirm"}
+            value={textBuffer}
+            placeholder={"Enter description"}
+            onConfirm={() => {
+                setDescription(textBuffer);
+                setIsTextEditorOpen(false);
+            }}
+            onChangeText={text => setTextBuffer(text)}
+        />
+    </View>
+  );
+};
 
 const localStyles = StyleSheet.create({
   modalContainerStyle: {
