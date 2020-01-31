515dad653c0c4cf8d9dc63316dda93542b7b324a
josh <griffinjoshua5@gmail.com>
2020-01-31 19:23:14 +1300

    Add UserActions updates

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -3,12 +3,33 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
+import moment from 'moment';
+
 export const USER_ACTION_TYPES = {
   LOG_IN: 'USER/LOG_IN',
   LOG_OUT: 'USER/LOG_OUT',
+  SET_TIME: 'USER/SET_TIME',
+  ACTIVE: 'USER/ACTIVE',
+};
+
+const login = user => ({ type: USER_ACTION_TYPES.LOG_IN, payload: { user } });
+const logout = () => ({ type: USER_ACTION_TYPES.LOG_OUT });
+const setTime = () => ({ type: USER_ACTION_TYPES.SET_TIME });
+
+const active = () => (dispatch, getState) => {
+  const { user } = getState();
+  const { time } = user;
+
+  const tenMinutesLater = moment(time).add(3, 'minutes');
+  const timeNow = moment();
+
+  if (tenMinutesLater.isBefore(timeNow)) return dispatch(logout());
+  return dispatch(setTime());
 };
 
 export const UserActions = {
-  login: user => ({ type: USER_ACTION_TYPES.LOG_IN, payload: { user } }),
-  logout: () => ({ type: USER_ACTION_TYPES.LOG_OUT }),
+  login,
+  logout,
+  setTime,
+  active,
 };
