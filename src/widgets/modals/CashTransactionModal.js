a07410971f1a935df3540c1c18ba979ff59d2ce1
Will Thomson <will.tho@outlook.com>
2020-02-10 14:23:48 +1300

    Fix more missing cash register dependencies

--- src/widgets/modals/CashTransactionModal.js
+++ src/widgets/modals/CashTransactionModal.js
@@ -60,11 +60,13 @@ export const CashTransactionModal = ({ onConfirm }) => {
     [name, type, amount, reason, description]
   );
 
-  const onChangeText = useMemo(() => text => setDescriptionBuffer(text));
-  const onChangeAmount = useMemo(() => value => setAmountBuffer(value));
+  const onChangeText = useMemo(() => text => setDescriptionBuffer(text), []);
+  const onChangeAmount = useMemo(() => value => setAmountBuffer(value), []);
 
-  const renderNameLeftText = useCallback(({ firstName, lastName, name: fullName, isPatient }) =>
-    isPatient ? `${lastName}, ${firstName}` : `${fullName}`
+  const renderNameLeftText = useCallback(
+    ({ firstName, lastName, name: fullName, isPatient }) =>
+      isPatient ? `${lastName}, ${firstName}` : `${fullName}`,
+    []
   );
 
   const renderNameRightText = useCallback(({ isCustomer, isSupplier, isPatient, dateOfBirth }) => {
@@ -80,7 +82,7 @@ export const CashTransactionModal = ({ onConfirm }) => {
       return dispensingStrings.supplier;
     }
     return '';
-  });
+  }, []);
 
   const resetBottomModal = () => {
     setIsNameModalOpen(false);
