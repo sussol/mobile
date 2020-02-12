bef68d6f698c9cd04863fc48e7d745c5498abeea
josh <griffinjoshua5@gmail.com>
2020-02-12 18:27:53 +1300

    Revert "Add correctly assign TransactionItem note to batches"
    This reverts commit 5af48a18036b5487ec6609437b5408d296f48bd5.

--- src/database/utilities/createRecord.js
+++ src/database/utilities/createRecord.js
@@ -701,13 +701,12 @@ const createSupplierInvoice = (database, supplier, user) => {
  */
 const createTransactionBatch = (database, transactionItem, itemBatch, isAddition = true) => {
   const { item, batch, expiryDate, packSize, costPrice, sellPrice, donor } = itemBatch;
-  const { note, transaction } = transactionItem || {};
+  const { transaction } = transactionItem || {};
 
   const transactionBatch = database.create('TransactionBatch', {
     id: generateUUID(),
     itemId: item.id,
     itemName: item.name,
-    note,
     itemBatch,
     batch,
     expiryDate,
