3dd98a4957a33602e1f22d7d67f1fc64472a71e7
Will Thomson <will.tho@outlook.com>
2020-02-19 15:21:58 +1300

    Disable cash register if no payment types defined

--- src/reducers/ModulesReducer.js
+++ src/reducers/ModulesReducer.js
@@ -26,20 +26,20 @@ import { SYNC_TRANSACTION_COMPLETE } from '../sync/constants';
 const checkModule = key => UIDatabase.getSetting(key).toLowerCase() === 'true';
 
 const initialState = () => {
+  const usingInsurance = UIDatabase.objects('InsuranceProvider').length > 0;
+  const usingPaymentTypes = UIDatabase.objects('PaymentType').length > 0;
+  const usingPrescriptionCategories = UIDatabase.objects('PrescriptionCategory').length > 0;
+  const usingSupplierCreditCategories = UIDatabase.objects('SupplierCreditCategory').length > 0;
+
   const usingDashboard = checkModule(SETTINGS_KEYS.DASHBOARD_MODULE);
   const usingDispensary = checkModule(SETTINGS_KEYS.DISPENSARY_MODULE);
   const usingVaccines = checkModule(SETTINGS_KEYS.VACCINE_MODULE);
-  const usingCashRegister = checkModule(SETTINGS_KEYS.CASH_REGISTER_MODULE);
+  const usingCashRegister = checkModule(SETTINGS_KEYS.CASH_REGISTER_MODULE) && usingPaymentTypes;
   const usingPayments = checkModule(SETTINGS_KEYS.PAYMENT_MODULE);
   const usingSupplierCredits = checkModule(SETTINGS_KEYS.SUPPLIER_CREDIT_MODULE);
   const usingPatientTypes = checkModule(SETTINGS_KEYS.PATIENT_TYPES);
 
   const usingModules = usingDashboard || usingDispensary || usingVaccines || usingCashRegister;
-
-  const usingInsurance = UIDatabase.objects('InsuranceProvider').length > 0;
-  const usingPrescriptionCategories = UIDatabase.objects('PrescriptionCategory').length > 0;
-  const usingSupplierCreditCategories = UIDatabase.objects('SupplierCreditCategory').length > 0;
-  const usingPaymentTypes = UIDatabase.objects('PaymentType').length > 0;
 
   return {
     usingPayments,
