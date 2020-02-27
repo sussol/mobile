e0b0b6524a55b84a2c2468aecca12334ec4edf20
josh <griffinjoshua5@gmail.com>
2020-02-28 12:38:23 +1300

    Add comments

--- src/sync/constants.js
+++ src/sync/constants.js
@@ -3,6 +3,16 @@
  * Sustainable Solutions (NZ) Ltd. 2019
  */
 
+/**
+ * List of app version -> server compatibilities.
+ *
+ * The key is the mobile major version.
+ * The field is the lower bound inclusive major.minor version
+ * of a compatible server.
+ *
+ * I.e. 4: 409 - Any mobile version 4 (or greater) requires at server
+ * version of 4.09 or greater.
+ */
 export const SERVER_COMPATIBILITIES = {
   4: 409,
 };
