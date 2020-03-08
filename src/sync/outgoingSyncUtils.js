9f0b61f502d5914f9b361b02374c61349be4da52
Will Thomson <will.tho@outlook.com>
2020-03-09 09:27:39 +1300

    Add is_edited field to outgoing stocktake batch sync

--- src/sync/outgoingSyncUtils.js
+++ src/sync/outgoingSyncUtils.js
@@ -184,6 +184,7 @@ const generateSyncData = (settings, recordType, record) => {
         Batch: record.batch,
         item_ID: record.itemId,
         optionID: record.option && record.option.id,
+        is_edited: record.hasBeenCounted,
       };
     }
     case 'Transaction': {
