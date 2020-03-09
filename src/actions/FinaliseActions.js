d97530e3636925a4f36f30efab70c0b3cf5cb14c
josh <griffinjoshua5@gmail.com>
2020-03-10 09:20:47 +1300

    Add setFinaliseItem action

--- src/actions/FinaliseActions.js
+++ src/actions/FinaliseActions.js
@@ -6,9 +6,14 @@
 export const FINALISE_ACTIONS = {
   OPEN_MODAL: 'Finalise/openModal',
   CLOSE_MODAL: 'Finalise/closeModal',
+  SET_FINALISE_ITEM: 'Finalise/setFinaliseItem',
 };
 
 const openModal = () => ({ type: FINALISE_ACTIONS.OPEN_MODAL });
 const closeModal = () => ({ type: FINALISE_ACTIONS.CLOSE_MODAL });
+const setFinaliseItem = finaliseItem => ({
+  type: FINALISE_ACTIONS.SET_FINALISE_ITEM,
+  payload: { finaliseItem },
+});
 
-export const FinaliseActions = { openModal, closeModal };
+export const FinaliseActions = { openModal, closeModal, setFinaliseItem };
