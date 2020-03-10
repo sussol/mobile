9423872af17a0747a4a72785f5ed6dbdbd5f0995
josh <griffinjoshua5@gmail.com>
2020-03-10 17:20:28 +1300

    Add exporting of pages from index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,22 @@
  */
 
 export { FirstUsePage } from './FirstUsePage';
+export { CashRegisterPage } from './CashRegisterPage';
+export { CustomerInvoicePage } from './CustomerInvoicePage';
+export { CustomerInvoicesPage } from './CustomerInvoicesPage';
+export { SupplierInvoicePage } from './SupplierInvoicePage';
+export { SupplierInvoicesPage } from './SupplierInvoicesPage';
+export { CustomerRequisitionPage } from './CustomerRequisitionPage';
+export { CustomerRequisitionsPage } from './CustomerRequisitionsPage';
+export { SupplierRequisitionPage } from './SupplierRequisitionPage';
+export { SupplierRequisitionsPage } from './SupplierRequisitionsPage';
+export { DashboardPage } from './DashboardPage';
+export { DispensingPage } from './DispensingPage';
+export { MenuPage } from './MenuPage';
+export { PrescriptionPage } from './PrescriptionPage';
+export { RealmExplorer } from './RealmExplorer';
+export { SettingsPage } from './SettingsPage';
+export { StocktakesPage } from './StocktakesPage';
+export { StocktakeManagePage } from './StocktakeManagePage';
+export { StocktakeEditPage } from './StocktakeEditPage';
+export { StockPage } from './StockPage';
