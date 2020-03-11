78be29593175e94931e755bd3a64e79e38dc8873
Will Thomson <will.tho@outlook.com>
2020-03-11 15:56:25 +1300

    Update insurance selectors

--- src/selectors/insurance.js
+++ src/selectors/insurance.js
@@ -3,10 +3,20 @@
  * Sustainable Solutions (NZ) Ltd. 2020
  */
 
+export const selectInsurancePolicyIsActive = ({ insurancePolicy }) => {
+  const { isActive = true } = insurancePolicy ?? {};
+  return isActive;
+};
+
+export const selectInsurancePolicyDiscountRate = ({ insurancePolicy }) => {
+  const isActive = selectInsurancePolicyIsActive({ insurancePolicy });
+  const { discountRate = 0 } = isActive ? insurancePolicy ?? {} : {};
+  return discountRate;
+};
+
 export const selectInsuranceDiscountRate = ({ insurance }) => {
   const { selectedInsurancePolicy } = insurance;
-  const { discountRate = 0 } = selectedInsurancePolicy || {};
-  return discountRate;
+  return selectInsurancePolicyDiscountRate({ selectedInsurancePolicy });
 };
 
 export const selectInsuranceModalOpen = ({ insurance }) => {
