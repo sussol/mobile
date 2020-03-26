0810e2be862e12afe9d6e3c42d1144da8638043e
josh <griffinjoshua5@gmail.com>
2020-03-27 08:58:33 +1300

    Add optional breach log end timestamp

--- src/database/DataTypes/BreachLog.js
+++ src/database/DataTypes/BreachLog.js
@@ -14,7 +14,7 @@ BreachLog.schema = {
     id: 'string',
     temperatureLogs: { type: 'linkingObjects', objectType: 'TemperatureLog', property: 'breach' },
     startTimestamp: 'date',
-    endTimestamp: 'date',
+    endTimestamp: { type: 'date', optional: true },
   },
 };
 
