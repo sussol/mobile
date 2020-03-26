b693a0f06e490b5a2e3400f40aec9147a1403970
josh <griffinjoshua5@gmail.com>
2020-03-26 16:19:39 +1300

    Add Sensor class

---
+++ src/database/DataTypes/Sensor.js
@@ -0,0 +1,21 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2020
+ */
+
+import Realm from 'realm';
+
+export class Sensor extends Realm.Object {}
+
+Sensor.schema = {
+  name: 'Sensor',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    macAddress: { type: 'string', optional: true },
+    name: { type: 'string', optional: true },
+    batteryLevel: { type: 'double', default: 0 },
+  },
+};
+
+export default Sensor;
