5c2b22025f5513e4d0da53eac0fd58ac9e5c1461
josh <griffinjoshua5@gmail.com>
2020-03-27 08:26:41 +1300

    Add TemperatureLog type

---
+++ src/database/DataTypes/TemperatureLog.js
@@ -0,0 +1,22 @@
+/**
+ * Sustainable Solutions (NZ) Ltd. 2020
+ * mSupply Mobile
+ */
+
+import Realm from 'realm';
+
+export class TemperatureLog extends Realm.Object {}
+
+TemperatureLog.schema = {
+  name: 'TemperatureLog',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    temperature: 'double',
+    timestamp: 'date',
+    location: 'Location',
+    breach: 'BreachLog',
+  },
+};
+
+export default TemperatureLog;
