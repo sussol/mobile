8a70511d501f3e61e0185fe9345518a7f49db6f3
josh <griffinjoshua5@gmail.com>
2020-03-27 08:26:54 +1300

    Add BreachLog type

---
+++ src/database/DataTypes/BreachLog.js
@@ -0,0 +1,21 @@
+/**
+ * Sustainable Solutions (NZ) Ltd. 2020
+ * mSupply Mobile
+ */
+
+import Realm from 'realm';
+
+export class BreachLog extends Realm.Object {}
+
+BreachLog.schema = {
+  name: 'BreachLog',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    temperatureLogs: { type: 'linkingObjects', objectType: 'TemperatureLog', property: 'breach' },
+    startTimestamp: 'date',
+    endTimestamp: 'Location',
+  },
+};
+
+export default BreachLog;
