eee63e0e6a4d2016da8a9f24d183f125e457c941
josh <griffinjoshua5@gmail.com>
2020-04-01 15:19:34 +1300

    Add downloading logs actions

--- src/actions/TemperatureSyncActions.js
+++ src/actions/TemperatureSyncActions.js
@@ -5,11 +5,17 @@
 
 import { UIDatabase } from '../database';
 import { Sensor } from '../database/DataTypes';
+import { createRecord } from '../database/utilities';
+import { MILLISECONDS } from '../utilities';
 
 export const TEMPERATURE_SYNC_ACTIONS = {
   SCAN_START: 'TemperatureSync/scanStart',
   SCAN_COMPLETE: 'TemperatureSync/scanComplete',
   SCAN_ERROR: 'TemperatureSync/scanError',
+
+  DOWNLOAD_LOGS_START: 'TemperatureSync/downloadLogsStart',
+  DOWNLOAD_LOGS_COMPLETE: 'TemperatureSync/downloadLogsComplete',
+  DOWNLOAD_LOGS_ERROR: 'TemperatureSync/downloadLogsErorr',
 };
 
 const scanStart = () => ({ type: TEMPERATURE_SYNC_ACTIONS.SCAN_START });
@@ -44,6 +50,39 @@ const scanForSensors = () => async dispatch => {
   dispatch(updateSensors(data));
 
   if (success) dispatch(scanComplete());
+};
+
+const downloadLogsError = () => ({ type: TEMPERATURE_SYNC_ACTIONS.DOWNLOAD_LOGS_ERROR });
+const downloadLogsStart = () => ({ type: TEMPERATURE_SYNC_ACTIONS.DOWNLOAD_LOGS_START });
+const downloadLogsComplete = () => ({
+  type: TEMPERATURE_SYNC_ACTIONS.DOWNLOAD_LOGS_COMPLETE,
+});
+
+const downloadLogs = sensor => async dispatch => {
+  dispatch(downloadLogsStart());
+
+  const downloadedLogsResult = (await sensor?.downloadLogs()) ?? {};
+  const { success = false, data = [] } = downloadedLogsResult;
+
+  if (success && data?.[0]?.logs) {
+    const sensorLogs = data[0]?.logs;
+    const timeNow = new Date().getTime();
+
+    if (Object.isArray(sensorLogs)) {
+      UIDatabase.write(() =>
+        sensorLogs.forEach(({ temperature }, i) => {
+          const timestamp = timeNow - (i + 1) * MILLISECONDS.FIVE_MINUTES;
+          createRecord(UIDatabase, 'SensorLog', temperature, new Date(timestamp), sensor);
+        })
+      );
+    }
+
+    dispatch(downloadLogsComplete());
+  } else {
+    dispatch(downloadLogsError());
+  }
+
+  return Promise.resolve();
 };
 
 export const TemperatureSyncActions = {
