de851d4476141d3ea179363aaa93ff455a683da4
josh <griffinjoshua5@gmail.com>
2020-04-08 19:27:21 +1200

    Add BreachActions

--- src/actions/BreachActions.js
+++ src/actions/BreachActions.js
@@ -3,16 +3,34 @@
  * Sustainable Solutions (NZ) Ltd. 2020
  */
 
+import { UIDatabase } from '../database';
+
 export const BREACH_ACTIONS = {
   OPEN_MODAL: 'breachActions/openModal',
   CLOSE_MODAL: 'breachActions/closeModal',
   SET_FRIDGE_BREACH: 'breachActions/setFridgeBreach',
   SET_BATCH_BREACH: 'breachActions/setBatchBreach',
   SET_BATCH: 'breachActions/setBatch',
+  SET_TRANSACTION_ITEM: 'breachActions/setTransactionItem',
+  SET_STOCKTAKE_BATCH: 'breachActions/setStocktakeBatch',
 };
 
 const open = () => ({ type: BREACH_ACTIONS.OPEN_MODAL });
 const close = () => ({ type: BREACH_ACTIONS.CLOSE_MODAL });
+
+const setTransactionItem = transactionItemId => {
+  const transactionItem = UIDatabase.get('TransactionItem', transactionItemId);
+  const { breaches } = transactionItem;
+
+  return { type: BREACH_ACTIONS.SET_TRANSACTION_ITEM, payload: { breaches, transactionItem } };
+};
+
+const setStocktakeBatch = stocktakeBatchId => {
+  const stocktakeBatch = UIDatabase.get('StocktakeBatch', stocktakeBatchId);
+  const { breaches } = stocktakeBatch ?? {};
+
+  return { type: BREACH_ACTIONS.SET_STOCKTAKE_BATCH, payload: { breaches, stocktakeBatch } };
+};
 
 const setFridgeBreach = breachId => ({
   type: BREACH_ACTIONS.SET_FRIDGE_BREACH,
@@ -29,4 +47,12 @@ const setBatch = batch => ({
   payload: { batch },
 });
 
-export const BreachActions = { open, close, setFridgeBreach, setBatchBreach, setBatch };
+export const BreachActions = {
+  open,
+  close,
+  setFridgeBreach,
+  setBatchBreach,
+  setBatch,
+  setTransactionItem,
+  setStocktakeBatch,
+};
