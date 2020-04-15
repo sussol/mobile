a207933fb8ae234c21b0131038ad0aa4befd5be0
josh <griffinjoshua5@gmail.com>
2020-04-16 08:55:40 +1200

    Add RowDetailActions

---
+++ src/actions/RowDetailActions.js
@@ -0,0 +1,24 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2019
+ */
+
+export const ROW_DETAIL_ACTIONS = {
+  OPEN_ITEM_DETAIL: 'rowDetail/openItemDetail',
+  OPEN_REQUISITION_ITEM_DETAIL: 'rowDetail/openRequisitionItemDetail',
+  CLOSE: 'rowDetail/close',
+};
+
+const openItemDetail = item => ({
+  type: ROW_DETAIL_ACTIONS.OPEN_ITEM_DETAIL,
+  payload: { rowData: item },
+});
+
+const openRequisitionItemDetail = requisitionItem => ({
+  type: ROW_DETAIL_ACTIONS.OPEN_REQUISITION_ITEM_DETAIL,
+  payload: { rowData: requisitionItem },
+});
+
+const close = () => ({ type: ROW_DETAIL_ACTIONS.CLOSE });
+
+export const RowDetailActions = { openItemDetail, close, openRequisitionItemDetail };
