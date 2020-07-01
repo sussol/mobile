5ffa173e84132b7536af356cd776b55d7d56526a
josh <griffinjoshua5@gmail.com>
2020-07-01 23:34:44 +1200

    Add vaccines constants file

---
+++ src/utilities/modules/vaccines/constants.js
@@ -0,0 +1,26 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2020
+ */
+
+const DOMAIN_OFFSET = 3;
+const AXIS_OFFSET = 50;
+const STROKE_WIDTH = 2;
+const STROKE_SIZE = 3;
+const INTERPOLATION = 'natural';
+const MAX_DATA_POINTS = 30;
+
+const SENSOR_LOGS_PER_TEMPERATURE_LOG = 6;
+
+export const VACCINE_CONSTANTS = {
+  SENSOR_LOGS_PER_TEMPERATURE_LOG,
+};
+
+export const CHART_CONSTANTS = {
+  DOMAIN_OFFSET,
+  AXIS_OFFSET,
+  STROKE_SIZE,
+  STROKE_WIDTH,
+  INTERPOLATION,
+  MAX_DATA_POINTS,
+};
