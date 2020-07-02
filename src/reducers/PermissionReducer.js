971a6eaa769f4392004c71067daa1a12648d0552
josh <griffinjoshua5@gmail.com>
2020-07-03 11:10:11 +1200

    Add PermissionReducer/Actions/Selectors

---
+++ src/reducers/PermissionReducer.js
@@ -0,0 +1,32 @@
+/**
+ * mSupply Mobile
+ * Sustainable Solutions (NZ) Ltd. 2020
+ */
+
+import { PERMISSION_ACTIONS } from '../actions/PermissionActions';
+
+const initialState = () => ({ location: false, writeStorage: false, bluetooth: false });
+
+export const PermissionReducer = (state = initialState(), action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case PERMISSION_ACTIONS.SET_LOCATION: {
+      const { status } = payload;
+
+      return { ...state, location: status };
+    }
+    case PERMISSION_ACTIONS.SET_WRITE_STORAGE: {
+      const { status } = payload;
+
+      return { ...state, writeStorage: status };
+    }
+    case PERMISSION_ACTIONS.SET_BLUETOOTH: {
+      const { status } = payload;
+
+      return { ...state, bluetooth: status };
+    }
+    default:
+      return state;
+  }
+};
