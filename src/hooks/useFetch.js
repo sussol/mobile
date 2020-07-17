87d11f92d60916bb93d48a33de3afd4dffeb7ad6
Will Thomson <will.tho@outlook.com>
2020-07-17 20:06:39 +1200

    Add updated useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,103 +4,91 @@
  */
 
 import React from 'react';
-import { Client as BugsnagClient } from 'bugsnag-react-native';
 
-import { getAuthHeader, AUTH_ERROR_CODES } from 'sussol-utilities';
-
-import { SETTINGS_KEYS } from '../settings';
-import { UIDatabase } from '../database';
-
-const { SYNC_SITE_NAME, SYNC_SITE_PASSWORD_HASH } = SETTINGS_KEYS;
-
-const CONNECTION_TIMEOUT_PERIOD = 10000;
-
-const ERROR_CODES = {
-  ...AUTH_ERROR_CODES,
-  EMPTY_RESPONSE: 'No records found',
-};
-
-const bugsnagClient = new BugsnagClient();
-
-class BugsnagError extends Error {
-  constructor(message, data, ...args) {
-    super(message, ...args);
-    bugsnagClient.notify(this, report => {
-      report.errorMessage = message;
-      report.metadata = data;
-    });
-  }
-}
-
-const getAuthorizationHeader = () => {
-  const username = UIDatabase.getSetting(SYNC_SITE_NAME);
-  const password = UIDatabase.getSetting(SYNC_SITE_PASSWORD_HASH);
-  return getAuthHeader(username, password);
-};
-
-const isValidError = error => Object.values(ERROR_CODES).includes(error?.message);
+const doIf = (ifThis, doThis, ...withThese) => (ifThis() ? doThis(...withThese) : null);
 
 /**
  * Custom hook to fetch data inside mounted component.
  *
- * Fetches data on component mount. If the calling component is unmounted or the timeout is reached,
+ * If the calling component is unmounted or the timeout is reached,
  * the fetch is aborted.
  */
-export const useFetch = (url, options = {}, timeout = CONNECTION_TIMEOUT_PERIOD) => {
-  const [data, setData] = React.useState(null);
-  const [error, setError] = React.useState(null);
-  const isMounted = React.useRef(true);
+export const useFetch = url => {
+  const _timer = React.useRef(null);
+  const _controller = React.useRef(null);
 
-  /* eslint-disable no-undef */
-  const abortController = new AbortController();
+  const _isMounted = React.useRef(false);
+  const _isBlocked = React.useRef(false);
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [response, setResponse] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  const reset = () => {
+    _isBlocked.current = true;
+    clearTimeout(_timer.current);
+    if (_controller.current) _controller.current.abort();
+    setResponse(null);
+    setError(null);
+  };
 
   React.useEffect(() => {
-    if (url) {
-      (async () => {
-        const { signal } = abortController;
-        const headers = { authorization: getAuthorizationHeader() };
-        try {
-          const response = await fetch(url, { headers, signal, ...options });
-          const { ok, status } = response;
-          if (ok) {
-            const responseData = await response.json();
-            const { error: responseError } = responseData;
-            if (responseError) throw new BugsnagError(responseError, { url, headers });
-            if (!responseData.length) throw new Error(ERROR_CODES.EMPTY_RESPONSE);
-            if (isMounted.current) setData(responseData);
-          } else {
-            switch (status) {
-              case 400:
-              default:
-                throw new Error(ERROR_CODES.CONNECTION_FAILURE);
-              case 401:
-                throw new BugsnagError(ERROR_CODES.INVALID_PASSWORD, { url, headers });
-            }
-          }
-        } catch (responseError) {
-          if (isMounted.current) {
-            if (!isValidError(responseError)) {
-              responseError.message = ERROR_CODES.CONNECTION_FAILURE;
-            }
-            setError(responseError);
-          }
-        }
-      })();
-      const timer = setTimeout(() => {
-        if (isMounted.current) setData();
-        abortController.abort();
-      }, timeout);
-      return () => {
-        isMounted.current = false;
-        clearTimeout(timer);
-        abortController.abort();
-      };
-    }
+    _isMounted.current = true;
     return () => {
-      isMounted.current = false;
+      _isMounted.current = false;
+      reset();
     };
-  }, [url]);
+  }, []);
 
-  const isFetching = !(data || error);
-  return [data, error, isFetching];
+  const _fetch = async (path, init, opts) => {
+    const { responseHandler = res => res, errorHandler = err => err, timeout = 10000 } = opts;
+
+    const isMounted = () => _isMounted.current;
+    const isUnblocked = () => !_isBlocked.current;
+    const isMountedAndUnblocked = () => isMounted() && isUnblocked();
+
+    const onResponse = async res => {
+      const processedResponse = await responseHandler(res);
+      doIf(isMounted, setResponse, processedResponse);
+    };
+
+    const onError = async err => {
+      const processedError = await errorHandler(err);
+      doIf(isMounted, setError, processedError);
+    };
+
+    const beforeFetch = () => {
+      _isBlocked.current = true;
+      // eslint-disable-next-line no-undef
+      _controller.current = new AbortController();
+      _timer.current = setTimeout(() => {
+        if (_controller.current) _controller.current.abort();
+      }, timeout);
+      setIsLoading(true);
+    };
+
+    const tryFetch = async () => fetch(url + path, { ...init, signal: _controller.current.signal });
+
+    const afterFetch = () => {
+      _isBlocked.current = false;
+      clearTimeout(_timer.current);
+      setIsLoading(false);
+    };
+
+    const doFetch = async () => {
+      try {
+        doIf(isMountedAndUnblocked, beforeFetch);
+        const res = await doIf(isMounted, tryFetch);
+        await doIf(isMounted, onResponse, res);
+      } catch (err) {
+        await doIf(isMounted, onError(err));
+      } finally {
+        doIf(isMounted, afterFetch);
+      }
+    };
+
+    doIf(isMountedAndUnblocked, doFetch);
+  };
+
+  return { fetch: _fetch, reset, isLoading, response, error };
 };
