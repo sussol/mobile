c34c6a00d30a7ff73fc128dc2097198201bebe6d
Will Thomson <will.tho@outlook.com>
2020-08-14 17:02:47 +1200

    Fix duplicate records in sync out array

--- src/sync/SyncQueue.js
+++ src/sync/SyncQueue.js
@@ -103,9 +103,9 @@ export class SyncQueue {
           };
 
           if (this.isValidSyncOutRecord(syncOutRecord)) {
-            const existingSyncOutRecord = this.database.get('SyncOut', recordId);
+            const existingSyncOutRecord = this.database.get('SyncOut', recordId, 'recordId');
             if (existingSyncOutRecord) {
-              this.database.save('SyncOut', { ...existingSyncOutRecord, ...syncOutRecord });
+              this.database.save('SyncOut', { id: existingSyncOutRecord.id, ...syncOutRecord });
             } else {
               this.database.create('SyncOut', { id: generateUUID(), ...syncOutRecord });
             }
