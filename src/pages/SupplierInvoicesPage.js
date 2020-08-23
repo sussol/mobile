7cf6c925764a92851066f243b6d8f404780cc109
Will Thomson <will.tho@outlook.com>
2020-08-23 21:30:05 +1200

    Deselect invoices on create supplier invoice

--- src/pages/SupplierInvoicesPage.js
+++ src/pages/SupplierInvoicesPage.js
@@ -61,6 +61,7 @@ export const SupplierInvoices = ({
 
   const onCreateInvoice = otherParty => {
     dispatch(createSupplierInvoice(otherParty, currentUser));
+    onDeselectAll();
     onCloseModal();
   };
 
