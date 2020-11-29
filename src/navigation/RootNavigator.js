fce60904fa3c120484a101ea33f4a64da552f2d4
josh <griffinjoshua5@gmail.com>
2020-11-30 12:24:28 +1300

    Add navigation breadcrumbs

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -14,33 +14,40 @@
  */
 
 import React from 'react';
+import Bugsnag from '@bugsnag/react-native';
 import { StackActions } from '@react-navigation/native';
 
 export const rootNavigatorRef = React.createRef();
 
-const navigate = (screenName, screenProps) =>
+const navigate = (screenName, screenProps) => {
+  const breadcrumb = `Navigating: ${screenName}`;
+  Bugsnag.leaveBreadcrumb(breadcrumb);
   rootNavigatorRef.current?.navigate(screenName, screenProps);
+};
 
-const replace = (screenName, screenProps) =>
+const replace = (screenName, screenProps) => {
+  const breadcrumb = `Navigating: ${screenName}`;
+  Bugsnag.leaveBreadcrumb(breadcrumb);
   rootNavigatorRef.current?.dispatch(StackActions.replace(screenName, screenProps));
+};
 
-const goBack = () => rootNavigatorRef.current?.dispatch(StackActions.pop());
+const goBack = () => {
+  const breadcrumb = 'Navigating: Pop';
+  Bugsnag.leaveBreadcrumb(breadcrumb);
+  rootNavigatorRef.current?.dispatch(StackActions.pop());
+};
 
 const canGoBack = () => !!rootNavigatorRef.current?.canGoBack();
 
 const getCurrentRouteName = () => {
   if (!rootNavigatorRef.current?.getRootState()) return '';
-
   const routes = rootNavigatorRef.current?.getRootState()?.routes ?? [];
-
   return routes[routes.length - 1]?.name ?? '';
 };
 
 const getPrevRouteName = () => {
   if (!rootNavigatorRef.current?.getRootState()) return '';
-
   const routes = rootNavigatorRef.current?.getRootState()?.routes ?? [];
-
   return routes[routes.length - 2]?.name ?? '';
 };
 
