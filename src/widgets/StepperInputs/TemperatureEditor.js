76ff5047ecb600e85a753c6a1dab728b40476516
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-15 11:45:23 +1300

    Add TemperatureEditor

---
+++ src/widgets/StepperInputs/TemperatureEditor.js
@@ -0,0 +1,68 @@
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+
+import { TextInputWithAffix } from '../TextInputs';
+import { Incrementor } from './Incrementor';
+import { DARKER_GREY } from '../../globalStyles';
+
+const keepInRange = (num, min, max) => {
+  const temp = Math.min(num, max);
+  return Math.max(temp, min);
+};
+
+export const TemperatureEditor = ({
+  value,
+  onChange,
+  label,
+  textInputStyle,
+  suffixTextStyle,
+  stepAmount,
+  maximum,
+  minimum,
+}) => {
+  const formatted = keepInRange(value, minimum, maximum).toFixed(2);
+
+  const onIncrement = () => onChange(value + stepAmount);
+  const onDecrement = () => onChange(value - stepAmount);
+
+  return (
+    <>
+      <Incrementor
+        onIncrement={onIncrement}
+        onDecrement={onDecrement}
+        label={label}
+        Content={
+          <TextInputWithAffix
+            editable={false}
+            SuffixComponent={<Text style={suffixTextStyle}>{`${'\u00B0'}C`}</Text>}
+            style={textInputStyle}
+            value={formatted}
+          />
+        }
+      />
+    </>
+  );
+};
+
+TemperatureEditor.defaultProps = {
+  label: '',
+  textInputStyle: { color: DARKER_GREY, minWidth: 60, textAlign: 'right' },
+  suffixTextStyle: { color: DARKER_GREY },
+  stepAmount: 0.5,
+  maximum: 999,
+  minimum: 1,
+};
+
+TemperatureEditor.propTypes = {
+  minimum: PropTypes.number,
+  maximum: PropTypes.number,
+  label: PropTypes.string,
+  textInputStyle: PropTypes.object,
+  suffixTextStyle: PropTypes.object,
+  stepAmount: PropTypes.number,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
