35517d556dd0658b7c52b2def2193d4d609fe22a
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-15 20:57:55 +1300

    Add additional spacer helpers

--- src/widgets/Spacer.js
+++ src/widgets/Spacer.js
@@ -1,15 +1,77 @@
-import React from 'react';
+/* eslint-disable arrow-body-style */
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
-export const Spacer = ({ vertical, space }) => {
-  const widthOrHeight = vertical ? 'minHeight' : 'minWidth';
+export const Spacer = ({ vertical, horizontal, space }) => {
+  const internalStyle = {};
 
-  const internalStyle = { [widthOrHeight]: space };
+  if (vertical) {
+    internalStyle.minHeight = space;
+  }
+
+  if (horizontal) {
+    internalStyle.minWidth = space;
+  }
 
   return <View style={internalStyle} />;
 };
 
-Spacer.defaultProps = { vertical: false, space: 0 };
+Spacer.defaultProps = { vertical: false, horizontal: true, space: 0 };
 
-Spacer.propTypes = { vertical: PropTypes.bool, space: PropTypes.number };
+Spacer.propTypes = {
+  vertical: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  space: PropTypes.number,
+};
+
+export const SpacedChildren = ({ children, space, vertical, horizontal }) => {
+  return Children.map(children, (child, i) => {
+    const isLastChild = i === React.Children.count(children) - 1;
+    return (
+      <WithSpace space={isLastChild ? 0 : space} vertical={vertical} horizontal={horizontal}>
+        {child}
+      </WithSpace>
+    );
+  });
+};
+
+SpacedChildren.defaultProps = {
+  children: null,
+  space: 0,
+  vertical: false,
+  horizontal: true,
+};
+
+SpacedChildren.propTypes = {
+  children: PropTypes.node,
+  space: PropTypes.number,
+  vertical: PropTypes.bool,
+  horizontal: PropTypes.bool,
+};
+
+export const WithSpace = ({ space, vertical, horizontal, before, after, children }) => (
+  <>
+    {before && <Spacer space={space} vertical={vertical} horizontal={horizontal} />}
+    {children}
+    {after && <Spacer space={space} vertical={vertical} horizontal={horizontal} />}
+  </>
+);
+
+WithSpace.defaultProps = {
+  space: 0,
+  vertical: false,
+  horizontal: true,
+  before: false,
+  after: true,
+  children: null,
+};
+
+WithSpace.propTypes = {
+  space: PropTypes.number,
+  vertical: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  before: PropTypes.bool,
+  after: PropTypes.bool,
+  children: PropTypes.node,
+};
