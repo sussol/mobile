2c27e71107bb05e259e669ffc8ba59bf78eef4b8
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-17 00:17:03 +1300

    Add Recetangle component

---
+++ src/widgets/Rectangle.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { DANGER_RED } from '../globalStyles';
+
+const SIZES = {
+  large: { width: 180, height: 60 },
+};
+
+export const Rectangle = ({ children, colour, size }) => {
+  const dimensions = SIZES[size];
+  const background = { background: colour };
+  const internalStyle = [containerStyle, dimensions, background];
+
+  return <View style={internalStyle}>{children}</View>;
+};
+
+const containerStyle = {
+  borderRadius: 5.33,
+};
+
+Rectangle.defaultProps = {
+  children: null,
+  colour: DANGER_RED,
+  size: 'large',
+};
+
+Rectangle.propTypes = {
+  children: PropTypes.node,
+  colour: PropTypes.string,
+  size: PropTypes.string,
+};
