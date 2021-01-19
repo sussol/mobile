5cf799f7b6e7ab66a7c5c49b22ee4d898efc111e
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-20 11:34:25 +1300

    Add NewSensorPage placeholder wizard

--- src/pages/NewSensorPage.js
+++ src/pages/NewSensorPage.js
@@ -1,1 +1,24 @@
-export const NewSensorPage = () => null;
+import React from 'react';
+import { View } from 'react-native';
+
+import { Wizard } from '../widgets';
+
+const TabOne = () => <View style={{ backgroundColor: 'red', flex: 1, width: 500, height: 500 }} />;
+const TabTwo = () => (
+  <View style={{ backgroundColor: 'green', flex: 1, width: 500, height: 500 }} />
+);
+const TabThree = () => (
+  <View style={{ backgroundColor: 'blue', flex: 1, width: 500, height: 500 }} />
+);
+
+const tabs = [
+  { component: TabOne, name: 'prescription', title: '' },
+  { component: TabTwo, name: 'prescription', title: '' },
+  { component: TabThree, name: 'prescription', title: '' },
+];
+
+export const NewSensorPage = () => (
+  <>
+    <Wizard tabs={tabs} useNewStepper />
+  </>
+);
