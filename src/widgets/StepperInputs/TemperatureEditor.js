2092d3a8d8391ec92b70ee4a4722c72ee83cd4fe
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-22 12:13:22 +1300

    Add TemperatureEditor adjustments: keeping the temperature within a valid range and keeping a sane temperature value

--- src/widgets/StepperInputs/TemperatureEditor.js
+++ src/widgets/StepperInputs/TemperatureEditor.js
@@ -9,6 +9,8 @@ import { Incrementor } from './Incrementor';
 
 import { APP_FONT_FAMILY, DARKER_GREY } from '../../globalStyles';
 import { generalStrings } from '../../localization';
+import { FlexColumn } from '../FlexColumn';
+import temperature from '../../utilities/temperature';
 
 const keepInRange = (num, min, max) => {
   const temp = Math.min(num, max);
@@ -22,13 +24,20 @@ export const TemperatureEditor = ({
   textInputStyle,
   suffixTextStyle,
   stepAmount,
-  maximum,
-  minimum,
+  threshold,
+  above,
+  belowMinimum,
+  aboveMaximum,
 }) => {
+  const minimum = above ? threshold : belowMinimum;
+  const maximum = above ? aboveMaximum : threshold;
+
   const formatted = keepInRange(value, minimum, maximum).toFixed(2);
 
-  const onIncrement = () => onChange(value + stepAmount);
-  const onDecrement = () => onChange(value - stepAmount);
+  const onIncrement = () =>
+    onChange(keepInRange(temperature(value + stepAmount).temperature(), minimum, maximum));
+  const onDecrement = () =>
+    onChange(keepInRange(temperature(value - stepAmount).temperature(), minimum, maximum));
 
   return (
     <>
@@ -39,7 +48,12 @@ export const TemperatureEditor = ({
         Content={
           <TextInputWithAffix
             editable={false}
-            SuffixComponent={<Text style={suffixTextStyle}>{`${'\u00B0'}Celsius`}</Text>}
+            SuffixComponent={
+              <FlexColumn style={{ marginTop: 12 }}>
+                <Text style={suffixTextStyle}>{`${'\u00B0'}Celsius`}</Text>
+                <Text style={{ fontSize: 10 }}> and below</Text>
+              </FlexColumn>
+            }
             style={textInputStyle}
             value={formatted}
           />
@@ -54,23 +68,26 @@ TemperatureEditor.defaultProps = {
   textInputStyle: {
     fontSize: 14,
     color: DARKER_GREY,
-    width: 40,
+    width: 50,
     textAlign: 'right',
     fontFamily: APP_FONT_FAMILY,
   },
   suffixTextStyle: { fontSize: 12, color: DARKER_GREY, fontFamily: APP_FONT_FAMILY, width: 50 },
   stepAmount: 0.5,
-  maximum: 999,
-  minimum: 1,
+  above: true,
+  belowMinimum: -30,
+  aboveMaximum: 999,
 };
 
 TemperatureEditor.propTypes = {
-  minimum: PropTypes.number,
-  maximum: PropTypes.number,
+  above: PropTypes.bool,
+  threshold: PropTypes.number.isRequired,
   label: PropTypes.string,
   textInputStyle: PropTypes.object,
   suffixTextStyle: PropTypes.object,
   stepAmount: PropTypes.number,
   value: PropTypes.number.isRequired,
   onChange: PropTypes.func.isRequired,
+  belowMinimum: PropTypes.number,
+  aboveMaximum: PropTypes.number,
 };
