f3c7a610a6dcf53216fbc12ce05cde811de81df4
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-21 22:01:27 +1300

    Add vaccine reducer blink/start/stop cases

--- src/reducers/VaccineReducer.js
+++ src/reducers/VaccineReducer.js
@@ -3,15 +3,29 @@ import { VACCINE_ACTIONS } from '../actions/VaccineActions';
 const initialState = () => ({
   isScanning: false,
   scannedSensorAddresses: [],
+  sendingBlinkTo: '',
 });
 
 export const VaccineReducer = (state = initialState(), action) => {
   const { type } = action;
+
   switch (type) {
+    case VACCINE_ACTIONS.BLINK_START: {
+      const { payload } = action;
+      const { macAddress } = payload;
+
+      return { ...state, sendingBlinkTo: macAddress };
+    }
+
+    case VACCINE_ACTIONS.BLINK_STOP: {
+      return { ...state, sendingBlinkTo: '' };
+    }
+
     case VACCINE_ACTIONS.SCAN_START: {
       return {
         ...state,
         isScanning: true,
+        scannedSensorAddresses: [],
       };
     }
 
