3771683b979b8ebb08e05dda159b7f489a687a05
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-24 10:34:27 +1300

    Add update to breach config row to be passed more props

--- src/pages/NewSensor/NewSensorStepTwo.js
+++ src/pages/NewSensor/NewSensorStepTwo.js
@@ -11,14 +11,46 @@ import { SUSSOL_ORANGE, WHITE } from '../../globalStyles';
 import { buttonStrings, vaccineStrings } from '../../localization';
 import { WizardActions } from '../../actions/WizardActions';
 import { goBack } from '../../navigation/actions';
+import { selectConfigs } from '../../selectors/newSensor';
+import { NewSensorActions } from '../../actions';
 
-export const NewSensorStepTwoComponent = ({ nextTab, previousTab, exit }) => (
+export const NewSensorStepTwoComponent = ({
+  nextTab,
+  previousTab,
+  exit,
+  hotConsecutiveConfig,
+  coldCumulativeConfig,
+  coldConsecutiveConfig,
+  hotCumulativeConfig,
+  updateDuration,
+  updateTemperature,
+}) => (
   <TabContainer>
     <PaperSection height={320} headerText={vaccineStrings.new_sensor_step_two_title}>
-      <BreachConfigRow type="HOT_CONSECUTIVE" />
-      <BreachConfigRow type="COLD_CONSECUTIVE" />
-      <BreachConfigRow type="HOT_CUMULATIVE" />
-      <BreachConfigRow type="COLD_CUMULATIVE" />
+      <BreachConfigRow
+        type="HOT_CONSECUTIVE"
+        {...hotConsecutiveConfig}
+        updateDuration={updateDuration}
+        updateTemperature={updateTemperature}
+      />
+      <BreachConfigRow
+        type="COLD_CONSECUTIVE"
+        {...coldConsecutiveConfig}
+        updateDuration={updateDuration}
+        updateTemperature={updateTemperature}
+      />
+      <BreachConfigRow
+        type="HOT_CUMULATIVE"
+        {...hotCumulativeConfig}
+        updateDuration={updateDuration}
+        updateTemperature={updateTemperature}
+      />
+      <BreachConfigRow
+        type="COLD_CUMULATIVE"
+        {...coldCumulativeConfig}
+        updateDuration={updateDuration}
+        updateTemperature={updateTemperature}
+      />
     </PaperSection>
 
     <FlexRow flex={1} justifyContent="flex-end" alignItems="flex-end">
@@ -38,18 +70,45 @@ export const NewSensorStepTwoComponent = ({ nextTab, previousTab, exit }) => (
   </TabContainer>
 );
 
+const stateToProps = state => {
+  const {
+    hotConsecutiveConfig,
+    coldCumulativeConfig,
+    coldConsecutiveConfig,
+    hotCumulativeConfig,
+  } = selectConfigs(state);
+
+  return { hotConsecutiveConfig, coldCumulativeConfig, coldConsecutiveConfig, hotCumulativeConfig };
+};
+
 const dispatchToProps = dispatch => {
   const nextTab = () => dispatch(WizardActions.nextTab());
   const previousTab = () => dispatch(WizardActions.previousTab());
   const exit = () => dispatch(goBack());
+  const updateDuration = (type, value) =>
+    dispatch(NewSensorActions.updateConfig(type, 'duration', value));
+  const updateTemperature = (type, value) =>
+    dispatch(NewSensorActions.updateConfig(type, 'temperature', value));
 
-  return { nextTab, previousTab, exit };
+  return { nextTab, previousTab, exit, updateDuration, updateTemperature };
+};
+
+const configShape = {
+  temperature: PropTypes.number.isRequired,
+  duration: PropTypes.number.isRequired,
+  threshold: PropTypes.number.isRequired,
 };
 
 NewSensorStepTwoComponent.propTypes = {
   nextTab: PropTypes.func.isRequired,
   previousTab: PropTypes.func.isRequired,
   exit: PropTypes.func.isRequired,
+  hotConsecutiveConfig: PropTypes.shape(configShape).isRequired,
+  coldCumulativeConfig: PropTypes.shape(configShape).isRequired,
+  coldConsecutiveConfig: PropTypes.shape(configShape).isRequired,
+  hotCumulativeConfig: PropTypes.shape(configShape).isRequired,
+  updateDuration: PropTypes.func.isRequired,
+  updateTemperature: PropTypes.func.isRequired,
 };
 
-export const NewSensorStepTwo = connect(null, dispatchToProps)(NewSensorStepTwoComponent);
+export const NewSensorStepTwo = connect(stateToProps, dispatchToProps)(NewSensorStepTwoComponent);
