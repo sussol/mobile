151be020cac99fcb7987e8e56001710648189051
Joshua Griffin <griffinjoshua5@gmail.com>
2021-01-25 15:39:05 +1300

    Add update to VaccineChart padding

--- src/widgets/VaccineChart.js
+++ src/widgets/VaccineChart.js
@@ -48,6 +48,7 @@ export const VaccineChart = ({
           height={height}
           minDomain={chartMinDomain}
           maxDomain={chartMaxDomain}
+          padding={{ top: 0, bottom: 50, right: 10, left: 50 }}
         >
           <VictoryAxis
             offsetX={CHART_CONSTANTS.AXIS_OFFSET}
