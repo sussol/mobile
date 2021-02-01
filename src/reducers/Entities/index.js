7b00d8d0ddf7f2346169880b2e9eb8ed35298767
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-01 22:34:22 +1300

    Add import of location reducer

--- src/reducers/Entities/index.js
+++ src/reducers/Entities/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import { TemperatureBreachConfigReducer } from './TemperatureBreachConfigReducer';
 import { SensorReducer } from './SensorReducer';
+import { LocationReducer } from './LocationReducer';
 
 export const EntitiesReducer = combineReducers({
   temperatureBreachConfiguration: TemperatureBreachConfigReducer,
   sensor: SensorReducer,
+  location: LocationReducer,
 });
