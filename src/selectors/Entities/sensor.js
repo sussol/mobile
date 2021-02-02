3eb704b99865d400682493013985a1a8255f9470
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-02 23:24:16 +1300

    Add sensor selector updates

--- src/selectors/Entities/sensor.js
+++ src/selectors/Entities/sensor.js
@@ -1,19 +1,21 @@
 import { selectSpecificEntityState } from './index';
 
+export const selectSensorState = state => selectSpecificEntityState(state, 'sensor');
+
 export const selectNewSensor = state => {
-  const sensorState = selectSpecificEntityState(state, 'sensor');
+  const sensorState = selectSensorState(state);
   const { newId, byId } = sensorState;
   return byId[newId];
 };
 
 export const selectEditingSensor = state => {
-  const sensorState = selectSpecificEntityState(state, 'sensor');
+  const sensorState = selectSensorState(state);
   const { editingId, byId } = sensorState;
   return byId[editingId];
 };
 
 export const selectNewSensorId = state => {
-  const sensorState = selectSpecificEntityState(state, 'sensor');
+  const sensorState = selectSensorState(state);
   const { newId } = sensorState;
 
   return newId;
