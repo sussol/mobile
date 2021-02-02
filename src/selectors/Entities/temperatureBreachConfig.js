dbaa260846244f4f71d190ab5e3d941b64d39e56
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-02 17:13:22 +1300

    Add updated temperature breach config selectors

--- src/selectors/Entities/temperatureBreachConfig.js
+++ src/selectors/Entities/temperatureBreachConfig.js
@@ -6,6 +6,47 @@ export const selectNewConfigs = state => {
   return newIds.map(id => byId[id]);
 };
 
+export const selectEditingConfigs = state => {
+  const configState = selectSpecificEntityState(state, 'temperatureBreachConfiguration');
+  const { byId, editingIds } = configState;
+  return editingIds.map(id => byId[id]);
+};
+
+export const selectEditingConfigsByType = state => {
+  const newConfigs = selectEditingConfigs(state);
+
+  return newConfigs.reduce(
+    (acc, config) => ({
+      ...acc,
+      [config?.type]: config,
+    }),
+    {}
+  );
+};
+
+export const selectEditingConfigThresholds = state => {
+  const editingConfigs = selectEditingConfigsByType(state);
+  const {
+    HOT_CONSECUTIVE: hotConsecutiveConfig = {},
+    COLD_CONSECUTIVE: coldConsecutiveConfig = {},
+    HOT_CUMULATIVE: hotCumulativeConfig = {},
+    COLD_CUMULATIVE: coldCumulativeConfig = {},
+  } = editingConfigs;
+
+  const coldConsecutiveThreshold = (hotConsecutiveConfig.minimumTemperature ?? 0) - 0.1;
+  const hotConsecutiveThreshold = (coldConsecutiveConfig.maximumTemperature ?? 0) + 0.1;
+
+  const hotCumulativeThreshold = (coldCumulativeConfig.maximumTemperature ?? 0) - 0.1;
+  const coldCumulativeThreshold = (hotCumulativeConfig.minimumTemperature ?? 0) + 0.1;
+
+  return {
+    coldConsecutiveThreshold,
+    hotConsecutiveThreshold,
+    hotCumulativeThreshold,
+    coldCumulativeThreshold,
+  };
+};
+
 export const selectNewConfigsByType = state => {
   const newConfigs = selectNewConfigs(state);
 
