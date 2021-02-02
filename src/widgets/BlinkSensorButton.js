bcc7ba8d2b02da6d8516fcbccdddcb0538f7a719
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-02 23:26:25 +1300

    Add blinkSensor button component

---
+++ src/widgets/BlinkSensorButton.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { ActivityIndicator } from 'react-native';
+import { DARKER_GREY } from '../globalStyles/index';
+import { IconButton } from './IconButton';
+import { LightbulbIcon } from './icons';
+import { selectSendingBlinkTo } from '../selectors/Bluetooth/bluetooth';
+import { BlinkActions } from '../actions/Bluetooth/BlinkActions';
+
+export const BlinkSensorButtonComponent = ({ isBlinking, blink, isBlinkDisabled }) =>
+  isBlinking ? (
+    <ActivityIndicator style={{ width: 50 }} size="small" color={DARKER_GREY} />
+  ) : (
+    <IconButton
+      containerStyle={{ width: 50, justifyContent: 'center' }}
+      Icon={<LightbulbIcon color={DARKER_GREY} />}
+      onPress={blink}
+      isDisabled={isBlinkDisabled}
+    />
+  );
+
+BlinkSensorButtonComponent.propTypes = {
+  isBlinkDisabled: PropTypes.bool.isRequired,
+  blink: PropTypes.func.isRequired,
+  isBlinking: PropTypes.bool.isRequired,
+};
+
+const stateToProps = (state, props) => {
+  const { macAddress } = props;
+
+  const sendingBlinkTo = selectSendingBlinkTo(state);
+  const isBlinking = sendingBlinkTo === macAddress;
+  const isBlinkDisabled = !isBlinking && sendingBlinkTo;
+
+  return { isBlinking, isBlinkDisabled };
+};
+
+const dispatchToProps = (dispatch, props) => {
+  const { macAddress } = props;
+
+  const blink = () => dispatch(BlinkActions.startSensorBlink(macAddress));
+
+  return { blink };
+};
+
+export const BlinkSensorButton = connect(stateToProps, dispatchToProps)(BlinkSensorButtonComponent);
