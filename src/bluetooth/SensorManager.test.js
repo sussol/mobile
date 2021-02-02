d41cdaaf83d9b45ec70106e836de49422b272eff
Mark Prins <mark@malcolmprins.net>
2021-02-03 09:23:10 +1300

    fix sensorManager Test

--- src/bluetooth/SensorManager.test.js
+++ src/bluetooth/SensorManager.test.js
@@ -1,15 +1,20 @@
-import { SensorManager } from './SensorManager';
+import SensorManager from './SensorManager';
 
 describe('SensorManager: saveSensor', () => {
-  it('Creates sensor with expected defaults', () => {
-    const dbService = {};
+  it('Creates sensor with expected defaults', async () => {
+    const dummyLocation = {
+      id: 1,
+      code: 'a',
+      name: 'Location A',
+    };
+    const dbService = { getLocations: () => [dummyLocation] };
     const utils = { createUuid: () => '1' };
-    const sensorManager = new SensorManager(dbService, utils);
+    const sensorManager = SensorManager(dbService, utils);
 
     const sensor = {
       id: 'a',
       isActive: true,
-      location: null,
+      location: dummyLocation,
       logInterval: 300,
       macAddress: '00:AA:BB:CC:DD:EE:FF',
       name: 'A sensor',
@@ -19,12 +24,12 @@ describe('SensorManager: saveSensor', () => {
       id: '1',
       batteryLevel: 0,
       isActive: true,
-      location: null,
+      location: dummyLocation,
       logInterval: 300,
       macAddress: '00:AA:BB:CC:DD:EE:FF',
       name: 'A sensor',
     };
-
-    expect(sensorManager.createSensor(sensor)).toEqual(shouldBe);
+    const newSensor = await sensorManager.createSensor(sensor);
+    expect(newSensor).toEqual(shouldBe);
   });
 });
