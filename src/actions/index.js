1ce1ae5e3d9417ba32692361443cc8dda3eb6467
Kat <katherine@sussol.net>
2021-02-03 17:29:41 +1300

    Fix index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,9 @@ export { WIZARD_ACTIONS, WizardActions } from './WizardActions';
 export { USER_ACTION_TYPES, UserActions } from './UserActions';
 export { CASH_TRANSACTION_ACTION_TYPES, CashTransactionActions } from './CashTransactionActions';
 export { DASHBOARD_ACTION_TYPES, DashboardActions } from './DashboardActions';
-export { DOWNLOAD_ACTIONS, DownloadActions } from './Bluetooth/DownloadActions';
-export { BLINK_ACTIONS, BlinkActions } from './Bluetooth/BlinkActions';
-export { SCAN_ACTIONS, ScanActions } from './Bluetooth/ScanActions';
+export { DOWNLOAD_ACTIONS, SensorDownloadActions } from './Bluetooth/SensorDownloadActions';
+export { BLINK_ACTIONS, SensorBlinkActions } from './Bluetooth/SensorBlinkActions';
+export { SCAN_ACTIONS, SensorScanActions } from './Bluetooth/SensorScanActions';
+export { UPDATE_ACTIONS, SensorUpdateActions } from './Bluetooth/SensorUpdateActions';
 
 export * from './Entities';
