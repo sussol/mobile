702ab485810021ac11b7de29affd70d34ccf226e
Kat <katherine@sussol.net>
2021-02-04 15:00:48 +1300

    Move bluetooth state selection into index

--- src/selectors/Bluetooth/bluetooth.js
+++ src/selectors/Bluetooth/bluetooth.js
@@ -3,43 +3,52 @@
  * Sustainable Solutions (NZ) Ltd. 2021
  */
 
-export const selectSendingBlinkTo = ({ bluetooth }) => {
+import { selectBluetoothState } from './index';
+
+export const selectSendingBlinkTo = state => {
+  const bluetooth = selectBluetoothState(state);
   const { blink } = bluetooth || {};
   const { sendingBlinkTo = '' } = blink || {};
   return sendingBlinkTo;
 };
 
-export const selectIsScanning = ({ bluetooth }) => {
+export const selectIsScanning = state => {
+  const bluetooth = selectBluetoothState(state);
   const { scan } = bluetooth || {};
   const { isScanning = false } = scan || {};
   return isScanning;
 };
 
-export const selectScannedSensors = ({ bluetooth }) => {
+export const selectScannedSensors = state => {
+  const bluetooth = selectBluetoothState(state);
   const { scan } = bluetooth || {};
   const { scannedSensorAddresses = [] } = scan || {};
   return scannedSensorAddresses;
 };
 
-export const selectIsSyncingTemps = ({ bluetooth }) => {
+export const selectIsSyncingTemps = state => {
+  const bluetooth = selectBluetoothState(state);
   const { download } = bluetooth || {};
   const { isSyncingTemps = false } = download || {};
   return isSyncingTemps;
 };
 
-export const selectDownloadingLogsFrom = ({ bluetooth }) => {
+export const selectDownloadingLogsFrom = state => {
+  const bluetooth = selectBluetoothState(state);
   const { download } = bluetooth || {};
   const { downloadingLogsFrom } = download;
   return downloadingLogsFrom;
 };
 
-export const selectLastDownloadTime = ({ bluetooth }, macAddress) => {
+export const selectLastDownloadTime = (state, macAddress) => {
+  const bluetooth = selectBluetoothState(state);
   const { download } = bluetooth || {};
   const { lastDownloadTime } = download;
   return lastDownloadTime[macAddress] ? new Date(lastDownloadTime[macAddress]) : null;
 };
 
-export const selectLastDownloadFailed = ({ bluetooth }, macAddress) => {
+export const selectLastDownloadFailed = (state, macAddress) => {
+  const bluetooth = selectBluetoothState(state);
   const { download } = bluetooth || {};
   const { lastDownloadStatus } = download;
   const status = lastDownloadStatus[macAddress];
