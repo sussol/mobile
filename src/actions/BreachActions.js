e3845173e5f891feed8a4682b2e4d380b920c5a7
Kat <katherine@sussol.net>
2021-02-05 16:03:15 +1300

    Add breach creation actions

--- src/actions/BreachActions.js
+++ src/actions/BreachActions.js
@@ -3,15 +3,42 @@
  * Sustainable Solutions (NZ) Ltd. 2020
  */
 
+import { BreachManager } from '../bluetooth/BreachManager';
 import { UIDatabase } from '../database';
 
 export const BREACH_ACTIONS = {
+  CREATE_CONSECUTIVE_SUCCESS: 'breachActions/createConsecutiveSuccess',
+  CREATE_CONSECUTIVE_FAIL: 'breachActions/createConsecutiveFail',
   CLOSE_MODAL: 'breachActions/closeModal',
   VIEW_FRIDGE_BREACHES: 'breachActions/viewFridgeBreaches',
   VIEW_ITEM_BREACHES: 'breachActions/viewItemBreaches,',
 };
 
 const close = () => ({ type: BREACH_ACTIONS.CLOSE_MODAL });
+const createConsecutiveFail = () => ({ type: BREACH_ACTIONS.CREATE_CONSECUTIVE_FAIL });
+
+const createConsecutiveSuccess = (sensor, updatedBreaches, updatedLogs) => ({
+  type: BREACH_ACTIONS.CREATE_CONSECUTIVE_SUCCESS,
+  payload: { sensor, updatedBreaches, updatedLogs },
+});
+
+const createConsecutiveBreaches = sensor => dispatch => {
+  const { id } = sensor;
+
+  try {
+    const logs = BreachManager.getLogsToCheck(id);
+    const configs = BreachManager.getBreachConfigs();
+    const mostRecentBreach = BreachManager.getMostRecentBreach(id);
+
+    const breaches = BreachManager.createBreaches(sensor, logs, configs, mostRecentBreach);
+    const [updatedBreaches, updatedLogs] = BreachManager.updateBreaches(breaches, logs);
+    dispatch(createConsecutiveSuccess(sensor, updatedBreaches, updatedLogs));
+  } catch (error) {
+    dispatch(createConsecutiveFail);
+  }
+
+  return null;
+};
 
 const viewStocktakeBatchBreaches = stocktakeBatchId => {
   const stocktakeBatch = UIDatabase.get('StocktakeBatch', stocktakeBatchId);
@@ -35,6 +62,7 @@ const viewFridgeBreach = breachId => {
 };
 
 export const BreachActions = {
+  createConsecutiveBreaches,
   close,
   viewFridgeBreach,
   viewStocktakeBatchBreaches,
