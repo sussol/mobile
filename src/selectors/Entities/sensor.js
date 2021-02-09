e8dfec7fbaf811315171aa01d1c4a3bf91b2c3bd
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-09 21:48:12 +1300

    Add selectors

--- src/selectors/Entities/sensor.js
+++ src/selectors/Entities/sensor.js
@@ -1,3 +1,4 @@
+import { VACCINE_CONSTANTS } from '../../utilities/modules/vaccines/index';
 import { selectSpecificEntityState } from './index';
 
 export const selectSensorState = state => selectSpecificEntityState(state, 'sensor');
@@ -36,4 +37,34 @@ export const selectNewSensorId = state => {
 export const selectSensors = state => {
   const sensorsById = selectSensorsById(state);
   return Object.values(sensorsById);
+};
+
+export const selectIsLowBatteryByMac = (state, mac) => {
+  const sensor = selectSensorByMac(state, mac);
+  const { batteryLevel } = sensor;
+  return batteryLevel <= VACCINE_CONSTANTS.LOW_BATTERY_PERCENTAGE;
+};
+
+export const selectIsInHotBreachByMac = (state, mac) => {
+  const sensor = selectSensorByMac(state, mac);
+  const { isInHotBreach } = sensor;
+  return isInHotBreach;
+};
+
+export const selectIsInColdBreachByMac = (state, mac) => {
+  const sensor = selectSensorByMac(state, mac);
+  const { isInColdBreach } = sensor;
+  return isInColdBreach;
+};
+
+export const selectIsInBreachByMac = (state, mac) => {
+  const sensor = selectSensorByMac(state, mac);
+  const { isInHotBreach, isInColdBreach } = sensor;
+  return isInHotBreach || isInColdBreach;
+};
+
+export const selectIsInDangerByMac = (state, mac) => {
+  const isLowBattery = selectIsLowBatteryByMac(state, mac);
+  const isInBreach = selectIsInBreachByMac(state, mac);
+  return isLowBattery || isInBreach;
 };
