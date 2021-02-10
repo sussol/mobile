ec78f5ad020d23b084e0bc57067f5e22d493f984
Joshua Griffin <griffinjoshua5@gmail.com>
2021-02-10 19:55:02 +1300

    Add change to logic of useConditionalAnimationRef

--- src/hooks/useConditionalAnimationRef.js
+++ src/hooks/useConditionalAnimationRef.js
@@ -1,9 +1,5 @@
 import { useRef, useEffect } from 'react';
-
-// Most animations are considered 'interactions' - if there is no break
-// in the interaction, then InteractionManager.runAfterInteractions() is
-// never fired.
-const DURATION_OFFSET = 100;
+import { MILLISECONDS } from '../utilities';
 
 /**
  * Hook used to return a ref which should be applied to an animatable view created by
@@ -13,19 +9,18 @@ const DURATION_OFFSET = 100;
  * @param {string} animation the animation type see: react-native-animatable.
  * @param {number} duration the number of milliseconds per animation.
  */
-export const useConditionalAnimationRef = (condition, animation = 'flash', duration = 1000) => {
+export const useConditionalAnimationRef = (
+  condition,
+  animation = 'flash',
+  duration = MILLISECONDS.ONE_SECOND
+) => {
   const ref = useRef();
-  const interval = useRef();
 
   const start = () => {
-    interval.current = setInterval(
-      () => ref?.current[animation](duration),
-      duration + DURATION_OFFSET
-    );
+    ref?.current[animation](duration);
   };
 
   const stop = () => {
-    clearInterval(interval.current);
     ref?.current?.stopAnimation();
   };
 
