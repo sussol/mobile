189c0f3216ce5836fe7b36eb77ea34e547c0e559
Kat <katherine@sussol.net>
2021-02-26 15:07:07 +1300

    Add back in min 0.1 difference

--- src/selectors/Entities/temperatureBreachConfig.js
+++ src/selectors/Entities/temperatureBreachConfig.js
@@ -33,11 +33,11 @@ export const selectEditingConfigThresholds = state => {
     COLD_CUMULATIVE: coldCumulativeConfig = {},
   } = editingConfigs;
 
-  const coldConsecutiveThreshold = hotConsecutiveConfig.minimumTemperature ?? 0 - 0.1;
-  const hotConsecutiveThreshold = coldConsecutiveConfig.maximumTemperature ?? 0 + 0.1;
+  const coldConsecutiveThreshold = (hotConsecutiveConfig.minimumTemperature ?? 0) - 0.1;
+  const hotConsecutiveThreshold = (coldConsecutiveConfig.maximumTemperature ?? 0) + 0.1;
 
-  const coldCumulativeThreshold = hotCumulativeConfig.minimumTemperature ?? 0 + 0.1;
-  const hotCumulativeThreshold = coldCumulativeConfig.maximumTemperature ?? 0 - 0.1;
+  const coldCumulativeThreshold = (hotCumulativeConfig.minimumTemperature ?? 0) + 0.1;
+  const hotCumulativeThreshold = (coldCumulativeConfig.maximumTemperature ?? 0) - 0.1;
 
   return {
     coldConsecutiveThreshold,
@@ -68,11 +68,11 @@ export const selectNewConfigThresholds = state => {
     COLD_CUMULATIVE: coldCumulativeConfig = {},
   } = newConfigs;
 
-  const coldConsecutiveThreshold = hotConsecutiveConfig.minimumTemperature ?? 0 - 0.1;
-  const hotConsecutiveThreshold = coldConsecutiveConfig.maximumTemperature ?? 0 + 0.1;
+  const coldConsecutiveThreshold = (hotConsecutiveConfig.minimumTemperature ?? 0) - 0.1;
+  const hotConsecutiveThreshold = (coldConsecutiveConfig.maximumTemperature ?? 0) + 0.1;
 
-  const coldCumulativeThreshold = hotCumulativeConfig.minimumTemperature ?? 0 + 0.1;
-  const hotCumulativeThreshold = coldCumulativeConfig.maximumTemperature ?? 0 - 0.1;
+  const coldCumulativeThreshold = (hotCumulativeConfig.minimumTemperature ?? 0) + 0.1;
+  const hotCumulativeThreshold = (coldCumulativeConfig.maximumTemperature ?? 0) - 0.1;
 
   return {
     coldConsecutiveThreshold,
