3ed8bf5e8ad92653469ceeecd0291c492f1d408f
Joshua Griffin <griffinjoshua5@gmail.com>
2021-03-01 21:17:26 +1300

    Add use of FormLabel for Title field

--- src/widgets/JSONForm/fields/Title.js
+++ src/widgets/JSONForm/fields/Title.js
@@ -1,17 +1,34 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable no-console */
 import React from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { StyleSheet } from 'react-native';
+import { FormLabel } from '../../FormInputs/FormLabel';
+
+import { APP_FONT_FAMILY, APP_GENERAL_FONT_SIZE } from '../../../globalStyles/fonts';
 
 export const Title = props => {
-  console.log('-------------------------------------------');
-  console.log('Title Field - props', props);
-  console.log('-------------------------------------------');
-  return (
-    <View style={{ borderWidth: 1, marginLeft: 10 }}>
-      <Text>TitleField</Text>
-      <Text>{props.title}</Text>
-    </View>
-  );
+  const { title, isRequired } = props;
+
+  return title ? (
+    <FormLabel textStyle={styles.textStyle} isRequired={isRequired} value={title} />
+  ) : null;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontWeight: 'bold',
+    fontSize: APP_GENERAL_FONT_SIZE,
+    fontFamily: APP_FONT_FAMILY,
+  },
+});
+
+Title.defaultProps = {
+  title: '',
+  isRequired: false,
+};
+
+Title.propTypes = {
+  title: PropTypes.string,
+  isRequired: PropTypes.bool,
 };
