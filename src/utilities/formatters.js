f715caa680b8bef50620aec6dab7153d2b7df76a
wlthomson <will.tho@outlook.com>
2021-03-04 21:20:29 +1300

    Update breach plot tick formatting

--- src/utilities/formatters.js
+++ src/utilities/formatters.js
@@ -61,21 +61,7 @@ export const formatTimeDifference = duration => {
   );
 };
 
-export const getTickFormatter = () => {
-  let currentDay = null;
-  return tick => {
-    let formatted = '';
-    const nextCurrentDay = moment(tick).day();
-    if (currentDay !== nextCurrentDay) {
-      currentDay = moment(tick).day();
-      formatted = moment(tick).format('h:mma[\n](D/M)');
-    } else {
-      formatted = moment(tick).format('h:mma');
-    }
-
-    return formatted;
-  };
-};
+export const getTickFormatter = () => tick => moment(tick).format('h:mma[\n](D/M)');
 
 export const formatDate = date => (date ? moment(date).fromNow() : generalStrings.not_available);
 
