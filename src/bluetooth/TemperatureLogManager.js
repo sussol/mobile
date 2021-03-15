87e85fba1e624f5a2c26a2fcea18b81eb3dc7395
Kat <katherine@sussol.net>
2021-03-15 16:00:58 +1300

    Update temperature logs to use count back logic

--- src/bluetooth/TemperatureLogManager.js
+++ src/bluetooth/TemperatureLogManager.js
@@ -62,6 +62,14 @@ class TemperatureLogManager {
       initial = moment.unix(timeNow).subtract((logsToSave.length - 1) * logInterval, 'seconds');
     } else {
       initial = moment.unix(mostRecentLogTime).add(logInterval, 's');
+
+      const numberOfLogIntervalsUntilNow =
+        Math.floor((timeNow - mostRecentLogTime) / logInterval) + 1;
+
+      initial = moment.unix(
+        mostRecentLogTime +
+          (numberOfLogIntervalsUntilNow * logInterval - maxNumberToSave * logInterval)
+      );
     }
 
     return logsToSave.map(({ temperature }, i) => {
