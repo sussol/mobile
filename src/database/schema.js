4fe8c384f50dd2835b980e4550462da1d5e88b87
Joshua Griffin <griffinjoshua5@gmail.com>
2021-03-18 21:36:41 +1300

    Add ADR table to schema

--- src/database/schema.js
+++ src/database/schema.js
@@ -5,6 +5,7 @@
 
 import {
   Abbreviation,
+  AdverseDrugReaction,
   Address,
   Currency,
   Ethnicity,
@@ -195,6 +196,7 @@ export const schema = {
   schema: [
     Abbreviation,
     Address,
+    AdverseDrugReaction,
     Currency,
     Ethnicity,
     FormSchema,
