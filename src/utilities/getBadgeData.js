5333ca75a3bc778be01ec851dc780923b244362a
Kat <katherine@sussol.net>
2021-06-18 15:42:09 +1200

    Add a badge to show all un-acknowledged breaches

--- src/utilities/getBadgeData.js
+++ src/utilities/getBadgeData.js
@@ -7,17 +7,32 @@ const routeList = {
   supplierInvoices: 'SupplierInvoice',
   stocktakes: 'Stocktake',
   customerInvoices: 'CustomerInvoice',
+  vaccines: 'TemperatureBreach',
 };
 
 const getBadgeData = routeName => {
   const dataType = routeName in routeList ? routeList[routeName] : '';
 
-  return [
-    {
-      count: dataType ? UIDatabase.objects(dataType).filtered('status != "finalised"').length : 0,
-      title: `${generalStrings.unfinalised} ${generalStrings[routeName]}`,
-    },
-  ];
+  switch (dataType) {
+    case 'TemperatureBreach':
+      return [
+        {
+          count: dataType
+            ? UIDatabase.objects(dataType).filtered('acknowledged == false').length
+            : 0,
+          title: `${generalStrings.unacknowledged} ${generalStrings[routeName]}`,
+        },
+      ];
+    default:
+      return [
+        {
+          count: dataType
+            ? UIDatabase.objects(dataType).filtered('status != "finalised"').length
+            : 0,
+          title: `${generalStrings.unfinalised} ${generalStrings[routeName]}`,
+        },
+      ];
+  }
 };
 
 export default getBadgeData;
