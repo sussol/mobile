d2c1478d15710ec0ecebc0827e56d1809f173991
Joshua Griffin <griffinjoshua5@gmail.com>
2021-07-12 11:25:44 +1200

    Add logging exports and constants

---
+++ src/utilities/logging/index.js
@@ -0,0 +1,41 @@
+import RNFS from 'react-native-fs';
+import moment from 'moment';
+
+const logDir = `${RNFS.ExternalStorageDirectoryPath}/Download/mSupplyMobile_data`;
+const logFileName = 'log.txt';
+const logFileDate = 'DD-MM-YY';
+const logFileSeparator = '__';
+
+export const LogLevel = {
+  trace: 10,
+  debug: 20,
+  info: 30,
+  warn: 40,
+  error: 50,
+  fatal: 60,
+};
+
+const logFileFilter = file => file?.includes(`${logFileSeparator}${logFileName}`);
+
+const getExceedsThresholdFilter = (threshold = [5, 'days']) => fileName => {
+  const thresholdDate = moment().subtract(...threshold);
+  const [maybeDate] = fileName.split(logFileSeparator);
+  const asMoment = moment(maybeDate, logFileDate);
+  return asMoment.isBefore(thresholdDate);
+};
+
+export const tidyLogFiles = async () => {
+  try {
+    const files = await RNFS.readdir(logDir);
+
+    return Promise.all(
+      files
+        .filter(logFileFilter)
+        .filter(getExceedsThresholdFilter())
+        .map(fileName => RNFS.unlink(`${logDir}/${fileName}`))
+    );
+  } catch (error) {
+    return null;
+    // Going to just ignore errors for now..?
+  }
+};
